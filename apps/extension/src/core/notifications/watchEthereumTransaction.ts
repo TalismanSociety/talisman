f189655bd525ea3aa703b1bcfee29bc2a361bb03
Kheops <arnault.nouvel@lgkev.com>
2022-05-27 13:10:37 +0900

    refactor: use shared notification utils for eth

--- apps/extension/src/core/notifications/watchEthereumTransaction.ts
+++ apps/extension/src/core/notifications/watchEthereumTransaction.ts
@@ -6,21 +6,11 @@ import {
 import { nanoid } from "nanoid"
 import * as Sentry from "@sentry/browser"
 import urlJoin from "url-join"
-
-const onNotificationClicked = (notificationId: string) => {
-  // we actually use an url as identifier, redirect to it
-  if (notificationId.startsWith("https://")) Browser.tabs.create({ url: notificationId })
-}
-
-const ensureNotificationClickHandler = () => {
-  if (!Browser.notifications.onClicked.hasListener(onNotificationClicked))
-    Browser.notifications.onClicked.addListener(onNotificationClicked)
-}
+import { createNotification } from "./createNotification"
 
 export const watchEthereumTransaction = async (ethChainId: number, txHash: string) => {
-  let txUrl: string | undefined
   try {
-    const ethereumNetwork = await ethereumNetworkStore.ethereumNetwork(ethChainId)
+    var ethereumNetwork = await ethereumNetworkStore.ethereumNetwork(ethChainId)
     if (!ethereumNetwork) {
       throw new Error(`Could not find ethereum network ${ethChainId}`)
     }
@@ -30,49 +20,26 @@ export const watchEthereumTransaction = async (ethChainId: number, txHash: strin
       throw new Error(`No provider for network ${ethChainId} (${ethereumNetwork.name})`)
     }
 
-    ensureNotificationClickHandler()
-
-    txUrl =
-      ethereumNetwork.explorerUrls.length > 0
-        ? urlJoin(ethereumNetwork.explorerUrls[0], "tx", txHash)
-        : nanoid()
+    const txUrl = ethereumNetwork.explorerUrls.length
+      ? urlJoin(ethereumNetwork.explorerUrls[0], "tx", txHash)
+      : nanoid()
 
     // PENDING
-    const notificationId = await Browser.notifications.create(txUrl, {
-      type: "basic",
-      title: "👀 Transaction submitted 🧐",
-      message: `Waiting on transaction confirmation on ${ethereumNetwork.name}.`,
-      iconUrl: "/images/tx-ok.png",
-    })
+    await createNotification("submitted", ethereumNetwork.name, txUrl)
 
-    const receipt = await provider.waitForTransaction(txHash)
+    try {
+      const receipt = await provider.waitForTransaction(txHash)
 
-    // delete previous notification before creating a new one, to make sure user sees the toast
-    await Browser.notifications.clear(notificationId)
-
-    if (receipt.blockNumber) {
-      // SUCCESS
-      await Browser.notifications.create(txUrl, {
-        type: "basic",
-        title: "✅ Transaction confirmed ⭐",
-        message: `Your transaction on ${ethereumNetwork.name} has been confirmed.`,
-        iconUrl: "/images/tx-ok.png",
-      })
-    } else {
-      // FAILED
-      throw new Error(`Your transaction on ${ethereumNetwork.name} has been confirmed.`)
+      // success if associated to a block number
+      await createNotification(
+        receipt.blockNumber ? "success" : "error",
+        ethereumNetwork.name,
+        txUrl
+      )
+    } catch (err) {
+      await createNotification("error", ethereumNetwork.name, txUrl, err as Error)
     }
   } catch (err) {
-    try {
-      const error = err as any
-      await Browser.notifications.create(txUrl ?? nanoid(), {
-        type: "basic",
-        title: "❌ Transaction failed 😵",
-        message: error?.reason || error?.message || "Unknown error",
-        iconUrl: "/images/tx-nok.png",
-      })
-    } catch (err2) {
-      Sentry.captureException(err2)
-    }
+    Sentry.captureException(err, { tags: { ethChainId, txHash } })
   }
 }
