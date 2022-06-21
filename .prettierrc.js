22364c8d01280d1fa36e14195c3e0aa39df669c2
alecdwm <alec@owls.io>
2022-06-21 18:46:57 +0200

    feat: added @trivago/prettier-plugin-sort-imports

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,10 @@ module.exports = {
   printWidth: 100,
   quoteProps: "consistent",
   semi: false,
+  pluginSearchDirs: ["./node_modules"],
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  importOrder: ["<THIRD_PARTY_MODULES>", "^@talisman/(.*)$", "^@core/(.*)$", "^@ui/(.*)$", "^[./]"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
 }
