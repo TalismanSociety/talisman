6fded29a7e9c3ee02ad95471d4da91265783ba54
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-07-17 00:58:05 +0900

    fix: reset zoom level in popup

--- apps/extension/src/index.popup.tsx
+++ apps/extension/src/index.popup.tsx
@@ -1,7 +1,11 @@
 import Popup from "@ui/apps/popup"
+import Browser from "webextension-polyfill"
+
 import { renderTalisman } from "./ui/"
 
 // append a class on root HTML element if popup was opened from extension button
 if (window.location.hash === "#embedded") document.documentElement.classList.add("embedded")
+// if not, we're in a window, reset zoom level to 100%
+else Browser.tabs.setZoom(undefined, 0)
 
 renderTalisman(<Popup />)
