4435da1e6859753d5eace34da02afb84a4377b0a
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-07-26 16:33:04 +0900

    feat: icon on external links

--- apps/extension/src/ui/apps/dashboard/layout/SideBar.tsx
+++ apps/extension/src/ui/apps/dashboard/layout/SideBar.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@talisman/components/Box"
 import { IconButton } from "@talisman/components/IconButton"
 import Nav, { NavItem } from "@talisman/components/Nav"
 import { PillButton } from "@talisman/components/PillButton"
@@ -6,6 +7,7 @@ import { WithTooltip } from "@talisman/components/Tooltip"
 import { breakpoints } from "@talisman/theme/definitions"
 import {
   CopyIcon,
+  ExternalLinkIcon,
   ImageIcon,
   PaperPlaneIcon,
   PlusIcon,
@@ -184,7 +186,8 @@ const Container = styled.aside`
       padding: 1.6rem;
     }
 
-    nav .link span:last-child {
+    nav .link span:last-child,
+    nav .link ${Box} {
       display: none;
     }
     nav .link {
@@ -216,6 +219,10 @@ const ResponsiveTooltip = ({
     <>{children}</>
   )
 }
+
+const LinkIcon = styled(ExternalLinkIcon)`
+  margin-left: -0.4rem !important;
+`
 
 export const SideBar = () => {
   const { account } = useSelectedAccount()
@@ -289,7 +296,9 @@ export const SideBar = () => {
               </ResponsiveTooltip>
             }
           >
-            NFTs
+            <Box>
+              NFTs <ExternalLinkIcon />
+            </Box>
           </NavItem>
           <NavItem
             external
@@ -300,7 +309,12 @@ export const SideBar = () => {
               </ResponsiveTooltip>
             }
           >
-            Crowdloans
+            <Box flex justify={"center"} gap={0.6}>
+              <Box>Crowdloans</Box>
+              <Box>
+                <ExternalLinkIcon />
+              </Box>
+            </Box>
           </NavItem>
           <NavItem
             to="/settings"
