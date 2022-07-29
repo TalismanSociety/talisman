fb976c5bb02ba8de707af411f1bf39bc96a4035b
alecdwm <alec@owls.io>
2022-07-29 18:30:39 +1000

    fix: abcd ocd

--- apps/extension/webpack/webpack.prod.js
+++ apps/extension/webpack/webpack.prod.js
@@ -98,8 +98,8 @@ let getPlugins = (env) => {
     else {
       plugins = [
         new webpack.DefinePlugin({
-          "process.env.SENTRY_RELEASE": JSON.stringify(getSentryRelease(env)),
           "process.env.SENTRY_DSN": JSON.stringify(process.env.SENTRY_DSN),
+          "process.env.SENTRY_RELEASE": JSON.stringify(getSentryRelease(env)),
         }),
         new SentryWebpackPlugin({
           // see https://docs.sentry.io/product/cli/configuration/ for details
