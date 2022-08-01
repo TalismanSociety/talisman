429dde054296046c75e50e9c7bc5b46f7ea5812d
Chid Gilovitz <chid@talisman.xyz>
2022-08-01 15:05:21 +0800

    fix: bug where env.build is not a string (#130)
    * fix: bug where env.build is not a string
    
    * fix: bug where result of getSentryRelease is not a string
    
    * fix: update webpack config to avoid use of DotEnv plugin
    
    * fix: abcd ocd
    
    * fix: ensure process.env.SENTRY_RELEASE always has a value
    
    * fix: more env vars
    
    * fix: process.env related errors in webpack build
    
    * fix: only pass through process.env.PASSWORD and process.env.TEST_MNEMONIC in development builds
    
    Co-authored-by: alecdwm <alec@owls.io>

--- apps/extension/webpack/webpack.common.js
+++ apps/extension/webpack/webpack.common.js
@@ -1,114 +1,130 @@
-/* eslint-env es6 */
+/* eslint-env es2021 */
+
+require("dotenv").config()
+
 const webpack = require("webpack")
 const path = require("path")
 const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin")
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
 const ForkTsCheckerNotifierWebpackPlugin = require("fork-ts-checker-notifier-webpack-plugin")
 const EslintWebpackPlugin = require("eslint-webpack-plugin")
-const DotEnv = require("dotenv-webpack")
-const getGitShortHash = require("./utils").getGitShortHash
-require("dotenv").config()
-const srcDir = path.join(__dirname, "..", "src")
-const coreDir = path.join(srcDir, "core")
-const distDir = path.join(__dirname, "..", "dist")
-
 const createStyledComponentsTransformer = require("typescript-plugin-styled-components").default
 const styledComponentsTransformer = createStyledComponentsTransformer()
 
-module.exports = (env) => {
-  return {
-    entry: {
-      substrate: ["@substrate/txwrapper-polkadot"],
-      popup: { import: path.join(srcDir, "index.popup.tsx") },
-      onboarding: { import: path.join(srcDir, "index.onboarding.tsx") },
-      dashboard: { import: path.join(srcDir, "index.dashboard.tsx") },
-      background: { import: path.join(coreDir, "background.ts"), dependOn: "substrate" },
-      content_script: path.join(coreDir, "content_script.ts"),
-      page: path.join(coreDir, "page.ts"),
-    },
-    output: {
-      path: distDir,
-      filename: "[name].js",
-    },
+const { srcDir, coreDir, distDir, getRelease, getGitShortHash } = require("./utils")
 
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: {
-            loader: "ts-loader",
+const config = (env) => ({
+  entry: {
+    substrate: ["@substrate/txwrapper-polkadot"],
+    popup: { import: path.join(srcDir, "index.popup.tsx") },
+    onboarding: { import: path.join(srcDir, "index.onboarding.tsx") },
+    dashboard: { import: path.join(srcDir, "index.dashboard.tsx") },
+    background: { import: path.join(coreDir, "background.ts"), dependOn: "substrate" },
+    content_script: path.join(coreDir, "content_script.ts"),
+    page: path.join(coreDir, "page.ts"),
+  },
+  output: {
+    path: distDir,
+    filename: "[name].js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            getCustomTransformers: () => ({ before: [styledComponentsTransformer] }),
+            // disable type checker - we will use it in fork plugin
+            transpileOnly: true,
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: [
+          {
+            loader: "@svgr/webpack",
             options: {
-              getCustomTransformers: () => ({ before: [styledComponentsTransformer] }),
-              // disable type checker - we will use it in fork plugin
-              transpileOnly: true,
+              exportType: "named",
+              dimensions: false,
             },
           },
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.svg$/i,
-          issuer: /\.[jt]sx?$/,
-          use: [
-            {
-              loader: "@svgr/webpack",
-              options: {
-                exportType: "named",
-                dimensions: false,
-              },
-            },
-            {
-              loader: "url-loader",
-            },
-          ],
-        },
-        {
-          test: /\.(png|jpg|gif)$/i,
-          dependency: { not: ["url"] },
-          use: [
-            {
-              loader: "url-loader",
-              options: {
-                limit: 8192,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"],
-        },
-      ],
-    },
-    resolve: {
-      alias: {
-        "@talisman": path.resolve(srcDir, "@talisman/"),
-        "@core": path.resolve(srcDir, "core/"),
-        "@ui": path.resolve(srcDir, "ui/"),
-        //"styled-components": path.resolve(__dirname, "../../..", "node_modules", "styled-components"), // prevents duplication of styled components https://styled-components.com/docs/faqs#why-am-i-getting-a-warning-about-several-instances-of-module-on-the-page
-        // https://github.com/facebook/react/issues/20235
-        // fix for @polkadot/react-identicons which uses react 16
-        "react/jsx-runtime": path.resolve("../../node_modules/react/jsx-runtime.js"),
+          {
+            loader: "url-loader",
+          },
+        ],
       },
-      extensions: [".ts", ".tsx", ".js", ".css"],
-      fallback: {
-        stream: false,
-        assert: require.resolve("assert"),
+      {
+        test: /\.(png|jpg|gif)$/i,
+        dependency: { not: ["url"] },
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
       },
-    },
-    plugins: [
-      new DotEnv(),
-      new webpack.DefinePlugin({
-        "process.env.BUILD": env.build,
-        "process.env.COMMIT_SHA_SHORT": JSON.stringify(getGitShortHash()),
-        "process.env.VERSION": JSON.stringify(process.env.npm_package_version),
-      }),
-      new CaseSensitivePathsPlugin(),
-      new ForkTsCheckerWebpackPlugin(),
-      new ForkTsCheckerNotifierWebpackPlugin({ title: "TypeScript", excludeWarnings: false }),
-      new EslintWebpackPlugin({ context: "../", extensions: ["ts", "tsx"] }),
-      new webpack.ProvidePlugin({
-        Buffer: ["buffer", "Buffer"],
-      }),
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-  }
-}
+  },
+  resolve: {
+    alias: {
+      "@talisman": path.resolve(srcDir, "@talisman/"),
+      "@core": path.resolve(srcDir, "core/"),
+      "@ui": path.resolve(srcDir, "ui/"),
+      //"styled-components": path.resolve(__dirname, "../../..", "node_modules", "styled-components"), // prevents duplication of styled components https://styled-components.com/docs/faqs#why-am-i-getting-a-warning-about-several-instances-of-module-on-the-page
+      // https://github.com/facebook/react/issues/20235
+      // fix for @polkadot/react-identicons which uses react 16
+      "react/jsx-runtime": path.resolve("../../node_modules/react/jsx-runtime.js"),
+    },
+    extensions: [".ts", ".tsx", ".js", ".css"],
+    fallback: {
+      stream: false,
+      assert: require.resolve("assert"),
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      // passthroughs from the environment
+      "process.env.EXTENSION_PREFIX": JSON.stringify(process.env.EXTENSION_PREFIX || ""),
+      "process.env.NODE_DEBUG": JSON.stringify(process.env.NODE_DEBUG || ""),
+      "process.env.POSTHOG_AUTH_TOKEN": JSON.stringify(process.env.POSTHOG_AUTH_TOKEN || ""),
+      "process.env.SENTRY_AUTH_TOKEN": JSON.stringify(process.env.SENTRY_AUTH_TOKEN || ""),
+      "process.env.SENTRY_DSN": JSON.stringify(process.env.SENTRY_DSN || ""),
+
+      // dev-only passthroughs from the environment
+      "process.env.PASSWORD": JSON.stringify(
+        // only pass through when env.build is undefined (running a development build)
+        env.build === undefined ? process.env.PASSWORD || "" : ""
+      ),
+      "process.env.TEST_MNEMONIC": JSON.stringify(
+        // only pass through when env.build is undefined (running a development build)
+        env.build === undefined ? process.env.TEST_MNEMONIC || "" : ""
+      ),
+
+      // computed values
+      "process.env.BUILD": JSON.stringify(env.build),
+      "process.env.COMMIT_SHA_SHORT": JSON.stringify(getGitShortHash()),
+      "process.env.RELEASE": JSON.stringify(getRelease(env)),
+      "process.env.VERSION": JSON.stringify(process.env.npm_package_version),
+    }),
+    new CaseSensitivePathsPlugin(),
+    new ForkTsCheckerWebpackPlugin(),
+    new ForkTsCheckerNotifierWebpackPlugin({ title: "TypeScript", excludeWarnings: false }),
+    new EslintWebpackPlugin({ context: "../", extensions: ["ts", "tsx"] }),
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ],
+})
+
+module.exports = config
