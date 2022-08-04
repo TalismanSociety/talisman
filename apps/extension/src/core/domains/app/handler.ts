556668ac8a4b22fea47597e589f969e978f2bede
Chid Gilovitz <chid@talisman.xyz>
2022-08-04 16:58:23 +0800

    fix: pairs are now locked when extension is locked

--- apps/extension/src/core/domains/app/handler.ts
+++ apps/extension/src/core/domains/app/handler.ts
@@ -125,6 +125,8 @@ export default class AppHandler extends ExtensionHandler {
 
   private lock(): LoggedinType {
     this.stores.password.clearPassword()
+    const pairs = keyring.getPairs()
+    pairs.forEach((pair) => pair.lock())
     return this.authStatus()
   }
 
