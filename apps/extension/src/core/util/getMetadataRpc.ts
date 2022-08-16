20e3c9c20d5c5e21f34784d9bfcb003ec3239030
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-08-16 12:06:21 +0900

    fix: use metadata from store if connection issue

--- apps/extension/src/core/util/getMetadataRpc.ts
+++ apps/extension/src/core/util/getMetadataRpc.ts
@@ -1,5 +1,7 @@
 import { db } from "@core/libs/db"
 import RpcFactory from "@core/libs/RpcFactory"
+import * as Sentry from "@sentry/browser"
+
 import { getRuntimeVersion } from "./getRuntimeVersion"
 
 // key = `${chainId}-${specVersion}`
@@ -7,30 +9,41 @@ import { getRuntimeVersion } from "./getRuntimeVersion"
 const metadataRpcCache: Record<string, `0x${string}`> = {}
 
 export const getMetadataRpc = async (chainId: string, blockHash?: string) => {
-  // identify associated specVersion
-  const runtimeVersion = await getRuntimeVersion(chainId, blockHash)
-  const specVersion = Number(runtimeVersion.specVersion)
-  const cacheKey = `${chainId}-${specVersion}`
-
-  // check if it's either in the store or in the cache
   const currentMetadataRpc = await db.metadataRpc.get(chainId)
 
-  if (currentMetadataRpc?.specVersion === specVersion) return currentMetadataRpc.metadataRpc
-  if (metadataRpcCache[cacheKey]) return metadataRpcCache[cacheKey]
+  try {
+    // identify associated specVersion
+    const runtimeVersion = await getRuntimeVersion(chainId, blockHash)
+    const specVersion = Number(runtimeVersion.specVersion)
+    const cacheKey = `${chainId}-${specVersion}`
 
-  // pull from blockchain
-  const metadataRpc = await RpcFactory.send<`0x${string}`>(chainId, "state_getMetadata", [
-    blockHash,
-  ])
+    // check if current cached metadata matches current specVersion
+    if (currentMetadataRpc?.specVersion === specVersion) return currentMetadataRpc.metadataRpc
+    if (metadataRpcCache[cacheKey]) return metadataRpcCache[cacheKey]
 
-  // persist either in store (if latest) or in memory cache
-  if (!currentMetadataRpc || specVersion > currentMetadataRpc.specVersion)
-    await db.metadataRpc.put({
-      chainId,
-      specVersion,
-      metadataRpc,
-    })
-  else metadataRpcCache[cacheKey] = metadataRpc
+    // pull from blockchain
+    const metadataRpc = await RpcFactory.send<`0x${string}`>(chainId, "state_getMetadata", [
+      blockHash,
+    ])
 
-  return metadataRpc
+    // persist either in store (if latest) or in memory cache
+    if (!currentMetadataRpc || specVersion > currentMetadataRpc.specVersion)
+      await db.metadataRpc.put({
+        chainId,
+        specVersion,
+        metadataRpc,
+      })
+    else metadataRpcCache[cacheKey] = metadataRpc
+
+    return metadataRpc
+  } catch (err) {
+    Sentry.captureException(err, { tags: { chainId } })
+
+    // We may end up here if there is no healthy RPC when signing a transaction.
+    // if anything goes wrong, and if we have one, return the latest metadataRpc from the store
+    // In most case, the latest metadataRpc from our store will do the work.
+    if (currentMetadataRpc?.metadataRpc) return currentMetadataRpc?.metadataRpc
+
+    throw new Error("Failed to fetch metadata for chain " + chainId)
+  }
 }
