f0a5c687f81599aa62f221fed1dd0ae690eae65c
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-08-22 10:53:20 +0900

    fix: prevent adding erc20 duplicates (#171)
    * fix: existing token check for wallet_watchAsset
    
    * fix: prevent manual add erc20 duplicate
    
    * refactor: getErc20TokenId helper method

--- apps/extension/src/core/domains/ethereum/helpers.ts
+++ apps/extension/src/core/domains/ethereum/helpers.ts
@@ -1,1 +1,4 @@
 export const getEthDerivationPath = (index = 0) => `/m/44'/60'/0'/0/${index}`
+
+export const getErc20TokenId = (chainOrNetworkId: number | string, contractAddress: string) =>
+  `${chainOrNetworkId}-erc20-${contractAddress}`.toLowerCase()
