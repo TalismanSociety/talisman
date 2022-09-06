69675a3ea2d4677483a9e839bb692b6089c40663
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-09-06 14:12:06 +0900

    chore: cleanup dead code

--- apps/extension/src/@talisman/components/Field/index.ts
+++ apps/extension/src/@talisman/components/Field/index.ts
@@ -1,22 +1,14 @@
 import Checkbox from "./Checkbox"
-import FileDrop from "./FileDrop"
-import Input from "./Input"
-import InputAutoWidth from "./InputAutoWidth"
-import Number from "./Number"
-import Select from "./Select"
 import Textarea from "./Textarea"
 import Toggle from "./Toggle"
+
+// Legacy components barrel
 
 // @ts-ignore
 const Field = () => {}
 
-Field.Input = Input
-Field.InputAutoWidth = InputAutoWidth
 Field.Textarea = Textarea
-Field.FileDrop = FileDrop
 Field.Toggle = Toggle
 Field.Checkbox = Checkbox
-Field.Number = Number
-Field.Select = Select
 
 export default Field
