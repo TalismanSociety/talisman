5da6cb9d64b61dffdffe85828add04acdbceba48
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-09-07 17:22:19 +0900

    fix: performance

--- apps/extension/src/ui/apps/onboard/components/OnboardStyles.ts
+++ apps/extension/src/ui/apps/onboard/components/OnboardStyles.ts
@@ -1,6 +1,8 @@
 import { css } from "styled-components"
 
 export const styleOnboardTranslucidBackground = css`
+  backface-visibility: hidden;
+  perspective: 1000;
   background: rgba(var(--color-foreground-raw), 0.05);
   backdrop-filter: blur(9.6rem);
   transform: translate3d(0, 0, 0); // prevents flickering (enables GPU rendering)
