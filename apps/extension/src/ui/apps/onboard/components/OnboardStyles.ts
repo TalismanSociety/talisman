07a6b9f805664a38d5f062a28d090a70aefe0f4f
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-09-07 17:20:47 +0900

    fix: performance

--- apps/extension/src/ui/apps/onboard/components/OnboardStyles.ts
+++ apps/extension/src/ui/apps/onboard/components/OnboardStyles.ts
@@ -4,4 +4,5 @@ export const styleOnboardTranslucidBackground = css`
   background: rgba(var(--color-foreground-raw), 0.05);
   backdrop-filter: blur(9.6rem);
   transform: translate3d(0, 0, 0); // prevents flickering (enables GPU rendering)
+  transform: translateZ(0);
 `
