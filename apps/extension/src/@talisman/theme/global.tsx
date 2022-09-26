5c1698f6ce59200a34b7fa0bbb5005858017006e
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-09-26 10:47:43 +0900

    fix: placeholders

--- apps/extension/src/@talisman/theme/global.tsx
+++ apps/extension/src/@talisman/theme/global.tsx
@@ -157,9 +157,9 @@ const Global = createGlobalStyle`
       outline: none;
   }
 
-  ::placeholder {
+  /* ::placeholder {
     color: rgba(${({ theme }) => theme?.foreground}, 0.2);
-  }
+  } */
 
   .muted{
     font-size: 0.8em;
