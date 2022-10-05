b6530b3b5661644367771a72ca2863f92b9cbc8a
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-10-05 12:58:39 +0900

    feat: tooltip A/B testing

--- apps/extension/src/ui/apps/popup/components/Navigation/BottomNav.tsx
+++ apps/extension/src/ui/apps/popup/components/Navigation/BottomNav.tsx
@@ -1,7 +1,9 @@
+import { WithTooltip } from "@talisman/components/Tooltip"
 import { classNames } from "@talisman/util/classNames"
 import { api } from "@ui/api"
 import { useSelectedAccount } from "@ui/domains/Portfolio/SelectedAccountContext"
-import { ButtonHTMLAttributes, DetailedHTMLProps, FC, useCallback, useMemo } from "react"
+import { useFeatureVariantEquals } from "@ui/hooks/useFeatures"
+import { ButtonHTMLAttributes, DetailedHTMLProps, FC, useCallback } from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 import { useNavigationContext } from "../../context/NavigationContext"
 import { NavIconExpand, NavIconHome, NavIconMore, NavIconNft } from "./icons"
@@ -49,20 +51,30 @@ export const BottomNav = () => {
     api.dashboardOpen(`${location.pathname}${qs}`)
   }, [account, location.pathname])
 
+  const showTooltip = useFeatureVariantEquals("POPUP_BOTTOM_NAV_VARIANT", "WITH_TOOLTIP")
+
   return (
     <div className="border-t-grey-800 flex h-32 min-h-[6.4rem] justify-between border-t px-12 text-3xl">
-      <BottomNavButton onClick={handleHomeClick} current={location.pathname === "/portfolio"}>
-        <NavIconHome />
-      </BottomNavButton>
-      <BottomNavButton onClick={handleNftClick}>
-        <NavIconNft />
-      </BottomNavButton>
-      <BottomNavButton onClick={handleExpandClick}>
-        <NavIconExpand />
-      </BottomNavButton>
-      <BottomNavButton onClick={open}>
-        <NavIconMore />
-      </BottomNavButton>
+      <WithTooltip as="div" tooltip={showTooltip && "Portfolio"}>
+        <BottomNavButton onClick={handleHomeClick} current={location.pathname === "/portfolio"}>
+          <NavIconHome />
+        </BottomNavButton>
+      </WithTooltip>
+      <WithTooltip as="div" tooltip={showTooltip && "View NFTs"}>
+        <BottomNavButton onClick={handleNftClick}>
+          <NavIconNft />
+        </BottomNavButton>
+      </WithTooltip>
+      <WithTooltip as="div" tooltip={showTooltip && "Expand Portfolio View"}>
+        <BottomNavButton onClick={handleExpandClick}>
+          <NavIconExpand />
+        </BottomNavButton>
+      </WithTooltip>
+      <WithTooltip as="div" tooltip={showTooltip && "More Options"}>
+        <BottomNavButton onClick={open}>
+          <NavIconMore />
+        </BottomNavButton>
+      </WithTooltip>
     </div>
   )
 }
