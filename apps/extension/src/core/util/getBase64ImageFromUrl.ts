b95d2596ed2b04375caad22acddfd9ed681802ea
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-10-06 12:59:47 +0900

    fix: catch CORS error when fetching ERC20 token logo (#251)
    * fix: catch CORS error
    
    * fix: log error in debug mode

--- apps/extension/src/core/util/getBase64ImageFromUrl.ts
+++ apps/extension/src/core/util/getBase64ImageFromUrl.ts
@@ -1,19 +1,28 @@
-export const getBase64ImageFromUrl = async (url: string) => {
-  const response = await fetch(url)
-  const blob = await response.blob()
+import { DEBUG } from "@core/constants"
 
-  return new Promise<string>((resolve, reject) => {
-    const reader = new FileReader()
-    reader.onloadend = function () {
-      try {
-        if (!reader.result) throw new Error("No result")
-        const base64data = reader.result.toString()
-        resolve(base64data)
-      } catch (err) {
-        reject(err)
+export const getBase64ImageFromUrl = async (url: string) => {
+  try {
+    const response = await fetch(url)
+    const blob = await response.blob()
+
+    return new Promise<string>((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onloadend = function () {
+        try {
+          if (!reader.result) throw new Error("No result")
+          const base64data = reader.result.toString()
+          resolve(base64data)
+        } catch (err) {
+          reject(err)
+        }
       }
-    }
-    reader.onerror = reject
-    reader.readAsDataURL(blob)
-  })
+      reader.onerror = reject
+      reader.readAsDataURL(blob)
+    })
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    if (DEBUG) console.error(err)
+    // can happen if image doesn't exist or if browser runs into a CORS issue
+    return undefined
+  }
 }
