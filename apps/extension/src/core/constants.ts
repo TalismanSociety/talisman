3c3abaf43dc11d2888e7c8485d84fae164116787
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-10-29 23:39:14 +0900

    feat: default ETH in send funds for EVM account

--- apps/extension/src/core/constants.ts
+++ apps/extension/src/core/constants.ts
@@ -4,4 +4,4 @@ export const DEBUG = process.env.BUILD !== "production"
 export const DEFAULT_ETH_CHAIN_ID = 1 //Ethereum mainnet
 
 export const DEFAULT_SEND_FUNDS_TOKEN_SUBSTRATE = "polkadot-native-dot-polkadot"
-export const DEFAULT_SEND_FUNDS_TOKEN_ETHEREUM = "moonbeam-native-glmr-1284"
+export const DEFAULT_SEND_FUNDS_TOKEN_ETHEREUM = "1-native-eth-1"
