066a74d646df5dab39019391ff7d8144496d31f1
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-10-29 23:18:41 +0900

    feat: use ethereum mainnet as default network

--- apps/extension/src/core/constants.ts
+++ apps/extension/src/core/constants.ts
@@ -1,7 +1,7 @@
 export const PORT_EXTENSION = "talisman-extension"
 export const PORT_CONTENT = "talisman-content"
 export const DEBUG = process.env.BUILD !== "production"
-export const DEFAULT_ETH_CHAIN_ID = 1284 //Moonbeam
+export const DEFAULT_ETH_CHAIN_ID = 1 //Ethereum mainnet
 
 export const DEFAULT_SEND_FUNDS_TOKEN_SUBSTRATE = "polkadot-native-dot-polkadot"
 export const DEFAULT_SEND_FUNDS_TOKEN_ETHEREUM = "moonbeam-native-glmr-1284"
