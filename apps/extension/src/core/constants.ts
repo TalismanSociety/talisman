349a74508d9c67f37f3f05122744da372e3c027b
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-10-31 13:48:32 +0900

    feat: use ETH as default EVM token (#278)
    * feat: use ethereum mainnet as default network
    
    * feat: default ETH in send funds for EVM account
    
    * feat: display ETH balance even if 0
    
    * chore: cleanup

--- apps/extension/src/core/constants.ts
+++ apps/extension/src/core/constants.ts
@@ -1,7 +1,18 @@
 export const PORT_EXTENSION = "talisman-extension"
 export const PORT_CONTENT = "talisman-content"
 export const DEBUG = process.env.BUILD !== "production"
-export const DEFAULT_ETH_CHAIN_ID = 1284 //Moonbeam
+export const DEFAULT_ETH_CHAIN_ID = 1 //Ethereum mainnet
 
+export const DEFAULT_PORTFOLIO_TOKENS_SUBSTRATE = [
+  "polkadot-native-dot-polkadot",
+  "kusama-native-ksm",
+]
+export const DEFAULT_PORTFOLIO_TOKENS_ETHEREUM = [
+  "1-native-eth",
+  "moonbeam-native-glmr",
+  "moonriver-native-movr",
+]
+
+// those are suffixed by chainId or networkId for dedupping chains
 export const DEFAULT_SEND_FUNDS_TOKEN_SUBSTRATE = "polkadot-native-dot-polkadot"
-export const DEFAULT_SEND_FUNDS_TOKEN_ETHEREUM = "moonbeam-native-glmr-1284"
+export const DEFAULT_SEND_FUNDS_TOKEN_ETHEREUM = "1-native-eth-1"
