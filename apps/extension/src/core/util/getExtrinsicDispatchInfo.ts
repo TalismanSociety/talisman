1b671ea8f1eb137c4ffbe0b36b108eb88318b538
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-11-14 19:01:07 +0900

    feat: feature flag (and refactoring)

--- apps/extension/src/core/util/getExtrinsicDispatchInfo.ts
+++ apps/extension/src/core/util/getExtrinsicDispatchInfo.ts
@@ -1,74 +1,40 @@
-import { assert, BN_HUNDRED, hexAddPrefix, hexStripPrefix, u8aConcatStrict } from "@polkadot/util"
+import { assert } from "@polkadot/util"
 import { GenericExtrinsic } from "@polkadot/types"
-//import { DetectCodec } from "@polkadot/types/detect"
 import { HexString } from "@polkadot/util/types"
 import RpcFactory from "@core/libs/RpcFactory"
-import { Codec } from "@polkadot/types-codec/types"
+import { featuresStore } from "@core/domains/app/store.features"
+import { stateCall } from "./stateCall"
 
-const USE_WEIGHT_V2 = true
-
-type IRuntimeDispatchInfo = {
-  partialFee: string
-  estimatedFee: string
-}
-
-const stateCall = async <T extends Codec = Codec, K extends string = string>(
-  chainId: string,
-  method: string,
-  resultType: K,
-  args: Codec[],
-  blockHash?: HexString
-) => {
-  // assume that there are always arguments
-  const registry = args[0].registry
-
-  const bytes = registry.createType("Raw", u8aConcatStrict(args.map((arg) => arg.toU8a())))
-
-  const result = await RpcFactory.send(chainId, "state_call", [method, bytes.toHex(), blockHash])
-
-  return registry.createType<T, K>(resultType, result)
+// this type structure is compatible with V1 result object
+type ExtrinsicDispatchInfo = {
+  partialFee: string // planck
 }
 
 export const getExtrinsicDispatchInfo = async (
   chainId: string,
   signedExtrinsic: GenericExtrinsic,
   blockHash?: HexString
-): Promise<IRuntimeDispatchInfo> => {
+): Promise<ExtrinsicDispatchInfo> => {
   assert(signedExtrinsic.isSigned, "Extrinsic must be signed (or fakeSigned) in order to query fee")
 
-  if (USE_WEIGHT_V2) {
-    const u8a = signedExtrinsic.toU8a()
-    const len = signedExtrinsic.registry.createType("u32", u8a.length)
+  if (await featuresStore.isFeatureEnabled("FEE_FROM_STATE_CALL")) {
+    // signedExtrinsic.encodedLength : same as payment_queryInfo RPC call
+    // signedExtrinsic.encodedLength + 1 : exact same as pjs's apps and subscan estimate fee. why ?
+    const len = signedExtrinsic.registry.createType("u32", signedExtrinsic.encodedLength)
 
     const dispatchInfo = await stateCall(
       chainId,
       "TransactionPaymentApi_query_info",
       "RuntimeDispatchInfo",
-      [signedExtrinsic, len]
+      [signedExtrinsic, len],
+      blockHash
     )
 
-    // const bytes = signedExtrinsic.registry.createType(
-    //   "Raw",
-    //   u8aConcatStrict([signedExtrinsic.toU8a(), len.toU8a()])
-    // )
-
-    // const queryInfoRaw = await RpcFactory.send(chainId, "state_call", [
-    //   "TransactionPaymentApi_query_info",
-    //   bytes.toHex(),
-    //   blockHash,
-    // ])
-
-    // const decoded = signedExtrinsic.registry.createType("RuntimeDispatchInfo", queryInfoRaw)
-    // // console.log({ decoded, human: decoded.toHuman() })
-    // console.log({})
-
-    const res = {
+    return {
       partialFee: dispatchInfo.partialFee.toString(),
-      estimatedFee: dispatchInfo.partialFee.muln(110).div(BN_HUNDRED).toString(),
     }
-    // console.log({ res })
-    return res
   } else {
+    // Legacy approach, returns non encoded object
     return RpcFactory.send(chainId, "payment_queryInfo", [signedExtrinsic.toHex(), blockHash])
   }
 }
---
+++ apps/extension/src/core/util/stateCall.ts
@@ -0,0 +1,21 @@
+import { u8aConcatStrict } from "@polkadot/util"
+import { HexString } from "@polkadot/util/types"
+import { Codec } from "@polkadot/types-codec/types"
+import RpcFactory from "@core/libs/RpcFactory"
+
+export const stateCall = async <T extends Codec = Codec, K extends string = string>(
+  chainId: string,
+  method: string,
+  resultType: K,
+  args: Codec[],
+  blockHash?: HexString
+) => {
+  // on a state call there are always arguments
+  const registry = args[0].registry
+
+  const bytes = registry.createType("Raw", u8aConcatStrict(args.map((arg) => arg.toU8a())))
+
+  const result = await RpcFactory.send(chainId, "state_callAt", [method, bytes.toHex(), blockHash])
+
+  return registry.createType<T, K>(resultType, result)
+}
