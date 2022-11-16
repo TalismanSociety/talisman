31de501023cc0de5978b37c70acc7dc0a2736ad7
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-11-16 09:54:18 +0900

    chore: cleanup

--- apps/extension/src/core/util/getTransactionDetails.ts
+++ apps/extension/src/core/util/getTransactionDetails.ts
@@ -5,26 +5,12 @@ import {
   TransactionPayload,
 } from "@core/domains/signing/types"
 import { db } from "@core/libs/db"
-import RpcFactory from "@core/libs/RpcFactory"
 import { log } from "@core/log"
 import { assert, hexToNumber } from "@polkadot/util"
 import * as Sentry from "@sentry/browser"
 import { getExtrinsicDispatchInfo } from "./getExtrinsicDispatchInfo"
 import { getRuntimeVersion } from "./getRuntimeVersion"
 import { getTypeRegistry } from "./getTypeRegistry"
-
-import { sleep } from "./sleep"
-
-const tryRpcSend = async (chainId: string, method: string, params: unknown[], attempts: number) => {
-  for (let i = 1; i <= attempts; i++) {
-    try {
-      return await RpcFactory.send(chainId, method, params)
-    } catch (err) {
-      if (i === attempts) throw err
-      await sleep(300)
-    }
-  }
-}
 
 export const getTransactionDetails = async (payload: SignerPayloadJSON) => {
   const {
@@ -72,14 +58,6 @@ export const getTransactionDetails = async (payload: SignerPayloadJSON) => {
 
       // fake sign it so fees can be queried
       extrinsic.signFake(address, { nonce, blockHash, genesisHash, runtimeVersion })
-
-      // estimate fees (attempt 3 times)
-      const payment = await tryRpcSend(
-        chain.id,
-        "payment_queryInfo",
-        [extrinsic.toHex(), blockHash],
-        3
-      )
 
       const { partialFee } = await getExtrinsicDispatchInfo(chain.id, extrinsic)
 
