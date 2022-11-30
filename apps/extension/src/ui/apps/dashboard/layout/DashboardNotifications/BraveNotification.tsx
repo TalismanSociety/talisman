7ff1299120701db2bf5262d046c0adfb7a34ebc9
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-11-30 14:26:30 +0900

    fix: no more brave warning modal after onboarding

--- apps/extension/src/ui/apps/dashboard/layout/DashboardNotifications/BraveNotification.tsx
+++ apps/extension/src/ui/apps/dashboard/layout/DashboardNotifications/BraveNotification.tsx
@@ -5,32 +5,17 @@ import { useIsBrave } from "@talisman/hooks/useIsBrave"
 import { useOpenClose } from "@talisman/hooks/useOpenClose"
 import { BraveIcon } from "@talisman/theme/icons"
 import { BraveWarningModal } from "@ui/domains/Settings/BraveWarning/BraveWarningModal"
+import { useAppState } from "@ui/hooks/useAppState"
 import { useCallback, useEffect, useMemo, useState } from "react"
 
 import { Notification } from "./Notification"
 
 export const BraveWarningNotification = () => {
   const isBrave = useIsBrave()
-  // TODO use useAppState instead
-  const [hideBraveWarning, setHideBraveWarning] = useState<boolean | undefined>(true)
-  const [hasBraveWarningBeenShown, setHasBraveWarningBeenShown] = useState<boolean | undefined>(
-    true
-  )
+  const { hideBraveWarning } = useAppState()
   const { isOpen, close, open } = useOpenClose()
 
-  useEffect(() => {
-    const sub = appStore.observable.subscribe((settings) => {
-      setHideBraveWarning(settings.hideBraveWarning)
-      setHasBraveWarningBeenShown(settings.hasBraveWarningBeenShown)
-    })
-    return () => sub.unsubscribe()
-  }, [])
-
   const showWarning = useMemo(() => isBrave && !hideBraveWarning, [hideBraveWarning, isBrave])
-
-  useEffect(() => {
-    if (showWarning && !hasBraveWarningBeenShown) open()
-  }, [hasBraveWarningBeenShown, open, showWarning])
 
   const handleHide = useCallback(() => {
     appStore.set({ hideBraveWarning: true })
