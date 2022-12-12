96ba729bd7feda91f1fecea33c13ce8f209b3944
Chid Gilovitz <chid@talisman.xyz>
2022-12-12 15:41:17 +0800

    fix: remove hardcoded test data

--- apps/extension/src/core/domains/app/store.addressBook.ts
+++ apps/extension/src/core/domains/app/store.addressBook.ts
@@ -10,15 +10,4 @@ export type AddressBookContact = {
 
 type AddressBookData = Record<Address, AddressBookContact>
 
-export const addressBookStore = new StorageProvider<AddressBookData>("addressBook", {
-  "13RDY9nrJpyTDBSUdBw12dGwhk19sGwsrVZ2bxkzYHBSagP2": {
-    address: "13RDY9nrJpyTDBSUdBw12dGwhk19sGwsrVZ2bxkzYHBSagP2",
-    addressType: "ss58",
-    name: "Gav's Polkadot Address",
-  },
-  "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B": {
-    address: "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B",
-    addressType: "ethereum",
-    name: "Vitalik's Eth Address",
-  },
-})
+export const addressBookStore = new StorageProvider<AddressBookData>("addressBook")
