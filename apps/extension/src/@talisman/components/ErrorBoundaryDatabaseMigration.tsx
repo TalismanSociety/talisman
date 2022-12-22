4a3ca3e6e107b6227b99e1f69cd7916dd5e03bcb
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-12-22 20:40:58 +0900

    feat: design for update page

--- apps/extension/src/@talisman/components/ErrorBoundaryDatabaseMigration.tsx
+++ apps/extension/src/@talisman/components/ErrorBoundaryDatabaseMigration.tsx
@@ -1,69 +1,24 @@
 import { MIGRATION_ERROR_MSG } from "@core/db"
-import SpinningHand from "@talisman/theme/images/hand_open_spin_animated_dark.gif"
 import { Component, ErrorInfo, FC, ReactNode } from "react"
-import styled from "styled-components"
-
-const ErrorContainer = styled.section`
-  display: flex;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-  min-width: 36rem;
-  min-height: 48rem;
-  justify-content: center;
-  color: var(--color-mid);
-  text-align: center;
-
-  section {
-    padding: 2rem;
-    width: 36rem;
-    height: 48rem;
-    display: flex;
-    flex-direction: column;
-
-    .content {
-      flex-grow: 1;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-
-      h1 {
-        font-size: 3.2rem;
-        font-weight: var(--font-weight-bold);
-        margin: 0;
-      }
-      img {
-        width: 20rem;
-      }
-      p {
-        font-size: 2rem;
-        margin-top: 2rem;
-        margin-bottom: 1.2rem;
-      }
-      p.balances-warning {
-        font-size: 1.4rem;
-      }
-    }
-  }
-`
+import { ProcessAnimation } from "talisman-ui"
 
 const ErrorMessage: FC = () => (
-  <ErrorContainer>
-    <section>
-      <div className="content text-white">
-        <h1>Upgrading</h1>
-        <h1>Talisman</h1>
-        <div>
-          <img className="inline-block" src={SpinningHand} alt="Talisman Spinning Hand logo" />
-        </div>
-        <p>Please wait</p>
-        <p className="balances-warning text-body-secondary">
-          Don't be alarmed if your balances are empty on initial load after this upgrade. Talisman
-          may need to re-fetch all balances.
-        </p>
+  <div className="text-whit mx-auto flex h-[60rem] w-[40rem] flex-col py-16 px-12 text-center">
+    <div className="flex grow flex-col justify-center font-bold">
+      <div className="text-xl">Updating Talisman</div>
+      <div className="my-[4.4rem]">
+        <ProcessAnimation status="processing" className="h-[15rem]" />
       </div>
-    </section>
-  </ErrorContainer>
+      <div className="text-md">
+        Adding new and improved
+        <br />
+        balance support
+      </div>
+    </div>
+    <div className="balances-warning text-body-secondary bg-grey-900 rounded p-8 font-normal">
+      Please note your balances may take a few seconds to refresh after the upgrade
+    </div>
+  </div>
 )
 
 interface Props {
