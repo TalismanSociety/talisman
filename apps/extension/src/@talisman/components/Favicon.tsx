0a0a296c3f1c98d87f84fa58fa9b7f10163f8cb1
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-12-30 09:57:42 +0900

    fix: squished favicon in app pill

--- apps/extension/src/@talisman/components/Favicon.tsx
+++ apps/extension/src/@talisman/components/Favicon.tsx
@@ -1,5 +1,4 @@
 import { useFaviconUrl } from "@ui/hooks/useFaviconUrl"
-import React from "react"
 import styled from "styled-components"
 
 interface IProps {
@@ -23,12 +22,14 @@ const StyledFavicon = styled(Favicon)`
   border-radius: 50%;
   background: var(--color-background);
   width: 3.2rem;
+  min-width: 3.2rem;
   height: 3.2rem;
   display: block;
   position: relative;
 
   > img {
     width: 2.6rem;
+    min-width: 2.6rem;
     height: 2.6rem;
     position: absolute;
     top: 0.3rem;
@@ -39,9 +40,11 @@ const StyledFavicon = styled(Favicon)`
     large &&
     `
     width: 4rem;
+    min-width: 4rem;
     height: 4rem;
     >img{
       width: 3.4rem;
+      min-width: 3.4rem;
       height: 3.4rem;
     }
   `}
@@ -50,9 +53,11 @@ const StyledFavicon = styled(Favicon)`
     small &&
     `
     width: 2.4rem;
+    min-width: 2.4rem;
     height: 2.4rem;
     >img{
       width: 1.8rem;
+      min-width: 1.8rem;
       height: 1.8rem;
     }
   `}
