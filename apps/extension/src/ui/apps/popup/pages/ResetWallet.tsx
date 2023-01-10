b14ab01cec3ef5d5ca0c23f897a5372381f2e57d
Chid Gilovitz <chid@talisman.xyz>
2023-01-10 09:28:55 +0800

    feat: add drawer to reset wallet extension route

--- apps/extension/src/ui/apps/popup/pages/ResetWallet.tsx
+++ apps/extension/src/ui/apps/popup/pages/ResetWallet.tsx
@@ -1,18 +1,43 @@
-import { yupResolver } from "@hookform/resolvers/yup"
-import { FormField } from "@talisman/components/Field/FormField"
-import StatusIcon from "@talisman/components/StatusIcon"
+import { Drawer } from "@talisman/components/Drawer"
+import { useOpenClose } from "@talisman/hooks/useOpenClose"
+import { AlertCircleIcon } from "@talisman/theme/icons"
 import Terrarium from "@talisman/theme/images/forgot_password_terrarium.png"
-import { api } from "@ui/api"
 import { useAnalytics } from "@ui/hooks/useAnalytics"
-import { useCallback, useEffect, useRef } from "react"
-import { SubmitHandler, useForm } from "react-hook-form"
-import styled from "styled-components"
+import { useEffect } from "react"
 import { Button } from "talisman-ui"
 
-import Layout, { Content, Footer, Header } from "../Layout"
+import Layout, { Content, Footer } from "../Layout"
+
+const ConfirmDrawer = ({ isOpen }: { isOpen: boolean }) => {
+  return (
+    <Drawer open={isOpen} anchor="bottom">
+      <div className="bg-grey-800 items-center rounded-t p-8 pt-12">
+        <div className="flex flex-col gap-6 px-12 text-center">
+          <div className="text-xl">
+            <AlertCircleIcon className="text-alert-warn" />
+          </div>
+          <h2 className="font-bold text-white ">Are you sure you want to reset Talisman?</h2>
+        </div>
+
+        <p className="text-body-secondary p-4 text-center text-sm">
+          Your current wallet, accounts and assets will be erased from this wallet. You will need to
+          re-import your account via recovery phrase. Note that you may have to re-add any other
+          imported accounts such as Ledger devices.
+        </p>
+        <div className="mt-4 flex flex-col gap-4">
+          <Button fullWidth primary>
+            Continue
+          </Button>
+          <Button fullWidth>Cancel</Button>
+        </div>
+      </div>
+    </Drawer>
+  )
+}
 
 export const ResetWallet = () => {
   const { popupOpenEvent } = useAnalytics()
+  const { open, isOpen } = useOpenClose()
 
   useEffect(() => {
     popupOpenEvent("reset wallet")
@@ -25,15 +50,16 @@ export const ResetWallet = () => {
           <img src={Terrarium} />
           <p className="text-body-secondary text-center">
             To reset your wallet you'll need to re-import your account via recovery phrase and set a
-            new password. Talisman never has access to your recovery phrase or password.
+            new password.
           </p>
         </div>
       </Content>
       <Footer>
-        <Button fullWidth primary>
+        <Button fullWidth primary onClick={open}>
           Reset wallet
         </Button>
       </Footer>
+      <ConfirmDrawer isOpen={isOpen} />
     </Layout>
   )
 }
