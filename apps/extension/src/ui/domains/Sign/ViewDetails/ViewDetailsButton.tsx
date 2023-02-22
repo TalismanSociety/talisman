a9054ee8ec3e8beb063a949b47aad86a058b5b3d
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-02-22 09:26:28 +0900

    fix: alignment on the "Decoding..." message

--- apps/extension/src/ui/domains/Sign/ViewDetails/ViewDetailsButton.tsx
+++ apps/extension/src/ui/domains/Sign/ViewDetails/ViewDetailsButton.tsx
@@ -25,7 +25,7 @@ export const ViewDetailsButton: FC<ViewDetailsButtonProps> = ({
 
   if (hasClickRequest && isAnalysing)
     return (
-      <span className="text-body-disabled flex h-16 items-center text-sm">
+      <span className="text-body-disabled inline-flex h-16 items-center text-sm">
         <LoaderIcon className="text-body-secondary animate-spin-slow" />
         <span className="ml-2">Decoding....</span>
       </span>
