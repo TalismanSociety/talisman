eb940574ac0b7229f65b93f5351114b8f73f5713
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-03-07 11:57:10 +0900

    wip: pending transactions

--- apps/extension/src/ui/domains/Transactions/PendingTransactionsButton.tsx
+++ apps/extension/src/ui/domains/Transactions/PendingTransactionsButton.tsx
@@ -1,5 +1,9 @@
 import { db } from "@core/db"
-import { EvmWalletTransaction, WalletTransaction } from "@core/domains/recentTransactions/types"
+import {
+  EvmWalletTransaction,
+  SubWalletTransaction,
+  WalletTransaction,
+} from "@core/domains/recentTransactions/types"
 import { Menu } from "@headlessui/react"
 import { IconButton } from "@talisman/components/IconButton"
 import { useOpenClose } from "@talisman/hooks/useOpenClose"
@@ -18,12 +22,33 @@ import useToken from "@ui/hooks/useToken"
 import { useTokenRates } from "@ui/hooks/useTokenRates"
 import { useLiveQuery } from "dexie-react-hooks"
 import { BigNumber } from "ethers"
-import { FC, useEffect, useMemo } from "react"
+import { FC, useCallback, useEffect, useMemo, useRef, useState } from "react"
 import { Button, Drawer } from "talisman-ui"
 
 import { ChainLogo } from "../Asset/ChainLogo"
 import Fiat from "../Asset/Fiat"
 import Tokens from "../Asset/Tokens"
+
+type TransactionRowPropsEvm = {
+  tx: EvmWalletTransaction
+  enabled: boolean
+  onContextMenuOpen?: () => void
+  onContextMenuClose?: () => void
+}
+
+type TransactionRowPropsSub = {
+  tx: SubWalletTransaction
+  enabled: boolean
+  onContextMenuOpen?: () => void
+  onContextMenuClose?: () => void
+}
+
+type TransactionRowProps = {
+  tx: WalletTransaction
+  enabled: boolean
+  onContextMenuOpen?: () => void
+  onContextMenuClose?: () => void
+}
 
 const ActionButton = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -36,9 +61,24 @@ const ActionButton = ({ children }: { children: React.ReactNode }) => {
   )
 }
 
-const TransactionRowEvm: FC<{ transaction: EvmWalletTransaction }> = ({ transaction }) => {
-  const { to, value } = transaction.unsigned
-  const evmNetwork = useEvmNetwork(transaction.evmNetworkId)
+const OpenBroadcast: FC<{ open: boolean; onOpenChange: (open: boolean) => void }> = ({
+  open,
+  onOpenChange,
+}) => {
+  useEffect(() => {
+    onOpenChange(open)
+  }, [onOpenChange, open])
+  return null
+}
+
+const TransactionRowEvm: FC<TransactionRowPropsEvm> = ({
+  tx,
+  enabled,
+  onContextMenuOpen,
+  onContextMenuClose,
+}) => {
+  const { to, value } = tx.unsigned
+  const evmNetwork = useEvmNetwork(tx.evmNetworkId)
   const token = useToken(evmNetwork?.nativeToken?.id)
   const tokenRates = useTokenRates(evmNetwork?.nativeToken?.id)
 
@@ -50,11 +90,26 @@ const TransactionRowEvm: FC<{ transaction: EvmWalletTransaction }> = ({ transact
     [token, tokenRates, value]
   )
 
+  const handleOpenChange = useCallback(
+    (open: boolean) => {
+      if (open) onContextMenuOpen?.()
+      else onContextMenuClose?.()
+    },
+    [onContextMenuClose, onContextMenuOpen]
+  )
+
   return (
     <Menu>
       {({ open }) => (
-        <div className="hover:bg-grey-750 group z-0 flex h-[45px] w-full grow items-center gap-4 rounded-sm px-4">
-          <ChainLogo id={transaction.evmNetworkId} className="shrink-0 text-xl" />
+        <div
+          className={classNames(
+            " group z-0 flex h-[45px] w-full grow items-center gap-4 rounded-sm px-4",
+            open && "bg-grey-750",
+            enabled && "hover:bg-grey-750"
+          )}
+        >
+          <OpenBroadcast open={open} onOpenChange={handleOpenChange} />
+          <ChainLogo id={tx.evmNetworkId} className="shrink-0 text-xl" />
           <div className="leading-paragraph relative flex w-full grow justify-between">
             <div className="text-left">
               <div className="flex items-center gap-2 text-sm font-bold">
@@ -68,7 +123,10 @@ const TransactionRowEvm: FC<{ transaction: EvmWalletTransaction }> = ({ transact
             <div className="relative grow text-right">
               {amount && token && (
                 <div
-                  className={classNames(open ? "opacity-0" : "opacity-100 group-hover:opacity-0")}
+                  className={classNames(
+                    open ? "opacity-0" : "opacity-100",
+                    enabled && "group-hover:opacity-0"
+                  )}
                 >
                   <div className="text-sm">
                     <Tokens
@@ -87,8 +145,9 @@ const TransactionRowEvm: FC<{ transaction: EvmWalletTransaction }> = ({ transact
               )}
               <div
                 className={classNames(
-                  "absolute top-0 right-0 z-10 flex h-[36px] items-center opacity-0 ",
-                  open ? "opacity-100" : "opacity-0 group-hover:opacity-100"
+                  " absolute top-0 right-0 z-10 flex h-[36px] items-center",
+                  open ? "visible opacity-100" : "invisible opacity-0",
+                  enabled && "group-hover:visible group-hover:opacity-100"
                 )}
               >
                 <div className="relative">
@@ -101,7 +160,11 @@ const TransactionRowEvm: FC<{ transaction: EvmWalletTransaction }> = ({ transact
                   <Menu.Button className="hover:bg-grey-700 text-body-secondary hover:text-body inline-block h-[36px] w-[36px] shrink-0 rounded-sm text-center">
                     <MoreHorizontalIcon className="inline" />
                   </Menu.Button>
-                  <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <Menu.Items
+                    className={classNames(
+                      "absolute right-0 z-10 mt-2 origin-top-right divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    )}
+                  >
                     <div className="border-grey-800 flex flex-col  whitespace-nowrap rounded-sm bg-black px-2 py-3 shadow-xl">
                       <Menu.Item>
                         <button className="hover:bg-grey-800 rounded-xs h-20 p-6">
@@ -133,10 +196,10 @@ const TransactionRowEvm: FC<{ transaction: EvmWalletTransaction }> = ({ transact
   )
 }
 
-const TransactionRow: FC<{ transaction: WalletTransaction }> = ({ transaction }) => {
-  switch (transaction.networkType) {
+const TransactionRow: FC<TransactionRowProps> = ({ tx, ...props }) => {
+  switch (tx.networkType) {
     case "evm":
-      return <TransactionRowEvm transaction={transaction} />
+      return <TransactionRowEvm tx={tx} {...props} />
     default:
       return null
   }
@@ -145,10 +208,33 @@ const TransactionRow: FC<{ transaction: WalletTransaction }> = ({ transaction })
 const TransactionsList: FC<{
   transactions?: WalletTransaction[]
 }> = ({ transactions }) => {
+  // because of the context menu, we need to control at this level which row shows buttons
+  const [activeTxHash, setActiveTxHash] = useState<string>()
+
+  const handleContextMenuOpen = useCallback(
+    (hash: string) => () => {
+      if (!activeTxHash) setActiveTxHash(hash)
+    },
+    [activeTxHash]
+  )
+
+  const handleContextMenuClose = useCallback(
+    (hash: string) => () => {
+      if (hash === activeTxHash) setActiveTxHash(undefined)
+    },
+    [activeTxHash]
+  )
+
   return (
     <div className="space-y-8">
       {transactions?.map((tx) => (
-        <TransactionRow key={tx.hash} transaction={tx} />
+        <TransactionRow
+          key={tx.hash}
+          tx={tx}
+          enabled={!activeTxHash || activeTxHash === tx.hash}
+          onContextMenuOpen={handleContextMenuOpen(tx.hash)}
+          onContextMenuClose={handleContextMenuClose(tx.hash)}
+        />
       ))}
     </div>
   )
