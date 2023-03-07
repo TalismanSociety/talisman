6687cdf150504f34d646714ccb32c208c6b61202
Alec WM <alec@owls.io>
2023-03-07 11:54:43 +1100

    fix: greyscale antialiasing on macOS (#589)

--- apps/extension/postcss.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-env es6 */
-const tailwindcss = require("tailwindcss")
-const autoprefixer = require("autoprefixer")
-
-module.exports = {
-  plugins: [tailwindcss("./tailwind.config.js"), autoprefixer],
-}
