4c604bf96c2ba3cbbebf69370cff88fc1401db75
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-03-28 08:36:23 +0900

    feat: pending tx icon

--- apps/extension/src/ui/domains/Transactions/PendingTransactionsButton.tsx
+++ apps/extension/src/ui/domains/Transactions/PendingTransactionsButton.tsx
@@ -3,8 +3,18 @@ import { IconButton } from "@talisman/components/IconButton"
 import { useOpenClose } from "@talisman/hooks/useOpenClose"
 import { PendingTransactionsIcon } from "@talisman/theme/icons"
 import { useLiveQuery } from "dexie-react-hooks"
+import React, { FC } from "react"
 
 import { PendingTransactionsDrawer } from "./PendingTransactionsDrawer"
+
+// memo to prevent rerenders when balances update, which would reset animation
+const Button: FC<{ open: () => void }> = React.memo(({ open }) => {
+  return (
+    <IconButton onClick={open}>
+      <PendingTransactionsIcon />
+    </IconButton>
+  )
+})
 
 export const PendingTransactionsButton = () => {
   const transactions = useLiveQuery(() => db.transactions.toArray(), [])
@@ -14,9 +24,7 @@ export const PendingTransactionsButton = () => {
 
   return (
     <div>
-      <IconButton onClick={open} className={transactions?.length ? "visible" : "invisible"}>
-        <PendingTransactionsIcon />
-      </IconButton>
+      <Button open={open} />
       <PendingTransactionsDrawer isOpen={isOpen} onClose={close} transactions={transactions} />
     </div>
   )
