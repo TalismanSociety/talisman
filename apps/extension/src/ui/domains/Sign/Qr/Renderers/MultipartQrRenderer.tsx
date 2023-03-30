7feae414160dd9f22b977be81cfdd28dadf5f430
alecdwm <alec@owls.io>
2023-03-30 03:12:06 +0000

    fix: consistent animation speed for multipart qr codes

--- apps/extension/src/ui/domains/Sign/Qr/Renderers/MultipartQrRenderer.tsx
+++ apps/extension/src/ui/domains/Sign/Qr/Renderers/MultipartQrRenderer.tsx
@@ -1,29 +1,38 @@
 import { decodeString } from "@polkadot/react-qr/util"
 import { u8aConcat } from "@polkadot/util"
 import QrCodeStyling from "@solana/qr-code-styling"
-import { useEffect, useState } from "react"
+import { useEffect, useReducer, useRef, useState } from "react"
 
 import { FRAME_SIZE, talismanRedHandSvg } from "../constants"
 
 export const MultipartQrRenderer = ({ data }: { data?: Uint8Array }) => {
-  const [qrCodeFrames, setQrCodeFrames] = useState<Array<string | null> | null>(null)
+  const qrCodeFrames = useRef<Array<string | null> | null>(null)
+
+  const [animGeneration, resetQrAnimation] = useReducer((x) => (x + 1) % Number.MAX_SAFE_INTEGER, 0)
+  const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
     if (!data) return
-
-    const MULTIPART = new Uint8Array([0])
-    const encodeNumber = (value: number) => new Uint8Array([value >> 8, value & 0xff])
-    const numberOfFrames = Math.ceil(data.length / FRAME_SIZE)
-    const dataFrames = Array.from({ length: numberOfFrames })
-      .map((_, index) => data.subarray(index * FRAME_SIZE, (index + 1) * FRAME_SIZE))
-      .map((dataFrame, index) =>
-        u8aConcat(MULTIPART, encodeNumber(numberOfFrames), encodeNumber(index), dataFrame)
-      )
+    if (data.length > Math.pow(2, 31))
+      return setError("Payload is too large to be encoded in a QR code")
+    else setError(null)
 
     let cancelled = false
     ;(async () => {
-      const qrCodeFrames = []
-      for (const dataFrame of dataFrames) {
+      const MULTIPART = new Uint8Array([0])
+      const encodeNumber = (value: number) => new Uint8Array([value >> 8, value & 0xff])
+      const numberOfFrames = Math.ceil(data.length / FRAME_SIZE)
+
+      const dataFrames = Array.from({ length: numberOfFrames })
+        .map((_, index) => data.subarray(index * FRAME_SIZE, (index + 1) * FRAME_SIZE))
+        .map((dataFrame, index) =>
+          u8aConcat(MULTIPART, encodeNumber(numberOfFrames), encodeNumber(index), dataFrame)
+        )
+
+      qrCodeFrames.current = new Array(dataFrames.length).fill(null)
+      resetQrAnimation()
+      for (const [index, dataFrame] of dataFrames.entries()) {
+        // stop computing for nothing, happens especially in dev because of strict mode double mount
         if (cancelled) return
 
         const blob = await new QrCodeStyling({
@@ -37,11 +46,8 @@ export const MultipartQrRenderer = ({ data }: { data?: Uint8Array }) => {
           image: talismanRedHandSvg,
           imageOptions: { hideBackgroundDots: true, imageSize: 0.7, margin: 5 },
         }).getRawData("svg")
-        if (blob) qrCodeFrames.push(URL.createObjectURL(blob))
+        if (blob) qrCodeFrames.current[index] = URL.createObjectURL(blob)
       }
-
-      if (cancelled) return
-      setQrCodeFrames(qrCodeFrames)
     })()
 
     return () => {
@@ -51,20 +57,37 @@ export const MultipartQrRenderer = ({ data }: { data?: Uint8Array }) => {
 
   const [qrCode, setQrCode] = useState<string | null>(null)
   useEffect(() => {
-    if (qrCodeFrames === null) return setQrCode(null)
-    if (qrCodeFrames.length < 1) return setQrCode(null)
-    if (qrCodeFrames.length === 1) setQrCode(qrCodeFrames[0])
+    if (qrCodeFrames.current === null) return setQrCode(null)
+    if (qrCodeFrames.current.length < 1) return setQrCode(null)
+    if (qrCodeFrames.current.length === 1) setQrCode(qrCodeFrames.current[0])
 
-    let index = 0
-    setQrCode(qrCodeFrames[index])
-    const interval = setInterval(() => {
-      index = (index + 1) % qrCodeFrames.length
-      setQrCode(qrCodeFrames[index])
-    }, 100)
+    setQrCode(qrCodeFrames.current[0])
 
-    return () => clearInterval(interval)
-  }, [qrCodeFrames])
+    let active = true
+    const interval = 125 // 1000ms/125ms = 8 frames per second
+    const start = Date.now()
+    const animate = () => {
+      if (!active) return
 
+      const time = Date.now()
+      const index = Math.floor((time - start) / interval) % (qrCodeFrames.current?.length ?? 0)
+
+      setQrCode(qrCodeFrames.current?.[index] ?? null)
+      requestAnimationFrame(animate)
+    }
+    requestAnimationFrame(animate)
+
+    return () => {
+      active = false
+    }
+  }, [animGeneration])
+
+  if (error)
+    return (
+      <div className="text-alert-error relative h-full w-full whitespace-pre-wrap bg-white">
+        {error}
+      </div>
+    )
   if (!qrCode) return null
 
   return <img className="relative h-full w-full" src={qrCode} />
--- apps/extension/src/ui/domains/Sign/Qr/Renderers/RaptorQrRenderer.tsx
+++ apps/extension/src/ui/domains/Sign/Qr/Renderers/RaptorQrRenderer.tsx
@@ -1,96 +1,100 @@
 import { decodeString } from "@polkadot/react-qr/util"
 import { hexToNumber, numberToU8a, u8aConcat, u8aToU8a } from "@polkadot/util"
 import QrCodeStyling from "@solana/qr-code-styling"
-import { classNames } from "@talismn/util"
-import init, { Encoder } from "raptorq"
-import { useEffect, useRef, useState } from "react"
+import initRaptorq, { Encoder } from "raptorq"
+import { useEffect, useReducer, useRef, useState } from "react"
 
 import { FRAME_SIZE, talismanRedHandSvg } from "../constants"
 
 // This component uses raptorq wasm library (250ko) to generate the QR code, import only if necessary
 // spec here : https://github.com/varovainen/parity-signer/blob/2022-05-25-uos/docs/src/development/UOS.md
 const RaptorQrRenderer = ({ data }: { data?: Uint8Array }) => {
-  const [totalFramesCount, setTotalFramesCount] = useState<number>()
+  const qrCodeFrames = useRef<Array<string | null> | null>(null)
 
-  // storing in a ref so we can start iterating on frames before they are all ready
-  // they will appear as they are generated
-  const refQrCodes = useRef<string[]>()
-  const refImg = useRef<HTMLImageElement>(null)
+  const [animGeneration, resetQrAnimation] = useReducer((x) => (x + 1) % Number.MAX_SAFE_INTEGER, 0)
+  const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
     if (!data) return
-    let cancelled = false
+    if (data.length > Math.pow(2, 31))
+      return setError("Payload is too large to be encoded in a QR code")
+    else setError(null)
 
-    init().then(() => {
-      if (data.length > Math.pow(2, 31)) {
-        // eslint-disable-next-line no-console
-        console.error("Data is too large to be encoded in a QR code")
-        return
-      }
+    let cancelled = false
+    ;(async () => {
+      await initRaptorq()
 
       const framesCount = Math.ceil(data.length / FRAME_SIZE)
       const repairPackets = data.length <= FRAME_SIZE ? 0 : framesCount
       const framePrefix = numberToU8a(hexToNumber("0x80000000") + data.length)
 
       const encoder = Encoder.with_defaults(data, FRAME_SIZE)
-      const frames = encoder
+      const dataFrames = encoder
         .encode(repairPackets)
         .map((frame) => u8aToU8a(u8aConcat(framePrefix, u8aToU8a(frame))))
 
-      ;(async () => {
-        const qrCodeFrames: string[] = [] // work with a local var to prevent rerenders to mix they qrcodes (dev mode)
-        refQrCodes.current = qrCodeFrames
-        setTotalFramesCount(frames.length)
-
-        for (const dataFrame of frames) {
-          if (cancelled) return // stop computing for nothing, happens especially in dev because of strict mode double mount
-
-          const blob = await new QrCodeStyling({
-            type: "svg",
-            data: decodeString(dataFrame),
-            margin: 0,
-            qrOptions: { mode: "Byte", errorCorrectionLevel: "L" },
-            dotsOptions: { type: "dots" },
-            cornersSquareOptions: { type: "extra-rounded" },
-            cornersDotOptions: { type: "dot" },
-            image: talismanRedHandSvg,
-            imageOptions: { hideBackgroundDots: true, imageSize: 0.7, margin: 5 },
-          }).getRawData("svg")
-          if (blob) qrCodeFrames.push(URL.createObjectURL(blob))
-        }
-
+      qrCodeFrames.current = new Array(dataFrames.length).fill(null)
+      resetQrAnimation()
+      for (const [index, dataFrame] of dataFrames.entries()) {
+        // stop computing for nothing, happens especially in dev because of strict mode double mount
         if (cancelled) return
-      })()
-    })
+
+        const blob = await new QrCodeStyling({
+          type: "svg",
+          data: decodeString(dataFrame),
+          margin: 0,
+          qrOptions: { mode: "Byte", errorCorrectionLevel: "L" },
+          dotsOptions: { type: "dots" },
+          cornersSquareOptions: { type: "extra-rounded" },
+          cornersDotOptions: { type: "dot" },
+          image: talismanRedHandSvg,
+          imageOptions: { hideBackgroundDots: true, imageSize: 0.7, margin: 5 },
+        }).getRawData("svg")
+        if (blob) qrCodeFrames.current[index] = URL.createObjectURL(blob)
+      }
+    })()
 
     return () => {
       cancelled = true
     }
   }, [data])
 
+  const [qrCode, setQrCode] = useState<string | null>(null)
   useEffect(() => {
-    const img = refImg.current
-    if (!img || totalFramesCount === undefined) return
+    if (qrCodeFrames.current === null) return setQrCode(null)
+    if (qrCodeFrames.current.length < 1) return setQrCode(null)
+    if (qrCodeFrames.current.length === 1) setQrCode(qrCodeFrames.current[0])
 
-    let index = 0
-    const interval = setInterval(() => {
-      // array may change overtime, pick from ref
-      const qrCodes = refQrCodes.current
-      if (!qrCodes?.length) return
+    setQrCode(qrCodeFrames.current[0])
 
-      index = (index + 1) % qrCodes.length
-      img.src = qrCodes[index]
-    }, 100)
+    let active = true
+    const interval = 125 // 1000ms/125ms = 8 frames per second
+    const start = Date.now()
+    const animate = () => {
+      if (!active) return
 
-    return () => clearInterval(interval)
-  }, [totalFramesCount])
+      const time = Date.now()
+      const index = Math.floor((time - start) / interval) % (qrCodeFrames.current?.length ?? 0)
 
-  return (
-    <img
-      ref={refImg}
-      className={classNames("relative h-full w-full", totalFramesCount ? "block" : "hidden")}
-    />
-  )
+      setQrCode(qrCodeFrames.current?.[index] ?? null)
+      requestAnimationFrame(animate)
+    }
+    requestAnimationFrame(animate)
+
+    return () => {
+      active = false
+    }
+  }, [animGeneration])
+
+  if (error)
+    return (
+      <div className="text-alert-error relative h-full w-full whitespace-pre-wrap bg-white">
+        {error}
+      </div>
+    )
+  if (!qrCode) return null
+
+  return <img className="relative h-full w-full" src={qrCode} />
 }
 
 // export as default module to make it possible to lazy import
