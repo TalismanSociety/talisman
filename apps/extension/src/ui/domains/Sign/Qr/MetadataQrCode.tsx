feedebf9e17204c78dd84243ef1f9dc5c58d1042
alecdwm <alec@owls.io>
2023-03-30 01:08:56 +0000

    fix: use parity metadata qr codes for polkadot, kusama, westend

--- apps/extension/src/ui/domains/Sign/Qr/MetadataQrCode.tsx
+++ apps/extension/src/ui/domains/Sign/Qr/MetadataQrCode.tsx
@@ -1,6 +1,8 @@
 import { hexToU8a } from "@polkadot/util"
+import { Chain } from "@talismn/chaindata-provider"
 import { useQuery } from "@tanstack/react-query"
 import { api } from "@ui/api"
+import useChain from "@ui/hooks/useChain"
 
 import { QrCode } from "./QrCode"
 
@@ -10,6 +12,10 @@ type Props = {
 }
 
 export const MetadataQrCode = ({ genesisHash, specVersion }: Props) => {
+  const polkadot = useChain("polkadot")
+  const kusama = useChain("kusama")
+  const westend = useChain("westend")
+
   const { data, isLoading, error } = useQuery({
     queryKey: ["chainMetadataQr", genesisHash, specVersion],
     queryFn: async () => {
@@ -20,6 +26,16 @@ export const MetadataQrCode = ({ genesisHash, specVersion }: Props) => {
     refetchOnWindowFocus: false,
     refetchOnMount: false,
   })
+
+  // use parity metadata portal for these chains
+  const chain = new Map<string | null | undefined, Chain | undefined>()
+    .set(polkadot?.genesisHash, polkadot)
+    .set(kusama?.genesisHash, kusama)
+    .set(westend?.genesisHash, westend)
+    .get(genesisHash)
+  const latestMetadataQrUrl = chain?.latestMetadataQrUrl
+  if (latestMetadataQrUrl)
+    return <img className="relative h-full w-full" src={latestMetadataQrUrl} />
 
   if (isLoading || error) return null
 
