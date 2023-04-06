fae778a3184f6ed75ff8135fc6d7b0d972130f26
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-04-06 11:48:20 +0900

    feat: button on error page to yeet databases

--- apps/extension/src/@talisman/components/ErrorBoundary.tsx
+++ apps/extension/src/@talisman/components/ErrorBoundary.tsx
@@ -1,7 +1,9 @@
+import { DEBUG } from "@core/constants"
 import { ErrorBoundary as SentryErrorBoundary } from "@sentry/react"
 import STATIC from "@talisman/theme/images/hand_open_static_dark.gif"
-import { FC, ReactNode } from "react"
+import { FC, ReactNode, useCallback } from "react"
 import styled from "styled-components"
+import { Button } from "talisman-ui"
 
 import { SimpleButton } from "./SimpleButton"
 
@@ -44,25 +46,53 @@ const ErrorContainer = styled.section`
   }
 `
 
-const ErrorMessage: FC = () => (
-  <ErrorContainer>
-    <section>
-      <div className="content">
-        <h1>Oops !</h1>
-        <div>
-          <img className="inline-block" src={STATIC} alt="Talisman Hand logo" />
+const ErrorMessage: FC<{ error: Error }> = ({ error }) => {
+  const isDbVersionError = (error as any)?.inner?.name === "VersionError"
+
+  const clearDatabases = useCallback(() => {
+    indexedDB.deleteDatabase("Talisman")
+    indexedDB.deleteDatabase("TalismanBalances")
+    indexedDB.deleteDatabase("TalismanChaindata")
+    indexedDB.deleteDatabase("TalismanConnectionMeta")
+    alert(
+      "Databases cleared. Please close and reopen your browser (all windows) for Talisman to reinitialize."
+    )
+    window.close()
+  }, [])
+
+  return (
+    <ErrorContainer>
+      <section>
+        <div className="content">
+          <h1>Oops !</h1>
+          <div>
+            <img className="inline-block" src={STATIC} alt="Talisman Hand logo" />
+          </div>
+          {isDbVersionError ? (
+            <p>Sorry, an error occured in Talisman.</p>
+          ) : (
+            <p>Invalid database version.</p>
+          )}
         </div>
-        <p>Sorry, an error occured in Talisman.</p>
-      </div>
-      <div>
-        <SimpleButton className="!w-full" primary onClick={() => window.close()}>
-          Close
-        </SimpleButton>
-      </div>
-    </section>
-  </ErrorContainer>
-)
+        <div className="space-y-4">
+          {DEBUG && isDbVersionError && (
+            <Button className="w-full" onClick={clearDatabases}>
+              Clear local databases
+            </Button>
+          )}
+          <Button className="!w-full" primary onClick={() => window.close()}>
+            Close
+          </Button>
+        </div>
+      </section>
+    </ErrorContainer>
+  )
+}
 
 export const ErrorBoundary = ({ children }: { children?: ReactNode }) => {
-  return <SentryErrorBoundary fallback={<ErrorMessage />}>{children}</SentryErrorBoundary>
+  return (
+    <SentryErrorBoundary fallback={({ error }) => <ErrorMessage error={error} />}>
+      {children}
+    </SentryErrorBoundary>
+  )
 }
