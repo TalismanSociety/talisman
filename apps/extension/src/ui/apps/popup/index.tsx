5980c90c8a25ddcb570b53708d76612952626f7b
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-05-10 16:25:27 +0900

    feat: remove pending requests from embedded popup

--- apps/extension/src/ui/apps/popup/index.tsx
+++ apps/extension/src/ui/apps/popup/index.tsx
@@ -3,7 +3,6 @@ import { ETH_NETWORK_ADD_PREFIX, WATCH_ASSET_PREFIX } from "@core/domains/ethere
 import { METADATA_PREFIX } from "@core/domains/metadata/types"
 import { SIGNING_TYPES } from "@core/domains/signing/types"
 import { AUTH_PREFIX } from "@core/domains/sitesAuthorised/types"
-import { KnownRequestTypes, ValidRequests } from "@core/libs/requests/types"
 import { FadeIn } from "@talisman/components/FadeIn"
 import { api } from "@ui/api"
 import { AccountExportModalProvider } from "@ui/domains/Account/AccountExportModal"
@@ -14,9 +13,8 @@ import { CopyAddressModalProvider } from "@ui/domains/CopyAddress"
 import { SelectedAccountProvider } from "@ui/domains/Portfolio/SelectedAccountContext"
 import { useIsLoggedIn } from "@ui/hooks/useIsLoggedIn"
 import { useIsOnboarded } from "@ui/hooks/useIsOnboarded"
-import { useRequests } from "@ui/hooks/useRequests"
 import { useEffect, useMemo } from "react"
-import { Navigate, Route, Routes, useNavigate } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 
 import { BackupWarningDrawer } from "./components/BackupWarningDrawer"
 import { CurrentSiteProvider } from "./context/CurrentSiteContext"
@@ -32,49 +30,9 @@ import { SendFundsPage } from "./pages/SendFunds"
 import { EthereumSignRequest } from "./pages/Sign/ethereum"
 import { SubstrateSignRequest } from "./pages/Sign/substrate"
 
-const PendingRequestRedirect = () => {
-  const allRequests = useRequests()
-  const navigate = useNavigate()
-
-  // detect any pending requests and redirect to the appropriate page
-  useEffect(() => {
-    const requests = allRequests.reduce((result, req) => {
-      if (!result[req.type]) result[req.type] = []
-      result[req.type].push(req)
-      return result
-    }, {} as Record<KnownRequestTypes, ValidRequests[]>)
-
-    if (requests.auth?.length) {
-      navigate(`/${AUTH_PREFIX}/${requests.auth[0].id}`)
-    } else if (requests["eth-network-add"]?.length) {
-      navigate(`/${ETH_NETWORK_ADD_PREFIX}/${requests["eth-network-add"][0].id}`)
-    } else if (requests["eth-watchasset"]?.length) {
-      navigate(`/${WATCH_ASSET_PREFIX}/${requests["eth-watchasset"][0].id}`)
-    } else if (requests.metadata?.length) {
-      navigate(`/${METADATA_PREFIX}/${requests.metadata[0].id}`)
-    } else if (requests["eth-send"]?.length) {
-      const req = requests["eth-send"][0]
-      navigate(`/${req.type}/${req.id}`)
-    } else if (requests["eth-sign"]?.length) {
-      const req = requests["eth-sign"][0]
-      navigate(`/${req.type}/${req.id}`)
-    } else if (requests["substrate-sign"]?.length) {
-      const req = requests["substrate-sign"][0]
-      navigate(`/${req.type}/${req.id}`)
-    } else if (requests.encrypt?.length) {
-      navigate(`/${ENCRYPT_ENCRYPT_PREFIX}/${requests.encrypt[0].id}`)
-    } else if (requests.decrypt?.length) {
-      navigate(`/${ENCRYPT_DECRYPT_PREFIX}/${requests.decrypt[0].id}`)
-    }
-  }, [allRequests, navigate])
-
-  return null
-}
-
 const Popup = () => {
   const isOnboarded = useIsOnboarded()
   const isLoggedIn = useIsLoggedIn()
-  const isEmbeddedPopup = useMemo(() => document.documentElement.classList.contains("embedded"), [])
 
   // force onboarding if not onboarded
   useEffect(() => {
@@ -100,10 +58,7 @@ const Popup = () => {
   return (
     // TODO implement layout here to prevent container flickering on route change (some routes render null until loaded)
     // workaround set size here
-    // also use a fade-in to reduce flickering from portfolio to request page when opening
     <FadeIn className="mx-auto h-[60rem] w-[40rem]">
-      {/* only embedded popup should auto redirect to pending requests */}
-      {isEmbeddedPopup && <PendingRequestRedirect />}
       <SelectedAccountProvider isPopup>
         <AccountRemoveModalProvider>
           <AccountRenameModalProvider>
