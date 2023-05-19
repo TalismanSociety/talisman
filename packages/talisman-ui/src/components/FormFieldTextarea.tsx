ea469754c087e057e8e682211f4d3935f3075271
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-05-19 11:04:22 +0900

    fix: bg color on FormFieldTextArea

--- packages/talisman-ui/src/components/FormFieldTextarea.tsx
+++ packages/talisman-ui/src/components/FormFieldTextarea.tsx
@@ -12,7 +12,7 @@ export const FormFieldTextarea = forwardRef<HTMLTextAreaElement, FormFieldTextar
       <textarea
         ref={ref}
         className={classNames(
-          "focus:text-body placeholder:text-body-disabled ring-grey-600 text-grey-300 bg-field text-md disabled:text-body-disabled w-full resize-none rounded bg-transparent py-8 px-12 font-light focus:ring-1",
+          "focus:text-body placeholder:text-body-disabled ring-grey-600 text-grey-300 bg-field text-md disabled:text-body-disabled w-full resize-none rounded px-12 py-8 font-light focus:ring-1",
           props.className
         )}
         {...props}
