dbf0bb626b8a3c5b4d03bc7a4f1685100590de23
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-06-12 09:49:06 +0900

    chore: upgrade floating ui

--- apps/extension/src/@talisman/components/Tooltip/WithTooltip.tsx
+++ apps/extension/src/@talisman/components/Tooltip/WithTooltip.tsx
@@ -1,99 +1,29 @@
-import { flip, offset, shift, useFloating } from "@floating-ui/react-dom"
-import { CSSProperties, ReactNode, useEffect, useMemo } from "react"
-import { createPortal } from "react-dom"
-import styled from "styled-components"
-
-import { useTooltipBoundary } from "./TooltipBoundaryContext"
+import { ReactNode } from "react"
+import { Tooltip, TooltipContent, TooltipTrigger } from "talisman-ui"
 
 type TooltipContainerProps = {
   tooltip: ReactNode
   children: ReactNode
   as?: "span" | "div"
   className?: string
-  noWrap?: boolean
+  noWrap?: boolean //deprecated
 }
 
-const Tooltip = styled.div`
-  z-index: 1;
-  background: #000;
-  border-radius: 0.4rem;
-  border: 1px solid rgb(63, 63, 63); // grey-700 from our tailwind palette
-  color: var(--color-foreground-muted-2x);
-  padding: 0.6rem;
-  transition: opacity var(--transition-speed) ease-in-out;
-  opacity: 0;
-  visibility: hidden;
-  font-size: var(--font-size-xsmall);
-  line-height: var(--font-size-xsmall);
-  font-weight: var(--font-weight-regular);
-  word-break: break-word;
-
-  &.show {
-    opacity: 1;
-    visibility: visible;
-  }
-`
-
+/**
+ * @deprecated prefer <Tooltip> component which provides better control over HTML output
+ * */
 export const WithTooltip = ({
   tooltip,
   children,
   as: Container = "span",
   className,
-  noWrap,
 }: TooltipContainerProps) => {
-  const boundary = useTooltipBoundary()
-
-  const { x, y, reference, floating, strategy, refs, update } = useFloating({
-    strategy: "fixed",
-    middleware: [offset(16), shift({ boundary }), flip({ boundary })],
-  })
-
-  useEffect(() => {
-    const container = refs.reference.current as HTMLElement
-    const floating = refs.floating.current as HTMLElement
-
-    if (!tooltip) return
-
-    const show = () => {
-      //update coordinates before displaying
-      update()
-      floating.classList.add("show")
-    }
-    const hide = () => {
-      floating.classList.remove("show")
-    }
-
-    container.addEventListener("mouseenter", show)
-    container.addEventListener("mouseleave", hide)
-
-    return () => {
-      container.removeEventListener("mouseenter", show)
-      container.removeEventListener("mouseleave", hide)
-    }
-  }, [refs, tooltip, update])
-
-  const tooltipStyle: CSSProperties = useMemo(
-    () => ({
-      position: strategy,
-      top: (y ?? 0) - (boundary?.offsetTop ?? 0),
-      left: (x ?? 0) - (boundary?.offsetLeft ?? 0),
-      whiteSpace: noWrap ? "pre" : undefined,
-      lineHeight: 1.2,
-    }),
-    [boundary?.offsetLeft, boundary?.offsetTop, noWrap, strategy, x, y]
-  )
-
   return (
-    <>
-      <Container ref={reference} className={className}>
-        {children}
-      </Container>
-      {createPortal(
-        <Tooltip ref={floating} className="tooltip " style={tooltipStyle}>
-          {tooltip}
-        </Tooltip>,
-        boundary as Element
-      )}
-    </>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Container className={className}>{children}</Container>
+      </TooltipTrigger>
+      <TooltipContent>{tooltip}</TooltipContent>
+    </Tooltip>
   )
 }
