04d6098727c468fa1f8e479684f1754f92245f53
Chid Gilovitz <chid@talisman.xyz>
2023-07-06 13:26:28 +0800

    fix: prevent setting state to undefined if initialised with value in useSensitiveState

--- apps/extension/src/ui/hooks/useSensitiveState.ts
+++ apps/extension/src/ui/hooks/useSensitiveState.ts
@@ -4,9 +4,7 @@ function useSensitiveState<T>(): [
   T | undefined,
   React.Dispatch<React.SetStateAction<T | undefined>>
 ]
-function useSensitiveState<T>(
-  initialValue: T
-): [T, React.Dispatch<React.SetStateAction<T | undefined>>]
+function useSensitiveState<T>(initialValue: T): [T, React.Dispatch<React.SetStateAction<T>>]
 function useSensitiveState<T>(
   initialValue?: T
 ): [T | undefined, React.Dispatch<React.SetStateAction<T | undefined>>] {
