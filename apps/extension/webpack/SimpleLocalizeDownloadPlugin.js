32fd8e541ca48901860096cd82eaaf1189a2aa57
Alec WM <alec@owls.io>
2023-07-11 11:08:34 +1000

    fix: allow extension builds with no SimpleLocalize api key (#922)
    * fix: allow extension builds with no SimpleLocalize api key
    
    * fix: simplelocalize race condition

--- apps/extension/webpack/SimpleLocalizeDownloadPlugin.js
+++ apps/extension/webpack/SimpleLocalizeDownloadPlugin.js
@@ -6,6 +6,129 @@ const { DefinePlugin } = require("webpack")
 const apiKey = process.env.SIMPLE_LOCALIZE_API_KEY
 const projectToken = process.env.SIMPLE_LOCALIZE_PROJECT_TOKEN
 const endpoint = `https://api.simplelocalize.io/api/v4/export?downloadFormat=single-language-json&downloadOptions=SPLIT_BY_NAMESPACES`
+const fallbackLanguages = { en: "English" }
+
+module.exports = class SimpleLocalizeDownloadPlugin {
+  constructor(
+    options = {
+      /**
+       * If false (the default), this plugin will attempt to download translations from SimpleLocalize.
+       * Following this, this plugin will substitute `process.env.SUPPORTED_LANGUAGES` for a map of downloaded languages.
+       *
+       * If true, this plugin will simply substitute `process.env.SUPPORTED_LANGUAGES` for `'{"en":"English"}'`.
+       */
+      devMode: false,
+    }
+  ) {
+    this.options = options
+  }
+
+  apply(compiler) {
+    const {
+      webpack: {
+        sources: { RawSource },
+      },
+    } = compiler
+
+    const { devMode } = this.options
+    const hasKeys = apiKey && projectToken
+
+    if (!devMode && !hasKeys)
+      console.warn(
+        `No SimpleLocalize API key has been configured. This build will use the existing i18n translations at apps/extension/public/locales`
+      )
+
+    // only fetch translations for prod builds,
+    // and only when the developer has SimpleLocalize keys available
+    const fetchTranslations = !devMode && hasKeys
+    if (!fetchTranslations) {
+      compiler.hooks.afterPlugins.tap("SimpleLocalizeDownloadPlugin", ({ options }) => {
+        setSupportedLanguages(options, fallbackLanguages)
+      })
+
+      // if we're not fetching translations, stop here
+      // (don't tap into compiler.hooks.beforeRun or compiler.hooks.make)
+      return
+    }
+
+    compiler.hooks.beforeRun.tapAsync(
+      "SimpleLocalizeDownloadPlugin",
+      async ({ options }, callback) => {
+        const languages = await simpleLocalizeFetch(
+          `https://cdn.simplelocalize.io/${projectToken}/_latest/_languages`
+        )
+          .catch((error) => {
+            console.error("Failed to fetch languages list:", error)
+            return []
+          })
+          .then((result) => Object.fromEntries(result.map((lang) => [lang.key, lang.name])))
+
+        setSupportedLanguages(
+          options,
+          Object.keys(languages).length > 0 ? languages : fallbackLanguages
+        )
+
+        callback()
+      }
+    )
+
+    compiler.hooks.make.tapAsync("SimpleLocalizeDownloadPlugin", async (compilation, callback) => {
+      console.log(`Getting translations from ${endpoint}`)
+      const namespaces = await getNamespaceUrls()
+      const namespaceJson = await Promise.all(
+        namespaces.flatMap(({ url, namespace, language }) =>
+          simpleLocalizeFetch(url)
+            .then((json) => ({ namespace, language, json }))
+            .catch((error) => {
+              console.error(`Unable to get ${language} file for namespace ${namespace}:`, error)
+              return []
+            })
+        )
+      )
+
+      namespaceJson.forEach(({ namespace, language, json }) =>
+        compilation.emitAsset(
+          `locales/${language}/${namespace}.json`,
+          new RawSource(JSON.stringify(json))
+        )
+      )
+
+      const languages = namespaceJson
+        .map(({ language }) => language)
+        .sort()
+        .filter((language, index, all) => all.indexOf(language) === index)
+      console.log(`Translations fetched for ${languages.join(", ")}`)
+      callback()
+    })
+  }
+}
+
+const setSupportedLanguages = (options, supportedLanguages = fallbackLanguages) => {
+  const definePlugin = options.plugins.find((plugin) => plugin instanceof DefinePlugin)
+  if (!definePlugin)
+    return console.warn(
+      `No DefinePlugin found - process.env.SUPPORTED_LANGUAGES will not be substituted`
+    )
+
+  console.log("Setting supported languages", supportedLanguages)
+  // Explanation for the double JSON.stringify:
+  //
+  // - first stringify turns JSON into a string e.g. `{ en: "English" }` -> `'{"en":"English"}'`
+  // - second stringify puts string quotes around the string, e.g. `'{"en":"English"}'` -> `'"{\\"en\\":\\"English\\"}"'`
+  //
+  // The first stringify is necessary because process.env['key'] should be of type string, not object.
+  //
+  // The second stringify is necessary because DefinePlugin expects a code fragment, not a string.
+  // From the DefinePlugin docs:
+  // > Note that because the plugin does a direct text replacement,
+  // > the value given to it must include actual quotes inside of the string itself.
+  // >
+  // > Typically, this is done either with either alternate quotes, such as '"production"',
+  // > or by using JSON.stringify('production').
+  definePlugin.definitions["process.env.SUPPORTED_LANGUAGES"] = JSON.stringify(
+    JSON.stringify(supportedLanguages)
+  )
+}
 
 const simpleLocalizeFetch = (url) =>
   nodeFetch(url, {
@@ -16,11 +139,12 @@ const simpleLocalizeFetch = (url) =>
     },
   }).then((response) => response.json())
 
-/*
-* Fetch languages from CDN
-* returns shape:
-  { url:string, namespace:string, language: string }[]
-*/
+/**
+ * Fetch languages from CDN.
+ * Returns shape:
+ *
+ *     Array<{ url: string, namespace: string, language: string }>
+ */
 const getNamespaceUrls = () =>
   simpleLocalizeFetch(endpoint)
     .then((data) => {
@@ -32,63 +156,5 @@ const getNamespaceUrls = () =>
       return content
     })
     .catch((error) => {
-      console.error("Error:", error)
+      console.error("Failed to get namespace urls:", error)
     })
-
-class SimpleLocalizeDownloadPlugin {
-  apply(compiler) {
-    const {
-      webpack: {
-        sources: { RawSource },
-      },
-    } = compiler
-
-    compiler.hooks.afterPlugins.tap("SimpleLocalizeDownloadPlugin", async ({ options }) => {
-      const definePlugin = options.plugins.find((plugin) => plugin instanceof DefinePlugin)
-      if (definePlugin) {
-        const languages = await simpleLocalizeFetch(
-          `https://cdn.simplelocalize.io/${projectToken}/_latest/_languages`
-        )
-        const supportedLanguages = languages.reduce((result, lang) => {
-          result[lang.key] = lang.name
-          return result
-        }, {})
-
-        console.log("Setting supported languages", supportedLanguages)
-        definePlugin.definitions["process.env.SUPPORTED_LANGUAGES"] =
-          JSON.stringify(supportedLanguages)
-      }
-    })
-
-    compiler.hooks.make.tapAsync("SimpleLocalizeDownloadPlugin", async (compilation, callback) => {
-      console.log("Getting translations from ", endpoint)
-      const namespaces = await getNamespaceUrls()
-      const namespaceJson = await Promise.all(
-        namespaces.map(({ url, namespace, language }) =>
-          simpleLocalizeFetch(url)
-            .then((json) => {
-              return { namespace, language, json }
-            })
-            .catch((error) => {
-              console.error(`Unable to get ${language} file for namespace ${namespace}`)
-              console.error(error)
-              return undefined
-            })
-        )
-      )
-
-      namespaceJson.forEach((item) => {
-        if (!item) return
-        const { namespace, language, json } = item
-        compilation.emitAsset(
-          `locales/${language}/${namespace}.json`,
-          new RawSource(JSON.stringify(json))
-        )
-      })
-
-      callback()
-    })
-  }
-}
-
-module.exports = SimpleLocalizeDownloadPlugin
