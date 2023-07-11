0e7222d1846465553b77b7e52e76adef12477d68
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-07-11 18:27:41 +0900

    feat: tw PasswordStrengh

--- apps/extension/src/@talisman/components/PasswordStrength.tsx
+++ apps/extension/src/@talisman/components/PasswordStrength.tsx
@@ -1,25 +1,18 @@
 import { Options, passwordStrength } from "check-password-strength"
 import { useMemo } from "react"
-import styled from "styled-components"
 
-const getColorFromStrengh = (strength: number) => {
+const getColorClassName = (strength: number) => {
   switch (strength) {
     case 1:
-      return "var(--color-status-error)"
+      return "text-alert-error"
     case 2:
-      return "var(--color-status-concern)"
+      return "text-alert-warn"
     case 3:
-      return "var(--color-status-success)"
+      return "text-alert-success"
     default:
-      return "transparent"
+      return ""
   }
 }
-
-const Container = styled.span.attrs((props: { strength: number }) => ({
-  strength: props.strength ?? -1,
-}))`
-  color: ${({ strength }) => getColorFromStrengh(strength)};
-`
 
 type PasswordStrengthProps = {
   password?: string
@@ -59,5 +52,5 @@ export const PasswordStrength = ({ password }: PasswordStrengthProps) => {
   // Don't show if too weak (empty)
   if (!strength.id) return null
 
-  return <Container strength={strength.id}>{strength.value}</Container>
+  return <span className={getColorClassName(strength.id)}>{strength.value}</span>
 }
