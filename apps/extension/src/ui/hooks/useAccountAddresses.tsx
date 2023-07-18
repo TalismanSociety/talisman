6c77c85c6cdeacaf2e40e2d09959e2b7015ce68c
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-07-18 20:59:18 +1200

    feat: watched only accounts connection management

--- apps/extension/src/ui/hooks/useAccountAddresses.tsx
+++ apps/extension/src/ui/hooks/useAccountAddresses.tsx
@@ -1,16 +1,16 @@
-import useAccounts from "./useAccounts"
+import useAccounts, { UseAccountsFilter } from "./useAccounts"
 
 /**
  *
  * @param ethereumOnly causes the function to return only ethereum addresses. Returns all addresses by default
  * @returns array of addresses of accounts
  */
-export const useAccountAddresses = (ethereumOnly?: boolean, allowWatched?: boolean): string[] => {
-  const accounts = useAccounts()
+export const useAccountAddresses = (
+  ethereumOnly?: boolean,
+  accountFilter?: UseAccountsFilter
+): string[] => {
+  const accounts = useAccounts(accountFilter)
   return accounts
-    .filter(
-      ({ type, origin }) =>
-        (!ethereumOnly || type === "ethereum") && (allowWatched || origin !== "WATCHED")
-    )
+    .filter(({ type }) => !ethereumOnly || type === "ethereum")
     .map(({ address }) => address)
 }
