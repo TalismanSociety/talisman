2fd3d770d4333eb1ee7422f79369b95896f0c7d2
Lee Jean Hwa <lee@Lees-MacBook-Pro.local>
2023-08-11 15:43:23 +0800

    added tsconfig.json and dependencies. removed files in old directory

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
