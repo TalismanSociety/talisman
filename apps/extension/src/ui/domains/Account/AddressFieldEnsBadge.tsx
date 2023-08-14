1e07bfe10d7439e597efc2c6ec5a7633f2c74158
alecdwm <alec@owls.io>
2023-08-14 05:39:26 +0000

    fix: use consistent pill styling for `AddressFieldEnsBadge`

--- apps/extension/src/ui/domains/Account/AddressFieldEnsBadge.tsx
+++ apps/extension/src/ui/domains/Account/AddressFieldEnsBadge.tsx
@@ -1,6 +1,8 @@
+import { WithTooltip } from "@talisman/components/Tooltip"
 import { CheckCircleIcon, LoaderIcon } from "@talisman/theme/icons"
 import { AccountIcon } from "@ui/domains/Account/AccountIcon"
 import { Address } from "@ui/domains/Account/Address"
+import { PillButton } from "talisman-ui"
 
 export const AddressFieldEnsBadge = ({
   ensLookup,
@@ -15,18 +17,27 @@ export const AddressFieldEnsBadge = ({
     {isEnsLookup && !ensLookup && isEnsFetching ? (
       <LoaderIcon className="text-body-disabled animate-spin-slow" />
     ) : null}
+
     {isEnsLookup && ensLookup && (
-      <div className="bg-grey-750 flex items-center gap-4 rounded-2xl p-3">
-        <div className="flex items-center gap-2">
-          <AccountIcon className="text-lg" address={ensLookup} />
-          <Address className="text-body bg-grey-750" address={ensLookup} />
-        </div>
-        {isEnsFetching ? (
-          <LoaderIcon className="text-body-disabled animate-spin-slow" />
-        ) : (
-          <CheckCircleIcon className="text-primary" />
-        )}
-      </div>
+      <WithTooltip
+        tooltip={ensLookup}
+        // if we're not in a popup, no need to wrap
+        noWrap={!document.getElementById("main")}
+      >
+        <PillButton className="h-16 max-w-full !cursor-default !px-4">
+          <div className="text-body flex h-16 max-w-full flex-nowrap items-center gap-4 overflow-x-hidden text-base">
+            <AccountIcon className="text-lg" address={ensLookup} />
+            <div className="leading-base grow overflow-hidden text-ellipsis whitespace-nowrap">
+              <Address className="text-body" address={ensLookup} noTooltip />
+            </div>
+            {isEnsFetching ? (
+              <LoaderIcon className="text-body-disabled animate-spin-slow" />
+            ) : (
+              <CheckCircleIcon className="text-primary" />
+            )}
+          </div>
+        </PillButton>
+      </WithTooltip>
     )}
   </>
 )
