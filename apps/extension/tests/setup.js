c1e8ffc7bd9736beea3d48d464b9c2cf1a3f708b
alecdwm <alec@owls.io>
2023-09-01 04:58:50 +0000

    fix: missing TextEncoder|TextDecoder in jest jsdom env

--- apps/extension/tests/setup.js
+++ apps/extension/tests/setup.js
@@ -1,8 +1,14 @@
 /* eslint-env es2021 */
 const chrome = require("sinon-chrome")
 const { Crypto } = require("@peculiar/webcrypto")
+const { TextDecoder } = require("@polkadot/x-textdecoder")
+const { TextEncoder } = require("@polkadot/x-textencoder")
 
 global.crypto = new Crypto()
+
+global.TextDecoder = global.TextDecoder ?? TextDecoder
+global.TextEncoder = global.TextEncoder ?? TextEncoder
+
 // This is required because the main library we've used to mock webextension-polyfill (jest-webextension-mock) does not provide a mock for the windows property
 // so we use sinon-chrome for that instead. In order to be compatible with webextension-polyfill, we wrap the create method in a promise. It may be necessary
 // to do this for other methods if they are used in tests.
