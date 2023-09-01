4bfcb669c99cc15d508bfa5afb0a02742341ec9c
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-09-01 17:29:48 +0900

    chore: cleanup

--- apps/extension/src/core/handlers/index.ts
+++ apps/extension/src/core/handlers/index.ts
@@ -15,7 +15,6 @@ const tabs = new Tabs(tabStores)
 // dev mode logs shouldn't log content for these messages
 const OBFUSCATE_LOG_MESSAGES: MessageTypes[] = [
   "pri(mnemonic.unlock)",
-  "pri(mnemonic.address)",
   "pri(app.authenticate)",
   "pri(app.checkPassword)",
   "pri(app.changePassword)",
@@ -26,6 +25,7 @@ const OBFUSCATE_LOG_MESSAGES: MessageTypes[] = [
   "pri(accounts.create.seed)",
   "pri(accounts.create.json)",
   "pri(accounts.setVerifierCertMnemonic)",
+  "pri(accounts.address.lookup)",
   "pri(app.onboardCreatePassword)",
 ]
 const OBFUSCATED_PAYLOAD = "#OBFUSCATED#"
