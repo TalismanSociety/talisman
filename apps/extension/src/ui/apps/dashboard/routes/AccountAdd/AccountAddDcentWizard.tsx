3e73b7c73f70103dfac5c1f2f5de790fc46c9615
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-09-29 10:44:11 +0900

    fix: double header on dcent connect page (#1096)

--- apps/extension/src/ui/apps/dashboard/routes/AccountAdd/AccountAddDcentWizard.tsx
+++ apps/extension/src/ui/apps/dashboard/routes/AccountAdd/AccountAddDcentWizard.tsx
@@ -1,22 +1,13 @@
-import { HeaderBlock } from "@talisman/components/HeaderBlock"
 import { AccountAddDcentWizard } from "@ui/domains/Account/AccountAdd/AccountAddDcent"
 import { useSelectAccountAndNavigate } from "@ui/hooks/useSelectAccountAndNavigate"
-import { useTranslation } from "react-i18next"
 
 import { DashboardLayout } from "../../layout/DashboardLayout"
 
 export const AccountAddDcentDashboardWizard = () => {
   const { setAddress } = useSelectAccountAndNavigate("/portfolio")
-  const { t } = useTranslation("admin")
 
   return (
     <DashboardLayout withBack centered>
-      <HeaderBlock
-        title={t("Connect D'CENT Biometric Wallet")}
-        text={t(
-          "Before proceeding with the connection between Talisman Wallet and D'CENT Biometric Wallet, please carefully read and follow the instructions below to ensure a smooth and secure experience."
-        )}
-      />
       <AccountAddDcentWizard onSuccess={setAddress} />
     </DashboardLayout>
   )
