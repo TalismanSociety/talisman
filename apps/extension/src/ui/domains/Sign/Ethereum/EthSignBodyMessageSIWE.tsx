f1541219445dbda1f62d7412c686acbbd47884ea
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-10-09 13:44:23 +0900

    feat: EIP-4361 Sign In With Ethereum (SIWE) (#1092)
    * feat: siwe sign-in form
    
    * feat: siwe view details
    
    * fix: don't display network in main form
    
    * fix: domain check
    
    * fix: visible scrollbar
    
    * feat: display statement on sign in form
    
    * fix: adjust statement UI

---
+++ apps/extension/src/ui/domains/Sign/Ethereum/EthSignBodyMessageSIWE.tsx
@@ -0,0 +1,119 @@
+import { AccountJsonAny } from "@core/domains/accounts/types"
+import { EthSignRequest } from "@core/domains/signing/types"
+import { log } from "@core/log"
+import { hexToString } from "@polkadot/util"
+import { ParsedMessage } from "@spruceid/siwe-parser"
+import { UserRightIcon } from "@talismn/icons"
+import { useEvmNetwork } from "@ui/hooks/useEvmNetwork"
+import { FC, useMemo } from "react"
+import { useTranslation } from "react-i18next"
+import { Button, Drawer, useOpenClose } from "talisman-ui"
+
+import { SignAlertMessage } from "../SignAlertMessage"
+import { ViewDetailsAddress } from "../ViewDetails/ViewDetailsAddress"
+import { ViewDetailsButton } from "../ViewDetails/ViewDetailsButton"
+import { ViewDetailsField } from "../ViewDetails/ViewDetailsField"
+import { SignParamAccountButton } from "./shared"
+
+const ViewDetailsContent: FC<{
+  account: AccountJsonAny
+  request: EthSignRequest
+  siwe: ParsedMessage
+  onClose: () => void
+}> = ({ account, request, siwe, onClose }) => {
+  const { t } = useTranslation("request")
+  const evmNetwork = useEvmNetwork(String(siwe.chainId))
+
+  const message = useMemo(() => hexToString(request.request), [request.request])
+
+  return (
+    <div className="bg-grey-850 flex max-h-[60rem] w-full flex-col gap-12 p-12">
+      <div className="scrollable scrollable-700 flex-grow overflow-y-auto overflow-x-hidden pr-4 text-sm leading-[2rem]">
+        <div className="text-body-secondary">{t("Details")}</div>
+        <p>
+          {t(
+            "You are about to sign in via Ethereum. Please ensure you trust the application before continuing."
+          )}
+        </p>
+        <ViewDetailsAddress
+          label={t("From")}
+          address={account.address}
+          blockExplorerUrl={evmNetwork?.explorerUrl}
+        />
+        <ViewDetailsField label={t("Domain")}>{siwe.domain}</ViewDetailsField>
+        <ViewDetailsField label={t("Statement")}>{siwe.statement}</ViewDetailsField>
+        <ViewDetailsField label={t("Network")}>
+          {evmNetwork?.name} ({siwe.chainId})
+        </ViewDetailsField>
+        <ViewDetailsField label={t("Nonce")}>{siwe.nonce}</ViewDetailsField>
+        <ViewDetailsField label={t("Issued At")}>{siwe.issuedAt}</ViewDetailsField>
+        <ViewDetailsField label={t("Expires At")}>{siwe.expirationTime}</ViewDetailsField>
+        <ViewDetailsField label={t("Message")}>
+          <div className="mt-2 pr-2">
+            <pre className="text-body-secondary scrollable scrollable-700 bg-grey-800 rounded-xs w-full overflow-x-auto p-4">
+              {message}
+            </pre>
+          </div>
+        </ViewDetailsField>
+      </div>
+      <Button className="shrink-0" onClick={onClose}>
+        {t("Close")}
+      </Button>
+    </div>
+  )
+}
+
+export const EthSignBodyMessageSIWE: FC<{
+  account: AccountJsonAny
+  request: EthSignRequest
+  siwe: ParsedMessage
+}> = ({ account, request, siwe }) => {
+  const { t } = useTranslation("request")
+  const { isOpen, open, close } = useOpenClose()
+
+  const isValidUrl = useMemo(() => {
+    try {
+      // current webapp's domain must be the same as the siwe's domain
+      const currUrl = new URL(request.url)
+      return siwe.domain === currUrl.hostname
+    } catch (err) {
+      log.error(err)
+      return false
+    }
+  }, [siwe, request])
+
+  return (
+    <div className="scrollable scrollable-800 flex h-full max-h-full w-full flex-col items-center overflow-auto">
+      <div className="my-12 flex w-full flex-col items-center">
+        <div className="bg-grey-800 rounded-full p-5">
+          <UserRightIcon className="text-primary text-[2.8rem]" />
+        </div>
+        <div className="mt-8 text-lg font-bold">{t("Sign In")}</div>
+        <div className="text-body-secondary my-16 flex w-full flex-col items-center gap-3 overflow-hidden">
+          <div className="text-body max-w-full truncate font-bold">{siwe.domain}</div>
+          <div className="text-body-secondary">{t("wants you to sign in with Ethereum")}</div>
+          <div className="[&>button>div>span]:text-body flex max-w-full items-center justify-center truncate [&>button>div>span]:font-bold">
+            <span>{t("with")}</span>
+            <SignParamAccountButton address={account.address} withIcon />
+          </div>
+        </div>
+        {!!siwe.statement && (
+          <div className="bg-grey-850  mb-16 w-full rounded-sm p-4 text-sm">
+            <div className="text-body-disabled text-xs">{t("Statement")}</div>
+            <div className="text-body leading-paragraph mt-2">{siwe.statement}</div>
+          </div>
+        )}
+        <ViewDetailsButton onClick={open} />
+      </div>
+      <div className="grow"></div>
+      {!isValidUrl && (
+        <SignAlertMessage type="error" className="mt-8">
+          {t("Sign in domain is different from website domain.")}
+        </SignAlertMessage>
+      )}
+      <Drawer anchor="bottom" containerId="main" isOpen={isOpen} onDismiss={close}>
+        <ViewDetailsContent account={account} request={request} siwe={siwe} onClose={close} />
+      </Drawer>
+    </div>
+  )
+}
