86ef283d968aff4d3d5abcbad43cee3733d93c28
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-10-18 19:35:21 +0900

    fix: filter behavior

--- apps/extension/src/ui/apps/dashboard/routes/Tokens/TokensPage.tsx
+++ apps/extension/src/ui/apps/dashboard/routes/Tokens/TokensPage.tsx
@@ -1,11 +1,13 @@
 import { EvmNetwork } from "@core/domains/ethereum/types"
 import { enabledTokensStore, isTokenEnabled } from "@core/domains/tokens/store.enabledTokens"
 import { CustomErc20Token, Erc20Token } from "@core/domains/tokens/types"
+import { Accordion, AccordionIcon } from "@talisman/components/Accordion"
 import { HeaderBlock } from "@talisman/components/HeaderBlock"
 import { SearchInput } from "@talisman/components/SearchInput"
 import { Spacer } from "@talisman/components/Spacer"
 import { ChevronRightIcon, PlusIcon } from "@talismn/icons"
 import { AnalyticsPage, sendAnalyticsEvent } from "@ui/api/analytics"
+import { ChainLogo } from "@ui/domains/Asset/ChainLogo"
 import { TokenLogo } from "@ui/domains/Asset/TokenLogo"
 import { EnableTestnetPillButton } from "@ui/domains/Settings/EnableTestnetPillButton"
 import { useAnalyticsPageView } from "@ui/hooks/useAnalyticsPageView"
@@ -18,10 +20,10 @@ import { isCustomErc20Token } from "@ui/util/isCustomErc20Token"
 import { isErc20Token } from "@ui/util/isErc20Token"
 import sortBy from "lodash/sortBy"
 import { ChangeEventHandler, FC, useCallback, useMemo, useRef, useState } from "react"
-import { useTranslation } from "react-i18next"
+import { Trans, useTranslation } from "react-i18next"
 import { useNavigate } from "react-router-dom"
 import { useIntersection } from "react-use"
-import { ListButton, PillButton, Toggle } from "talisman-ui"
+import { ListButton, PillButton, Toggle, useOpenClose } from "talisman-ui"
 
 import { DashboardLayout } from "../../layout/DashboardLayout"
 
@@ -83,20 +85,37 @@ const TokenRow = ({ token }: { token: Erc20Token }) => {
   )
 }
 
-const NetworkTokensGroup: FC<{ network: EvmNetwork; tokens: Erc20Token[] }> = ({
-  network,
-  tokens,
-}) => {
+const NetworkTokensGroup: FC<{
+  network: EvmNetwork
+  tokens: Erc20Token[]
+  enabledCount: number
+  totalCount: number
+}> = ({ network, tokens, enabledCount, totalCount }) => {
+  const { isOpen, toggle } = useOpenClose()
+  // const enabledTokens = useTokensEnabledState()
+  // const enabledTokensCount = useMemo(
+  //   () => tokens.filter((t) => isTokenEnabled(t, enabledTokens)).length,
+  //   [enabledTokens, tokens]
+  // )
+
   return (
-    <>
-      <div className="flex items-center gap-4 pb-2 pt-8">
-        <TokenLogo className="inline text-xl" tokenId={network.nativeToken?.id} />{" "}
-        <span>{network.name}</span>
-      </div>
-      {tokens.map((token) => (
-        <TokenRow key={token.id} token={token} />
-      ))}
-    </>
+    <div className="flex flex-col gap-4">
+      <ListButton onClick={toggle}>
+        <ChainLogo className="inline text-xl" id={network.id} />{" "}
+        <div className="text-md text-body grow truncate">{network.name}</div>
+        <div className="text-primary shrink-0">
+          {enabledCount} of {totalCount}
+        </div>
+        <AccordionIcon className="text-lg" isOpen={isOpen} />
+      </ListButton>
+      <Accordion isOpen={isOpen}>
+        <div className="flex flex-col gap-4 pb-4 pl-12">
+          {tokens.map((token) => (
+            <TokenRow key={token.id} token={token} />
+          ))}
+        </div>
+      </Accordion>
+    </div>
   )
 }
 
@@ -119,36 +138,50 @@ export const TokensPage = () => {
   const { tokens } = useTokens("all")
   const erc20Tokens = useMemo(() => sortBy(tokens.filter(isErc20Token), "symbol"), [tokens])
   const [search, setSearch] = useState("")
+  const enabledTokens = useTokensEnabledState()
 
   const allTokensByNetwork = useMemo(() => {
     if (!evmNetworks || !erc20Tokens) return []
 
     return sortBy(evmNetworks, "name")
-      .map((network) => ({
-        network,
-        tokens: sortBy(
+      .map((network) => {
+        const tokens = sortBy(
           erc20Tokens.filter((t) => t.evmNetwork?.id === network.id),
           "symbol"
-        ),
-      }))
+        )
+        return {
+          network,
+          tokens,
+          enabledCount: tokens.filter((t) => isTokenEnabled(t, enabledTokens)).length,
+          totalCount: tokens.length,
+        }
+      })
       .filter(({ tokens }) => tokens.length)
-  }, [evmNetworks, erc20Tokens])
+  }, [evmNetworks, erc20Tokens, enabledTokens])
 
   const filterTokens = useCallback(
     (tokens: (Erc20Token | CustomErc20Token)[]) => {
+      if (!search) return tokens
+
       const lowerSearch = search.toLowerCase()
       return tokens.filter((t) =>
-        search ? t.symbol.toLowerCase().includes(lowerSearch) : t.isDefault || isCustomErc20Token(t)
+        lowerSearch
+          ? t.symbol.toLowerCase().includes(lowerSearch)
+          : t.isDefault || isCustomErc20Token(t)
       )
     },
     [search]
   )
 
   const groups = useMemo(() => {
-    return allTokensByNetwork
-      .map((n) => ({ ...n, tokens: filterTokens(n.tokens) }))
-      .filter(({ tokens }) => tokens.length)
-  }, [allTokensByNetwork, filterTokens])
+    return allTokensByNetwork.map((n) => ({
+      ...n,
+      enabledCount: n.tokens.filter((t) => isTokenEnabled(t, enabledTokens)).length,
+      totalCount: n.tokens.length,
+      tokens: filterTokens(n.tokens),
+    }))
+    //.filter(({ tokens }) => tokens.length)
+  }, [allTokensByNetwork, enabledTokens, filterTokens])
 
   const handleAddToken = useCallback(() => {
     sendAnalyticsEvent({
@@ -168,10 +201,18 @@ export const TokensPage = () => {
       centered
       backTo="/settings/networks-tokens"
     >
-      <HeaderBlock title={t("Ethereum Tokens")} text={t("Add or delete custom ERC20 tokens")} />
+      <HeaderBlock
+        title={t("Ethereum Tokens")}
+        text={
+          <Trans
+            t={t}
+            defaults="Enable, add or delete custom ERC20 tokens.<br />Please consider enabling only the tokens you're interested in, to prevent any performance issue.<br/>The tokens list is provided by Coingecko."
+          />
+        }
+      />
       <Spacer large />
       <div className="flex gap-4">
-        <SearchInput onChange={setSearch} placeholder={t("Search tokens")} />
+        <SearchInput onChange={setSearch} placeholder={t("Search to display more tokens")} />
       </div>
       <Spacer small />
       <div className="flex justify-end gap-4">
@@ -181,11 +222,15 @@ export const TokensPage = () => {
         </PillButton>
       </div>
       <Spacer small />
-      <div className="flex flex-col gap-4">
-        {groups.map(({ network, tokens }) => (
-          <NetworkTokensGroup key={network.id} network={network} tokens={tokens} />
-        ))}
-      </div>
+      {groups.map(({ network, tokens, enabledCount, totalCount }) => (
+        <NetworkTokensGroup
+          key={network.id}
+          network={network}
+          tokens={tokens}
+          enabledCount={enabledCount}
+          totalCount={totalCount}
+        />
+      ))}
     </DashboardLayout>
   )
 }
