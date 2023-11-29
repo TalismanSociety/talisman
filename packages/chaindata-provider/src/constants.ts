64c99e43842d8723602de357de40692c765aa30b
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-11-29 11:40:45 +0900

    feat: debug from specific chaindata branch

--- packages/chaindata-provider/src/constants.ts
+++ packages/chaindata-provider/src/constants.ts
@@ -1,10 +1,13 @@
 import { ChainId, EvmNetworkId, TokenId } from "./types"
 
+// @dev : temporarily change branch here when testing changes in chaindata
+const CHAINDATA_BRANCH = "feat/per-branch-data"
+
 //
-// Chaindata (Published to GitHub Pages) Constants
+// Chaindata published files (dist folder)
 //
 
-export const chaindataUrl = "https://talismansociety.github.io/chaindata"
+export const chaindataUrl = `https://raw.githubusercontent.com/TalismanSociety/chaindata/${CHAINDATA_BRANCH}/dist`
 
 export const chaindataChainsAllUrl = `${chaindataUrl}/chains/all.json`
 export const chaindataChainsSummaryUrl = `${chaindataUrl}/chains/summary.json`
@@ -32,13 +35,13 @@ export const githubApi = "https://api.github.com"
 
 export const githubChaindataOrg = "TalismanSociety"
 export const githubChaindataRepo = "chaindata"
-export const githubChaindataBranch = "main"
+export const githubChaindataBranch = CHAINDATA_BRANCH
 
 export const githubChaindataBaseUrl = `https://raw.githubusercontent.com/${githubChaindataOrg}/${githubChaindataRepo}/${githubChaindataBranch}`
 
-export const githubChainsUrl = `${githubChaindataBaseUrl}/chaindata.json`
-export const githubTestnetChainsUrl = `${githubChaindataBaseUrl}/testnets-chaindata.json`
-export const githubEvmNetworksUrl = `${githubChaindataBaseUrl}/evm-networks.json`
+export const githubChainsUrl = `${githubChaindataBaseUrl}/data/chaindata.json`
+export const githubTestnetChainsUrl = `${githubChaindataBaseUrl}/data/testnets-chaindata.json`
+export const githubEvmNetworksUrl = `${githubChaindataBaseUrl}/data/evm-networks.json`
 
 export const githubChaindataChainsAssetsDir = "assets/chains"
 export const githubChaindataTokensAssetsDir = "assets/tokens"
