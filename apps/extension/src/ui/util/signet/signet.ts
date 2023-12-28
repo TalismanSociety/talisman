8073569ad36b3e5d652f64938d20e37a38cd9a22
chrisling-dev <chrisling.dev@gmail.com>
2023-12-28 13:07:54 +0800

    fix: better handling of different format of signet url

--- apps/extension/src/ui/util/signet/signet.ts
+++ apps/extension/src/ui/util/signet/signet.ts
@@ -1,8 +1,18 @@
 import { SignetVault } from "@ui/domains/Account/AccountAdd/AccountAddSignet/types"
 
 export const signet = {
+  /**
+   * Open Signet's connect page in a new tab for users to select vaults to connect.
+   * Communication is done using window.postMessage to communicate between 2 tabs.
+   * @param signetUrl Works for 4 formats of signetUrl: {{signetUrl}}, {{signetUrl}}/, {{signetUrl}}/overview, {{signetUrl}}/overview/
+   * @returns
+   */
   getVaults: async (signetUrl: string) => {
-    const newTab = window.open(`${signetUrl}/connect`)
+    // if overview is present, we get the beginning of the url until overview
+    // otherwise we use the whole url
+    const withoutOverview = signetUrl.split("overview")[0]
+    const url = new URL(withoutOverview)
+    const newTab = window.open(`${url.href}connect`)
 
     return new Promise<SignetVault[]>((resolve, reject) => {
       if (!newTab) return reject("Failed to open new tab")
@@ -18,7 +28,7 @@ export const signet = {
           window.removeEventListener("message", handleNewMessage)
         }
 
-        if (event.origin !== signetUrl) return
+        if (event.origin !== url.origin) return
 
         if (event.data.type === "signet(connect.cancel)") {
           close()
