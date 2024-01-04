74951da01a653b8e966d315376e2e9bc01f2a7a5
alecdwm <alec@owls.io>
2024-01-04 01:35:29 +0000

    fix: don't allow users to send substrate funds to transaction hashes

--- apps/extension/src/@talisman/util/isValidAddress.ts
+++ apps/extension/src/@talisman/util/isValidAddress.ts
@@ -1,4 +1,5 @@
-import { encodeAnyAddress } from "@talismn/util"
+import { u8aToHex } from "@polkadot/util"
+import { decodeAnyAddress, encodeAnyAddress, isEthereumAddress } from "@talismn/util"
 
 /**
  *
@@ -11,10 +12,17 @@ export const isValidAddress = (address: string, prefix?: number | null) => {
     // attempt to encode, it will throw an error if address is invalid
     const encoded = encodeAnyAddress(address, prefix ?? undefined)
 
-    //if a prefix is supplied, check that reencoding using this prefix matches the input address
+    // if a prefix is supplied, check that reencoding using this prefix matches the input address
     if (prefix !== undefined) return address === encoded
 
-    //if no prefix supplied, the fact that decoding + encoding succeded indicates that the address is valid
+    // don't allow public keys to be encoded into substrate addresses
+    // i.e. only allow substrate addresses from one encoded format to another (e.g. Kusama to Polkadot)
+    //
+    // this prevents users from pasting e.g. a transaction hash and having our UI
+    // interpret it as a polkadot address for an account that nobody has the key for
+    if (!isEthereumAddress(address) && address === u8aToHex(decodeAnyAddress(address))) return false
+
+    // if no prefix supplied, the fact that decoding + encoding succeded indicates that the address is valid
     return true
   } catch (error) {
     // input is not a substrate (SS58) address
