29eef546133443c73bb959dc00f960e783015a13
Kheops <26880866+0xKheops@users.noreply.github.com>
2024-01-08 16:11:57 +0900

    feat: delete disposable dbs on reload (#1216)

--- apps/extension/src/ui/hooks/useRuntimeReload.tsx
+++ apps/extension/src/ui/hooks/useRuntimeReload.tsx
@@ -1,15 +1,20 @@
+import { db as balancesDb } from "@talismn/balances"
+import { connectionMetaDb } from "@talismn/connection-meta"
 import { AnalyticsPage, sendAnalyticsEvent } from "@ui/api/analytics"
 import { useCallback, useState } from "react"
 import Browser from "webextension-polyfill"
 
 export const useRuntimeReload = (analyticsPage: AnalyticsPage) => {
   const [hasRuntimeReloadFn] = useState(() => typeof Browser?.runtime?.reload === "function")
-  const runtimeReload = useCallback(() => {
+  const runtimeReload = useCallback(async () => {
     sendAnalyticsEvent({
       ...analyticsPage,
       name: "Interact",
       action: "Reload Talisman button",
     })
+
+    // these 2 dbs do not contain any user data, they will be safely recreated on next startup
+    await Promise.allSettled([balancesDb.delete(), connectionMetaDb.delete()])
 
     Browser.runtime.reload()
   }, [analyticsPage])
