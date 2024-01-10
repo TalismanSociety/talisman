c1cba2725e26df38f5b2a95f55ba2ea5e9819438
Kheops <26880866+0xKheops@users.noreply.github.com>
2024-01-10 12:22:22 +0900

    feat: auto scan on asset discovery rollout

--- apps/extension/src/core/libs/migrations/index.ts
+++ apps/extension/src/core/libs/migrations/index.ts
@@ -1,4 +1,5 @@
 import { migrateToNewAccountTypes } from "@core/domains/accounts/migrations"
+import { migrateAssetDiscoveryRollout } from "@core/domains/assetDiscovery/migrations"
 import { migrateToNewDefaultEvmNetworks } from "@core/domains/ethereum/migrations"
 import { migrateSeedStoreToMultiple } from "@core/domains/mnemonics/migrations"
 
@@ -11,4 +12,5 @@ export const migrations: Migrations = [
   migrateSeedStoreToMultiple,
   migrateToNewAccountTypes,
   migrateToNewDefaultEvmNetworks,
+  migrateAssetDiscoveryRollout,
 ]
