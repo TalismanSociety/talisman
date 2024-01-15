b3c17a100cb1aa63a36447cc2bfdd9b16195a599
Kheops <26880866+0xKheops@users.noreply.github.com>
2024-01-15 14:51:46 +0900

    fix: suspense on EvmNetworkSelectPill

--- apps/extension/src/ui/domains/Ethereum/EvmNetworkSelectPill.tsx
+++ apps/extension/src/ui/domains/Ethereum/EvmNetworkSelectPill.tsx
@@ -1,13 +1,22 @@
+import { EvmNetworkId } from "@talismn/chaindata-provider"
 import { ChevronDownIcon } from "@talismn/icons"
 import { classNames } from "@talismn/util"
 import { useAuthorisedSites } from "@ui/hooks/useAuthorisedSites"
 import { useCurrentSite } from "@ui/hooks/useCurrentSite"
 import { useEvmNetwork } from "@ui/hooks/useEvmNetwork"
-import { useMemo } from "react"
+import { Suspense, useMemo } from "react"
 import { Tooltip, TooltipContent, TooltipTrigger, useOpenClose } from "talisman-ui"
 
 import { EvmNetworkSelectDrawer } from "./EvmNetworkSelectDrawer"
 import { NetworkLogo } from "./NetworkLogo"
+
+const EvmNetworkName = ({ evmNetworkId }: { evmNetworkId: EvmNetworkId }) => {
+  const network = useEvmNetwork(evmNetworkId)
+
+  if (!network) return null
+
+  return <>{network?.name}</>
+}
 
 export const EvmNetworkSelectPill = () => {
   const currentSite = useCurrentSite()
@@ -16,11 +25,12 @@ export const EvmNetworkSelectPill = () => {
     () => (currentSite?.id ? authorisedSites[currentSite?.id] : null),
     [authorisedSites, currentSite?.id]
   )
-  const network = useEvmNetwork(site?.ethChainId?.toString())
 
   const { isOpen, open, close } = useOpenClose()
 
-  if (!site?.ethChainId) return null
+  const evmNetworkId = site?.ethChainId?.toString()
+
+  if (!evmNetworkId) return null
 
   return (
     <>
@@ -28,20 +38,23 @@ export const EvmNetworkSelectPill = () => {
         <TooltipTrigger asChild>
           <button
             type="button"
-            className="text-body-secondary bg-grey-850 hover:bg-grey-800 hover:text-grey-300 border-grey-800 flex h-[3.6rem] shrink-0 items-center gap-3 rounded-3xl border pl-2 pr-3 text-sm"
+            className="text-body-secondary bg-grey-850 hover:bg-grey-800 hover:text-grey-300 border-grey-800 flex h-[3.6rem] w-[6.2rem] shrink-0 items-center gap-3 rounded-3xl border pl-2 pr-3 text-sm"
             onClick={open}
           >
-            <NetworkLogo
-              className={classNames(
-                "text-[2.8rem] transition-opacity",
-                network ? "opacity-100" : "opacity-0"
-              )} // flickering hack
-              ethChainId={network?.id}
-            />
-            <ChevronDownIcon className="shrink-0 text-base" />
+            <Suspense>
+              <NetworkLogo
+                className={classNames("text-[2.8rem] transition-opacity")} // flickering hack
+                ethChainId={evmNetworkId}
+              />
+              <ChevronDownIcon className="shrink-0 text-base" />
+            </Suspense>
           </button>
         </TooltipTrigger>
-        <TooltipContent>{network?.name}</TooltipContent>
+        <TooltipContent>
+          <Suspense fallback={null}>
+            <EvmNetworkName evmNetworkId={evmNetworkId} />
+          </Suspense>
+        </TooltipContent>
       </Tooltip>
       <EvmNetworkSelectDrawer isOpen={isOpen} onClose={close} />
     </>
