def0edff2acbe7e12a4cce9a2fb06fbbf75bd29f
Kheops <26880866+0xKheops@users.noreply.github.com>
2024-01-25 20:25:57 +0900

    fix: comment

--- apps/extension/src/core/db/upgrades/upgradeRemoveSymbolFromNativeTokenId.ts
+++ apps/extension/src/core/db/upgrades/upgradeRemoveSymbolFromNativeTokenId.ts
@@ -1,7 +1,7 @@
 import { WalletTransaction } from "@core/domains/transactions"
 import { Transaction as DbTransaction } from "dexie"
 
-// for DB version 2, Wallet version 1.21.0
+// For db version 8, Wallet version 1.21.0
 export const upgradeRemoveSymbolFromNativeTokenId = (tx: DbTransaction) => {
   return tx
     .table<WalletTransaction, string>("transactions")
