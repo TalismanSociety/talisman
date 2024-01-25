ebf57bf0310eee0df4c73a9a911c7f52aa7de64f
Kheops <26880866+0xKheops@users.noreply.github.com>
2024-01-25 16:54:01 +0900

    feat: upgrade transactions table

---
+++ apps/extension/src/core/db/upgrades/upgradeRemoveSymbolFromNativeTokenId.ts
@@ -0,0 +1,12 @@
+import { WalletTransaction } from "@core/domains/transactions"
+import { Transaction as DbTransaction } from "dexie"
+
+// for DB version 2, Wallet version 1.21.0
+export const upgradeRemoveSymbolFromNativeTokenId = async (tx: DbTransaction) => {
+  return tx
+    .table<WalletTransaction, string>("transactions")
+    .toCollection()
+    .modify((wtx) => {
+      if (wtx.tokenId) wtx.tokenId = wtx.tokenId.replace(/-native(-[a-zA-Z0-9])$/, "")
+    })
+}
