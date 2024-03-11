83b65ddf66359104cd69e8360e77f0b2b25f3abd
Kheops <26880866+0xKheops@users.noreply.github.com>
2024-03-11 12:49:48 +0900

    feat: support Scroll network (#1351)
    * feat: support Scroll networks
    
    * fix: contract target detection
    
    * chore: changeset

--- packages/extension-core/src/util/isContractAddress.ts
+++ packages/extension-core/src/util/isContractAddress.ts
@@ -5,7 +5,7 @@ import { EvmAddress } from "../domains/ethereum/types"
 export const isContractAddress = async (client: PublicClient, address: EvmAddress) => {
   try {
     const code = await client.getBytecode({ address })
-    return code !== "0x"
+    return !!code && code !== "0x"
   } catch (error) {
     // not a contract
     return false
