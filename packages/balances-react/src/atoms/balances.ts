285eef118f540fbd6ea89df517bba5ddd4a44176
alecdwm <alec@owls.io>
2024-03-14 05:23:11 +0000

    feat: @talismn/balances-react delete invalid cached balances and set up initializing balances

--- packages/balances-react/src/atoms/balances.ts
+++ packages/balances-react/src/atoms/balances.ts
@@ -1,4 +1,5 @@
 import {
+  AddressesByToken,
   Balance,
   BalanceJson,
   BalanceStatusLive,
@@ -11,7 +12,8 @@ import {
   deriveStatuses,
   getValidSubscriptionIds,
 } from "@talismn/balances"
-import { firstThenDebounce } from "@talismn/util"
+import { Token } from "@talismn/chaindata-provider"
+import { firstThenDebounce, isEthereumAddress } from "@talismn/util"
 import { liveQuery } from "dexie"
 import { atom } from "jotai"
 import { atomEffect } from "jotai-effect"
@@ -24,9 +26,12 @@ import { balanceModulesAtom } from "./balanceModules"
 import {
   chaindataAtom,
   chainsAtom,
+  chainsByIdAtom,
   evmNetworksAtom,
+  evmNetworksByIdAtom,
   miniMetadatasAtom,
   tokensAtom,
+  tokensByIdAtom,
 } from "./chaindata"
 import { miniMetadataHydratedAtom } from "./chaindataProvider"
 import { enabledChainsAtom } from "./config"
@@ -86,8 +91,11 @@ const balancesSubscriptionAtomEffect = atomEffect((get) => {
 
     get(allAddressesAtom),
     get(chainsAtom),
+    get(chainsByIdAtom),
     get(evmNetworksAtom),
+    get(evmNetworksByIdAtom),
     get(tokensAtom),
+    get(tokensByIdAtom),
     get(miniMetadatasAtom),
 
     get(enabledChainsAtom),
@@ -102,8 +110,11 @@ const balancesSubscriptionAtomEffect = atomEffect((get) => {
 
       allAddresses,
       chains,
-      _evmNetworks,
+      chainsById,
+      evmNetworks,
+      evmNetworksById,
       tokens,
+      tokensById,
       _miniMetadatas,
 
       enabledChains,
@@ -149,12 +160,95 @@ const balancesSubscriptionAtomEffect = atomEffect((get) => {
 
     if (enabledTokenIds.length < 1 || allAddresses.length < 1) return
 
-    const addressesByToken = Object.fromEntries(
-      enabledTokenIds.map((tokenId) => [tokenId, allAddresses])
-    )
+    const addressesByTokenByModule: Record<string, AddressesByToken<Token>> = {}
+    enabledTokenIds
+      .flatMap((tokenId) => tokensById[tokenId])
+      .forEach((token) => {
+        // filter out tokens on chains/evmNetworks which have no rpcs
+        const hasRpcs =
+          (token.chain?.id && (chainsById[token.chain.id]?.rpcs?.length ?? 0) > 0) ||
+          (token.evmNetwork?.id && (evmNetworksById[token.evmNetwork.id]?.rpcs?.length ?? 0) > 0)
+        if (!hasRpcs) return
 
-    // TODO: Delete invalid existing balances
-    // TODO: Set up `initializing` balances
+        if (!addressesByTokenByModule[token.type]) addressesByTokenByModule[token.type] = {}
+        addressesByTokenByModule[token.type][token.id] = allAddresses.filter((address) => {
+          // for each address, fetch balances only from compatible chains
+          return isEthereumAddress(address)
+            ? !!token.evmNetwork?.id || chainsById[token.chain?.id ?? ""]?.account === "secp256k1"
+            : !!token.chain?.id
+        })
+      })
+
+    // Delete invalid cached balances
+    const chainIds = new Set(chains.map((chain) => chain.id))
+    const evmNetworkIds = new Set(evmNetworks.map((evmNetwork) => evmNetwork.id))
+    const tokenIds = new Set(tokens.map((token) => token.id))
+    await deleteBalances((balance) => {
+      // delete cached balances for accounts which don't exist anymore
+      if (!balance.address || !allAddresses.includes(balance.address)) return true
+
+      // delete cached balances when chain/evmNetwork doesn't exist
+      if (balance.chainId === undefined && balance.evmNetworkId === undefined) return true
+      if (balance.chainId !== undefined && !chainIds.has(balance.chainId)) return true
+      if (balance.evmNetworkId !== undefined && !evmNetworkIds.has(balance.evmNetworkId))
+        return true
+
+      // delete cached balance when token doesn't exist
+      if (!tokenIds.has(balance.tokenId)) return true
+
+      // delete cached balance when module doesn't exist
+      if (!balanceModules.find((module) => module.type === balance.source)) return true
+
+      // delete cached balance for accounts on incompatible chains
+      // (substrate accounts shouldn't have evm balances)
+      // (evm accounts shouldn't have substrate balances (unless the chain uses secp256k1 accounts))
+      const chain = (balance.chainId && chains.find(({ id }) => id === balance.chainId)) || null
+      const hasChain = balance.chainId && chainIds.has(balance.chainId)
+      const hasEvmNetwork = balance.evmNetworkId && evmNetworkIds.has(balance.evmNetworkId)
+      const chainUsesSecp256k1Accounts = chain?.account === "secp256k1"
+      if (!isEthereumAddress(balance.address) && !hasChain) {
+        return true
+      }
+      if (isEthereumAddress(balance.address) && !(hasEvmNetwork || chainUsesSecp256k1Accounts)) {
+        return true
+      }
+
+      // keep balance
+      return false
+    })
+    if (abort.signal.aborted) return
+
+    // create placeholder rows for all missing balances, so FE knows they are initializing
+    const missingBalances: BalanceJson[] = []
+    const existingBalances = await balancesDb.balances.toArray()
+    const existingBalancesKeys = new Set(existingBalances.map((b) => `${b.tokenId}:${b.address}`))
+
+    for (const balanceModule of balanceModules) {
+      const addressesByToken = addressesByTokenByModule[balanceModule.type] ?? {}
+      for (const [tokenId, addresses] of Object.entries(addressesByToken))
+        for (const address of addresses) {
+          if (!existingBalancesKeys.has(`${tokenId}:${address}`))
+            missingBalances.push(balanceModule.getPlaceholderBalance(tokenId, address))
+        }
+    }
+
+    if (missingBalances.length) {
+      const updates = Object.entries(new Balances(missingBalances).toJSON()).map(
+        ([id, balance]) => ({ id, ...balance })
+      )
+      await balancesDb.balances.bulkPut(updates)
+    }
+    if (abort.signal.aborted) return
+
+    // after 30 seconds, change the status of all balances still initializing to stale
+    setTimeout(() => {
+      if (abort.signal.aborted) return
+
+      balancesDb.balances
+        .where({ status: "initializing" })
+        .modify({ status: "stale" })
+        .catch((error) => log.error("Failed to update balances", { error }))
+    }, 30_000)
 
     const subscriptionId = createSubscriptionId()
     // TODO: Create subscriptions in a service worker, where we can detect page closes
@@ -171,40 +265,39 @@ const balancesSubscriptionAtomEffect = atomEffect((get) => {
     }
 
     return balanceModules.map((balanceModule) => {
-      // filter out tokens to only include those which this module knows how to fetch balances for
-      const moduleTokenIds = Object.values(enabledTokens ?? {})
-        .filter(({ type }) => type === balanceModule.type)
-        .map(({ id }) => id)
-      const addressesByModuleToken = Object.fromEntries(
-        Object.entries(addressesByToken).filter(([tokenId]) => moduleTokenIds.includes(tokenId))
-      )
+      const unsub = balancesFn(
+        balanceModule,
+        addressesByTokenByModule[balanceModule.type] ?? {},
+        (error, balances) => {
+          // log errors
+          if (error) {
+            if (
+              error?.type === "STALE_RPC_ERROR" ||
+              error?.type === "WEBSOCKET_ALLOCATION_EXHAUSTED_ERROR"
+            ) {
+              const addressesByModuleToken = addressesByTokenByModule[balanceModule.type] ?? {}
+              return balancesDb.balances
+                .where({ source: balanceModule.type, chainId: error.chainId })
+                .filter((balance) => {
+                  if (!Object.keys(addressesByModuleToken).includes(balance.tokenId)) return false
+                  if (!addressesByModuleToken[balance.tokenId].includes(balance.address))
+                    return false
+                  return true
+                })
+                .modify({ status: "stale" })
+            }
 
-      const unsub = balancesFn(balanceModule, addressesByModuleToken, (error, balances) => {
-        // log errors
-        if (error) {
-          if (
-            error?.type === "STALE_RPC_ERROR" ||
-            error?.type === "WEBSOCKET_ALLOCATION_EXHAUSTED_ERROR"
-          )
-            return balancesDb.balances
-              .where({ source: balanceModule.type, chainId: error.chainId })
-              .filter((balance) => {
-                if (!Object.keys(addressesByModuleToken).includes(balance.tokenId)) return false
-                if (!addressesByModuleToken[balance.tokenId].includes(balance.address)) return false
-                return true
-              })
-              .modify({ status: "stale" })
+            return log.error(`Failed to fetch ${balanceModule.type} balances`, error)
+          }
 
-          return log.error(`Failed to fetch ${balanceModule.type} balances`, error)
+          // ignore empty balance responses
+          if (!balances) return
+          // ignore balances from old subscriptions which are still in the process of unsubscribing
+          if (abort.signal.aborted) return
+
+          updateBalances(balances)
         }
-
-        // ignore empty balance responses
-        if (!balances) return
-        // ignore balances from old subscriptions which are still in the process of unsubscribing
-        if (abort.signal.aborted) return
-
-        updateBalances(balances)
-      })
+      )
 
       return () => unsub.then((unsubscribe) => unsubscribe())
     })
