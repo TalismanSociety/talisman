b7f76f4261a0878ff2931115494e5c853cfe7e50
Chid Gilovitz <chid@talisman.xyz>
2024-03-26 14:58:56 +0800

    chore: add whats new content for 1.24.0

---
+++ apps/extension/src/ui/apps/popup/pages/Portfolio/assets/whats-new/v1.24.0/index.ts
@@ -0,0 +1,12 @@
+import { WhatsNewVersionData } from "../types"
+import content from "./content.md"
+import HeroUrl from "./Hero.png"
+
+const content1240: WhatsNewVersionData = {
+  "1.24.0": {
+    content,
+    HeroUrl,
+  },
+}
+
+export default content1240
