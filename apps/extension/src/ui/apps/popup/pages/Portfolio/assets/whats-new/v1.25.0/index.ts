d925b20a840695a83a12b9b1500e3e7d8670cade
Chid Gilovitz <chid@talisman.xyz>
2024-04-17 13:44:54 +0800

    fix: enable whats new without image

---
+++ apps/extension/src/ui/apps/popup/pages/Portfolio/assets/whats-new/v1.25.0/index.ts
@@ -0,0 +1,10 @@
+import { WhatsNewVersionData } from "../types"
+import content from "./content.md"
+
+const content1250: WhatsNewVersionData = {
+  "1.25.0": {
+    content,
+  },
+}
+
+export default content1250
