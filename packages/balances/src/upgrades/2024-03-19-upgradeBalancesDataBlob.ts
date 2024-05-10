9b2d7471129ac1782989b36ad8bf34a570e05ba9
Chid Gilovitz <chid@talisman.xyz>
2024-05-10 12:18:10 +0800

    fix: minor balances migration

--- packages/balances/src/upgrades/2024-03-19-upgradeBalancesDataBlob.ts
+++ packages/balances/src/upgrades/2024-03-19-upgradeBalancesDataBlob.ts
@@ -3,11 +3,18 @@ import pako from "pako"
 
 import { BalanceJson } from "../types"
 
-// for DB version 4, Wallet version 1.24.0
+// for DB version 4, Wallet version 1.26.0
 // converts the structured balances data to a single compressed string
 export const upgradeBalancesDataBlob = async (tx: Transaction) => {
   const balancesTable = tx.table<BalanceJson & { id: string }, string>("balances")
   const balancesData = await balancesTable.toCollection().toArray()
+  // migrate to new format
+  const complexBalanceTypes = ["substrate-native", "substrate-tokens", "substrate-assets"]
+  balancesData.forEach((balance) => {
+    if (complexBalanceTypes.includes(balance.source)) {
+      balance.values = []
+    }
+  })
   const output = pako.deflate(JSON.stringify(balancesData))
   // now write the compressed data back to the db and clear the old one
   await tx.table("balancesBlob").put({ data: output, id: Date.now().toString() })
