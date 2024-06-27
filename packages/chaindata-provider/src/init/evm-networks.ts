e23ccf27dcbf851214b3a093037dc8a1bd8bfbaf
Chid Gilovitz <chid@talisman.xyz>
2024-06-27 16:43:10 +0800

    chore: package version bump and update init data

--- packages/chaindata-provider/src/init/evm-networks.ts
+++ packages/chaindata-provider/src/init/evm-networks.ts
@@ -2,7 +2,7 @@ export const evmNetworks = [
   {
     id: "1",
     isTestnet: false,
-    sortIndex: 264,
+    sortIndex: 265,
     name: "Ethereum Mainnet",
     themeColor: "#62688f",
     logo: "https://raw.githubusercontent.com/TalismanSociety/chaindata/main/assets/chains/1.svg",
