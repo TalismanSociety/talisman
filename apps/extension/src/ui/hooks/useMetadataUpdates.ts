383e86d40911e1a04e433a16c0f8dbb37c910091
Kheops <26880866+0xKheops@users.noreply.github.com>
2024-07-15 14:01:53 +0900

    fix: manual metadata update requirement indicator

--- apps/extension/src/ui/hooks/useMetadataUpdates.ts
+++ apps/extension/src/ui/hooks/useMetadataUpdates.ts
@@ -2,42 +2,26 @@ import { db } from "@extension/core"
 import { HexString } from "@polkadot/util/types"
 import { api } from "@ui/api"
 import { useLiveQuery } from "dexie-react-hooks"
-import { useEffect, useState } from "react"
-import { useDebounce } from "react-use"
+import { useEffect, useMemo, useState } from "react"
 
 import { useChainByGenesisHash } from "./useChainByGenesisHash"
 
-type ChainMetadata = {
-  isReady: boolean
-  isKnownChain: boolean
-  hasMetadata: boolean
-  isMetadataUpToDate: boolean
-  isMetadataUpdating: boolean
-  hasMetadataUpdateFailed: boolean
-  requiresUpdate: boolean
-  updateUrl?: string
-}
+const useMetadata = (genesisHash?: HexString) => {
+  const metadata = useLiveQuery(
+    async () => (genesisHash ? (await db.metadata.get(genesisHash)) ?? null : null),
+    [genesisHash]
+  )
 
-const DEFAULT_VALUE: ChainMetadata = {
-  isReady: false,
-  isKnownChain: false,
-  hasMetadata: false,
-  isMetadataUpToDate: false,
-  isMetadataUpdating: false,
-  hasMetadataUpdateFailed: false,
-  requiresUpdate: false,
+  return { isLoaded: metadata !== undefined, metadata: metadata ?? null }
 }
 
 export const useMetadataUpdates = (genesisHash?: HexString, specVersion?: number) => {
+  const chain = useChainByGenesisHash(genesisHash)
+
   const [isMetadataUpdating, setIsMetadataUpdating] = useState(false)
   const [hasMetadataUpdated, setHasMetadataUpdated] = useState(false)
 
-  const chain = useChainByGenesisHash(genesisHash)
-
-  const metadata = useLiveQuery(
-    () => (genesisHash ? db.metadata.get(genesisHash) : undefined),
-    [genesisHash]
-  )
+  const { isLoaded, metadata } = useMetadata(genesisHash)
 
   // listen for metadata updates from backend
   useEffect(() => {
@@ -52,41 +36,26 @@ export const useMetadataUpdates = (genesisHash?: HexString, specVersion?: number
     })
   }, [genesisHash])
 
-  // there is a short delay after a metadata update for the chain and metadata available here to be updated
-  // debouncing prevents false positives
-  const [result, setResult] = useState(DEFAULT_VALUE)
-  useDebounce(
-    () => {
-      const hasMetadata = !!metadata
-      const isMetadataUpToDate =
-        specVersion === undefined ? !!metadata : metadata?.specVersion === specVersion
-      const rpcUrl = chain?.rpcs?.[0]?.url
-      const updateUrl = rpcUrl
-        ? `https://polkadot.js.org/apps/?rpc=${encodeURIComponent(rpcUrl)}#/settings/metadata`
-        : undefined
+  return useMemo(() => {
+    const isMetadataUpToDate =
+      specVersion === undefined ? !!metadata : metadata?.specVersion === specVersion
+    const rpcUrl = chain?.rpcs?.[0]?.url
+    const updateUrl = rpcUrl
+      ? `https://polkadot.js.org/apps/?rpc=${encodeURIComponent(rpcUrl)}#/settings/metadata`
+      : undefined
 
-      const isKnownChain = !!chain
+    const isKnownChain = !!chain
 
-      // consider ready to sign either if we can't update or if an update has been attempted.
-      const isReady = !chain || isMetadataUpToDate || hasMetadataUpdated
-      const hasMetadataUpdateFailed = hasMetadataUpdated && !isMetadataUpToDate
+    const hasMetadataUpdateFailed = hasMetadataUpdated && !isMetadataUpToDate
 
-      const requiresUpdate = !isMetadataUpToDate
+    const requiresUpdate = isLoaded && !isMetadataUpToDate
 
-      setResult({
-        isReady,
-        isKnownChain,
-        hasMetadata,
-        isMetadataUpToDate,
-        isMetadataUpdating,
-        hasMetadataUpdateFailed,
-        updateUrl,
-        requiresUpdate,
-      })
-    },
-    1000,
-    [chain, hasMetadataUpdated, isMetadataUpdating, metadata, specVersion]
-  )
-
-  return result
+    return {
+      isKnownChain,
+      isMetadataUpdating,
+      hasMetadataUpdateFailed,
+      updateUrl,
+      requiresUpdate,
+    }
+  }, [chain, hasMetadataUpdated, isLoaded, isMetadataUpdating, metadata, specVersion])
 }
