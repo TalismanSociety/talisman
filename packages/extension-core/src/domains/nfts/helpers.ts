5583d15ec104252607b16a4eb242f9d51d3799b8
Kheops <26880866+0xKheops@users.noreply.github.com>
2024-07-31 11:26:43 +0900

    feat/nft-analytics (#1556)
    * feat: nfts in general report
    
    * feat: nft page views analytics
    
    * feat: search bar size

--- packages/extension-core/src/domains/nfts/helpers.ts
+++ packages/extension-core/src/domains/nfts/helpers.ts
@@ -3,6 +3,7 @@ import { isEthereumAddress } from "@talismn/util"
 import { firstValueFrom } from "rxjs"
 
 import { activeEvmNetworksObservable } from "../balances/pool"
+import { NftCollection } from "./types"
 
 /**
  * IMPORTANT : make sure keyring is loaded before calling this
@@ -19,4 +20,13 @@ export const getNftsAccountsList = () => {
 
 export const getNftsNetworkIdsList = async () => {
   return (await firstValueFrom(activeEvmNetworksObservable)).map((n) => n.id).sort()
+}
+
+export const getNftCollectionFloorUsd = (collection: NftCollection): number | null => {
+  return (
+    collection.marketplaces
+      .filter((m) => m.floorUsd)
+      .map((mp) => mp.floorUsd ?? 0)
+      .sort((a, b) => a - b)[0] ?? null
+  )
 }
