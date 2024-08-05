994fb6ff5c3dc1709a062ddd30d126322fe1134e
alecdwm <alec@owls.io>
2024-08-05 05:38:14 +0000

    chore: tidy up analytics

--- packages/extension-core/src/config/posthog.ts
+++ packages/extension-core/src/config/posthog.ts
@@ -30,26 +30,53 @@ const talismanProperties = {
   testBuild: DEBUG || ["dev", "qa", "ci"].includes(process.env.BUILD as string),
 }
 
+/**
+ * Initializes the posthog client.
+ *
+ * It is recommended to call `setPosthogOptInPreference` with the user's opt-in preference before calling this.
+ */
 export const initPosthog = () => {
-  if (process.env.POSTHOG_AUTH_TOKEN) {
-    posthog.init(process.env.POSTHOG_AUTH_TOKEN, {
-      api_host: "https://app.posthog.com",
-      autocapture: false,
-      capture_pageview: false,
-      disable_session_recording: true,
-      persistence: "localStorage",
-      ip: false,
-      sanitize_properties: (properties) => {
-        // We can remove all the posthog user profiling properties except for those that are required for PostHog to work
-        const requiredProperties = Object.keys(properties).reduce((result, key) => {
-          if (!unsafeProperties.includes(key)) result[key] = properties[key]
-          return result
-        }, {} as Properties)
-        return {
-          ...requiredProperties,
-          ...talismanProperties,
-        }
-      },
-    })
+  if (!process.env.POSTHOG_AUTH_TOKEN) return
+
+  posthog.init(process.env.POSTHOG_AUTH_TOKEN, {
+    api_host: "https://app.posthog.com",
+    autocapture: false,
+    capture_pageview: false,
+    disable_session_recording: true,
+    persistence: "localStorage",
+    ip: false,
+    sanitize_properties: (properties) => {
+      // We can remove all the posthog user profiling properties except for those that are required for PostHog to work
+      const requiredProperties = Object.keys(properties).reduce((result, key) => {
+        if (!unsafeProperties.includes(key)) result[key] = properties[key]
+        return result
+      }, {} as Properties)
+      return {
+        ...requiredProperties,
+        ...talismanProperties,
+      }
+    },
+  })
+}
+
+/**
+ * Configures the user's opt-in preference on the posthog client.
+ *
+ * @param optInPreference `true` if the user has explicitly opted in, `false` if the user has explicitly opted out, otherwise `undefined`.
+ */
+export const setPosthogOptInPreference = (optInPreference: boolean | undefined) => {
+  const posthogOptedIn = posthog.has_opted_in_capturing()
+  const posthogOptedOut = posthog.has_opted_out_capturing()
+
+  switch (optInPreference) {
+    case true:
+      if (!posthogOptedIn || posthogOptedOut) posthog.opt_in_capturing()
+      break
+    case false:
+      if (posthogOptedIn || !posthogOptedOut) posthog.opt_out_capturing()
+      break
+    case undefined:
+      if (posthogOptedIn || posthogOptedOut) posthog.clear_opt_in_out_capturing()
+      break
   }
 }
---
+++ packages/extension-core/src/libs/GeneralReport.ts
@@ -0,0 +1,192 @@
+import keyring from "@polkadot/ui-keyring"
+import { DEBUG } from "extension-shared"
+import groupBy from "lodash/groupBy"
+import { type Properties } from "posthog-js"
+
+import { db } from "../db"
+import { AccountType } from "../domains/accounts/types"
+import { appStore } from "../domains/app/store.app"
+import { settingsStore } from "../domains/app/store.settings"
+import { balancePool } from "../domains/balances/pool"
+import { Balances } from "../domains/balances/types"
+import { getNftCollectionFloorUsd } from "../domains/nfts"
+import { nftsStore$ } from "../domains/nfts/store"
+import { chaindataProvider } from "../rpcs/chaindata"
+import { hasGhostsOfThePast } from "../util/hasGhostsOfThePast"
+import { roundToFirstInteger } from "../util/roundToFirstInteger"
+
+const REPORTING_PERIOD = 24 * 3600 * 1000 // 24 hours
+
+//
+// This should get sent at most once per 24 hours, whenever any other events get sent
+//
+export async function withGeneralReport(properties?: Properties) {
+  const analyticsReportSent = await appStore.get("analyticsReportSent")
+
+  // on first run, note down the timestamp but don't make a report
+  if (typeof analyticsReportSent !== "number") {
+    await appStore.set({ analyticsReportSent: Date.now() })
+    return properties
+  }
+
+  // on subsequent runs, do nothing if the diff between the recorded timestamp and now is less than REPORTING_PERIOD
+  if (Date.now() - analyticsReportSent < REPORTING_PERIOD) return properties
+
+  // and when the diff is greater, make a report
+  const generalReport = await getGeneralReport()
+  await appStore.set({ analyticsReportSent: Date.now() })
+  return { ...properties, $set: { ...(properties?.$set ?? {}), ...generalReport } }
+}
+
+async function getGeneralReport() {
+  const [allowTracking, onboarded] = await Promise.all([
+    settingsStore.get("useAnalyticsTracking"),
+    appStore.getIsOnboarded(),
+  ])
+  if (!allowTracking || !onboarded) return
+
+  //
+  // accounts
+  //
+
+  const accounts = keyring.getAccounts()
+
+  const ownedAccounts = accounts.filter(({ meta }) => meta.origin !== AccountType.Watched)
+  const ownedAccountsCount = ownedAccounts.length
+  const ownedAddresses = ownedAccounts.map((account) => account.address)
+  const ownedAddressesLower = ownedAddresses.map((a) => a.toLowerCase())
+
+  const watchedAccounts = accounts.filter(({ meta }) => meta.origin === AccountType.Watched)
+  const watchedAccountsCount = watchedAccounts.length
+
+  // account type breakdown
+  const accountBreakdown: Record<Lowercase<AccountType>, number> = {
+    talisman: 0,
+    qr: 0,
+    ledger: 0,
+    dcent: 0,
+    watched: 0,
+    signet: 0,
+  }
+  for (const account of accounts) {
+    const origin = account.meta.origin as AccountType | undefined
+    const type = origin?.toLowerCase?.() as Lowercase<AccountType> | undefined
+    if (type) accountBreakdown[type] = (accountBreakdown[type] ?? 0) + 1
+  }
+
+  //
+  // tokens
+  //
+
+  // cache chains, evmNetworks, tokens, tokenRates and balances here to prevent lots of fetch calls
+  try {
+    /* eslint-disable-next-line no-var */
+    var [chains, evmNetworks, tokens, tokenRates] = await Promise.all([
+      chaindataProvider.chainsById(),
+      chaindataProvider.evmNetworksById(),
+      chaindataProvider.tokensById(),
+      db.tokenRates
+        .toArray()
+        .then((dbTokenRates) =>
+          Object.fromEntries((dbTokenRates ?? []).map(({ tokenId, rates }) => [tokenId, rates]))
+        ),
+    ])
+
+    /* eslint-disable-next-line no-var */
+    var balances = new Balances(
+      Object.values(balancePool.balances).filter((balance) =>
+        ownedAddresses.includes(balance.address)
+      ),
+      { chains, evmNetworks, tokens, tokenRates }
+    )
+  } catch (cause) {
+    const error = new Error("Failed to access db to build general analyics report", { cause })
+    DEBUG && console.error(error) // eslint-disable-line no-console
+    throw error
+  }
+
+  // balances top 10 tokens/networks
+  const TOP_BALANCES_COUNT = 10
+  // get balance list per chain/evmNetwork and token
+  const balancesPerChainToken = groupBy(
+    balances.each.filter(
+      (balance) =>
+        balance &&
+        (balance.chain === null || !("isCustom" in balance.chain && balance.chain.isCustom)) &&
+        (balance.token === null || !("isCustom" in balance.token && balance.token.isCustom))
+    ),
+    (balance) => `${balance.chainId ?? balance.evmNetworkId}-${balance.tokenId}`
+  )
+
+  // get fiat sum object for those arrays of balances
+  const sortedFiatSumPerChainToken = Object.values(balancesPerChainToken)
+    .map((balances) => new Balances(balances, { chains, evmNetworks, tokens, tokenRates }))
+    .map((balances) => ({
+      balance: balances.sum.fiat("usd").total,
+      chainId: balances.sorted[0].chainId ?? balances.sorted[0].evmNetworkId,
+      tokenId: balances.sorted[0].tokenId,
+    }))
+    .sort((a, b) => b.balance - a.balance)
+
+  const totalFiatValue = roundToFirstInteger(balances.sum.fiat("usd").total)
+  const tokensBreakdown = sortedFiatSumPerChainToken
+    .filter((token, index) => token.balance > 1 || index < TOP_BALANCES_COUNT)
+    .map((token) => ({ ...token, balance: roundToFirstInteger(token.balance) }))
+
+  const topChainTokens = sortedFiatSumPerChainToken
+    .filter(({ balance }) => balance > 0)
+    .map(({ chainId, tokenId }) => ({ chainId, tokenId }))
+    .slice(0, 5)
+
+  const topToken = topChainTokens[0]
+    ? `${topChainTokens[0].chainId}: ${topChainTokens[0].tokenId}`
+    : undefined
+
+  //
+  // nfts
+  //
+
+  const hasGhosts = await hasGhostsOfThePast()
+  const hasGhostsNft = Object.values(hasGhosts).some((g) => g)
+
+  const ownedNfts = nftsStore$.value.nfts.filter((nft) =>
+    nft.owners.some((o) => ownedAddressesLower.includes(o.address.toLowerCase()))
+  )
+  const ownedCollections = nftsStore$.value.collections.filter((c) =>
+    ownedNfts.some((n) => n.collectionId === c.id)
+  )
+
+  const nftsCount = ownedNfts.length
+  const floorByCollectionId = Object.fromEntries(
+    ownedCollections
+      .map((collection) => [collection.id, getNftCollectionFloorUsd(collection)] as const)
+      .filter(([, floor]) => !!floor)
+  )
+  const nftsTotalValue = ownedNfts.reduce(
+    (total, nft) => total + (floorByCollectionId[nft.collectionId] ?? 0),
+    0
+  )
+  const topNftCollections = Object.entries(floorByCollectionId)
+    .sort((c1, c2) => (c2[1] ?? 0) - (c1[1] ?? 0))
+    .slice(0, 20)
+    .map(([collectionId]) => ownedCollections.find((c) => c.id === collectionId)?.name)
+
+  return {
+    // accounts
+    accountBreakdown,
+    accountsCount: ownedAccountsCount,
+    watchedAccountsCount,
+
+    // tokens
+    totalFiatValue,
+    tokens: tokensBreakdown,
+    topChainTokens,
+    topToken,
+
+    // nfts
+    nftsCount,
+    nftsTotalValue,
+    topNftCollections,
+    hasGhostsOfThePast: hasGhostsNft,
+  }
+}
