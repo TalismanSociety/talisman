8a20af3f05e8d7472e9cafe929170ce6fa5cfeea
alecdwm <alec@owls.io>
2024-08-09 04:10:59 +0000

    fix: privacyRoundCurrency tests

--- packages/extension-core/src/util/privacyRoundCurrency.ts
+++ packages/extension-core/src/util/privacyRoundCurrency.ts
@@ -20,11 +20,11 @@ import BigNumber from "bignumber.js"
  */
 export const privacyRoundCurrency = (amount: number) => {
   // For amounts at or below 1, round to 1 significant digit
-  if (amount <= 1) return BigNumber(amount).precision(1).toString()
+  if (amount <= 1) return BigNumber(amount).precision(1).toString(10)
 
   // For amounts below 1,000, round to the nearest whole number
-  if (amount < 1_000) return Math.round(amount).toString()
+  if (amount < 1_000) return BigNumber(amount).toFixed(0)
 
   // For amounts at 1,000 and above, round to 3 significant digits
-  return BigNumber(amount).precision(3).toString()
+  return BigNumber(amount).precision(3).toString(10)
 }
