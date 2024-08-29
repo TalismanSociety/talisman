f2680c852284284c13510e899738bdeaade0a8e1
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-08-29 09:36:07 +0900

    chore: cleanup

--- apps/extension/src/ui/domains/Staking/InlineStakingForm.tsx
+++ apps/extension/src/ui/domains/Staking/InlineStakingForm.tsx
@@ -41,16 +41,14 @@ const AssetPill: FC<{ token: Token | null }> = ({ token }) => {
   if (!token) return null
 
   return (
-    <PillButton className="h-16 rounded px-4">
-      <div className="flex items-center gap-4">
-        <TokenLogo tokenId={token.id} className="shrink-0 text-lg" />
-        <div className="flex items-center gap-2">
-          <div className="text-body text-base">{token.symbol}</div>
-          <div className="bg-body-disabled inline-block size-2 rounded-full"></div>
-          <div className="text-body-secondary text-sm">{t("Pooled Staking")}</div>
-        </div>
+    <div className="flex h-16 items-center gap-4 px-4">
+      <TokenLogo tokenId={token.id} className="shrink-0 text-lg" />
+      <div className="flex items-center gap-2">
+        <div className="text-body text-base">{token.symbol}</div>
+        <div className="bg-body-disabled inline-block size-2 rounded-full"></div>
+        <div className="text-body-secondary text-sm">{t("Pooled Staking")}</div>
       </div>
-    </PillButton>
+    </div>
   )
 }
 
@@ -78,8 +76,6 @@ const DisplayContainer: FC<PropsWithChildren> = ({ children }) => {
 const FiatDisplay = () => {
   const currency = useSelectedCurrency()
   const { tokenRates, formatter } = useInlineStakingWizard()
-
-  // const value = sendMax ? maxAmount : transfer
 
   if (!tokenRates) return null
 
@@ -146,13 +142,7 @@ const TokenInput = () => {
   }, [resizeTokensInput, token?.symbol])
 
   return (
-    <div
-      className={classNames(
-        "flex w-full max-w-[400px] flex-nowrap items-center justify-center gap-4"
-        //isEstimatingMaxAmount && "animate-pulse"
-      )}
-    >
-      {/* {isEstimatingMaxAmount && <div className="bg-grey-800 h-16 w-48 rounded"></div>} */}
+    <div className={"flex w-full max-w-[400px] flex-nowrap items-center justify-center gap-4"}>
       <input
         key="tokenInput"
         ref={refTokensInput}
@@ -160,18 +150,13 @@ const TokenInput = () => {
         inputMode="decimal"
         defaultValue={defaultValue}
         placeholder="0"
-        className={classNames(
-          "text-body peer inline-block w-fit min-w-0 text-ellipsis bg-transparent text-xl"
-          // sendMax && "placeholder:text-white",
-          // isEstimatingMaxAmount && "hidden" // hide until value is known
-        )}
+        className={"text-body peer inline-block w-fit min-w-0 text-ellipsis bg-transparent text-xl"}
         onChange={handleChange}
       />
       <div className="text-body flex shrink-0 items-center gap-2 text-base font-normal">
         <TokenLogo className="text-lg" tokenId={token?.id} />
         <div>{token?.symbol}</div>
       </div>
-      {/* <TokenPillButton tokenId={tokenId} onClick={onTokenClick} /> */}
     </div>
   )
 }
@@ -179,33 +164,12 @@ const TokenInput = () => {
 const FiatInput = () => {
   const { token, tokenRates, formatter, setPlancks } = useInlineStakingWizard()
   const currency = useSelectedCurrency()
-  //const currencyConfig = useCurren
+
   const defaultValue = useMemo(() => {
     const val = formatter?.fiat(currency) ?? ""
     return val ? String(Number(val.toFixed(2))) : val
   }, [currency, formatter])
-  // const {
-  //   token,
-  //   transfer,
-  //   maxAmount,
-  //   tokenRates,
-  //   isEstimatingMaxAmount,
-  //   refFiatInput,
-  //   resizeFiatInput,
-  // } = useSendFunds()
 
-  // const currency = useSelectedCurrency()
-
-  // const defaultValue = useMemo(
-  //   () =>
-  //     normalizeStringNumber(
-  //       sendMax && maxAmount ? maxAmount.fiat(currency) : transfer?.fiat(currency),
-  //       2
-  //     ),
-  //   [currency, maxAmount, sendMax, transfer]
-  // )
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   const handleChange: ChangeEventHandler<HTMLInputElement> = useCallback(
     (e) => {
       if (token && tokenRates?.[currency] && e.target.value) {
@@ -221,19 +185,7 @@ const FiatInput = () => {
 
       return setPlancks(null)
     },
-    // debounce((e) => {
-    //   if (sendMax) set("sendMax", false)
 
-    //   const text = e.target.value ?? ""
-    //   const num = Number(text)
-    //   const tokenRate = tokenRates?.[currency]
-
-    //   if (token && tokenRate && text.length && !isNaN(num)) {
-    //     const fiat = parseFloat(text)
-    //     const tokens = (fiat / tokenRate).toFixed(Math.ceil(token.decimals / 3))
-    //     set("amount", tokensToPlanck(tokens, token.decimals))
-    //   } else remove("amount")
-    // }, 250),
     [currency, setPlancks, token, tokenRates]
   )
 
@@ -257,35 +209,20 @@ const FiatInput = () => {
 
   return (
     <div
-      className={classNames(
-        // display flex in reverse order to leverage peer css
-        "end flex w-full max-w-[400px] flex-row-reverse flex-nowrap items-center justify-center"
-        // isEstimatingMaxAmount && "animate-pulse"
-      )}
+      // display flex in reverse order to leverage peer css
+      className="end flex w-full max-w-[400px] flex-row-reverse flex-nowrap items-center justify-center"
     >
       <input
         key="fiatInput"
         ref={refFiatInput}
         type="text"
         defaultValue={defaultValue}
-        // eslint-disable-next-line jsx-a11y/no-autofocus
-        //autoFocus={!sendMax && !transfer}
         placeholder={"0.00"}
-        className={classNames(
-          "text-body peer inline-block min-w-0 bg-transparent text-xl"
-          //   isEstimatingMaxAmount && "hidden" // hide until value is known
-        )}
+        className="text-body peer inline-block min-w-0 bg-transparent text-xl"
         onChange={handleChange}
       />
       {/* {isEstimatingMaxAmount && <div className="bg-grey-800 h-16 w-48 rounded"></div>} */}
-      <div
-        className={classNames(
-          "block shrink-0"
-          // isEstimatingMaxAmount ? "text-grey-800" : "peer-placeholder-shown:text-body-disabled"
-        )}
-      >
-        {currencyConfig[currency]?.symbol}
-      </div>
+      <div className="block shrink-0">{currencyConfig[currency]?.symbol}</div>
     </div>
   )
 }
@@ -330,7 +267,7 @@ export const AmountEdit = () => {
             )}
             <PillButton
               onClick={onSetMaxClick}
-              disabled={!maxPlancks} // TODO ={!maxAmount}
+              disabled={!maxPlancks}
               size="xs"
               className={classNames("h-[2.2rem] rounded-sm !px-4 !py-0")}
             >
@@ -398,8 +335,6 @@ const durationFromMs = (ms: number): Duration => {
     hours: hours % 24,
     days: days,
   }
-
-  //console.log("durationFromMs", { ms, seconds, minutes, hours, days, weeks, months, years, result })
 
   return result
 }
