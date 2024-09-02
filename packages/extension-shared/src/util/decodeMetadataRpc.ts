4f1f3d11079ecbf7789b4c054f2b30b252faf1d6
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-09-02 15:08:39 +0900

    feat: remove some pjs

--- packages/extension-shared/src/util/decodeMetadataRpc.ts
+++ packages/extension-shared/src/util/decodeMetadataRpc.ts
@@ -1,8 +1,2 @@
-import { u8aToHex } from "@polkadot/util"
-import { base64Decode } from "@polkadot/util-crypto"
-
-// TODO try this instead // Buffer.from(metadataDef.metadataRpc, "base64").toString("hex")
-//export const decodeMetadataRpc = (encoded: string) => Buffer.from(metadataDef.metadataRpc, "base64").toString("hex")
-
-// these are stored as base64 for lower storage size
-export const decodeMetadataRpc = (encoded: string) => u8aToHex(base64Decode(encoded))
+export const decodeMetadataRpc = (encoded: string) =>
+  Buffer.from(encoded, "base64").toString("hex") as `0x${string}`
