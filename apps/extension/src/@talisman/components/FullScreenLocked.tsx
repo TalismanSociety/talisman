f8a64ad935e45965edd1692dea2b6b389550ce38
alecdwm <alec@owls.io>
2024-10-08 00:57:29 +0000

    fix: deoddify

--- apps/extension/src/@talisman/components/FullScreenLocked.tsx
+++ apps/extension/src/@talisman/components/FullScreenLocked.tsx
@@ -1,8 +1,7 @@
 import { classNames } from "@talismn/util"
 import { ReactNode } from "react"
 
-import { HandMonoLogo, HandMonoSilhouetteLogo } from "@talisman/theme/logos"
-import { api } from "@ui/api"
+import { HandMonoLogo } from "@talisman/theme/logos"
 
 import { FadeIn } from "./FadeIn"
 
@@ -16,39 +15,15 @@ export const FullScreenLocked = ({ className, title, subtitle }: Props) => (
   <FadeIn className="flex h-screen w-screen flex-col items-center justify-center">
     <section
       className={classNames(
-        "text-body-secondary flex flex-col items-center",
-        "group cursor-pointer select-none",
+        "text-body-secondary flex select-none flex-col items-center",
         className
       )}
-      role="button"
-      tabIndex={0}
-      onClick={() => api.promptLogin()}
-      onKeyDown={(e) => ["Enter", " "].includes(e.key) && api.promptLogin()}
     >
       <div className="relative">
-        <HandMonoSilhouetteLogo
-          className={classNames(
-            "mb-8 block text-[12rem] text-white",
-            "opacity-100 transition-opacity duration-100 ease-out",
-            "group-focus-within:opacity-0 group-focus-within:ease-in group-hover:opacity-0 group-hover:ease-in group-focus:opacity-0 group-focus:ease-in",
-            "group-active:text-brand-orange group-active:opacity-0"
-          )}
-        />
-        <HandMonoLogo
-          className={classNames(
-            "absolute left-0 top-0 mb-8 block text-[12rem] text-white",
-            "opacity-0 transition-opacity duration-100 ease-in",
-            "group-focus-within:opacity-100 group-focus-within:ease-out group-hover:opacity-100 group-hover:ease-out group-focus:opacity-100 group-focus:ease-out",
-            "group-active:text-brand-orange group-active:opacity-100"
-          )}
-        />
+        <HandMonoLogo className={classNames("mb-8 block text-[12rem] text-white")} />
       </div>
-      {title && (
-        <h1 className="text-md text-grey-300 group-active:text-brand-orange mb-2 font-bold">
-          {title}
-        </h1>
-      )}
-      {subtitle && <h2 className="group-active:text-brand-orange text-xs">{subtitle}</h2>}
+      {title && <h1 className="text-md text-grey-300 mb-2 font-bold">{title}</h1>}
+      {subtitle && <h2 className="text-xs">{subtitle}</h2>}
     </section>
   </FadeIn>
 )
