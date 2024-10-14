fc0409edbf39c964c14b6875922b505a5cec9e2a
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-10-14 12:21:48 +0900

    feat: remove spinning hand / adjust styles on change password

--- apps/extension/src/ui/apps/dashboard/routes/Settings/ChangePasswordPage/ChangePasswordModal.tsx
+++ apps/extension/src/ui/apps/dashboard/routes/Settings/ChangePasswordPage/ChangePasswordModal.tsx
@@ -1,9 +1,9 @@
+import { AlertTriangleIcon } from "@talismn/icons"
 import { useMemo } from "react"
 import { useTranslation } from "react-i18next"
-import { Modal, ModalDialog } from "talisman-ui"
+import { Modal, ModalDialog, ProcessAnimation } from "talisman-ui"
 
 import { ChangePasswordStatusUpdateStatus, ChangePasswordStatusUpdateType } from "@extension/core"
-import { StatusIcon } from "@talisman/components/StatusIcon"
 
 export const ChangePasswordModal = ({
   isOpen,
@@ -32,15 +32,18 @@ export const ChangePasswordModal = ({
 
   return (
     <Modal isOpen={isOpen}>
-      <ModalDialog title={t("Changing password")} centerTitle>
-        <StatusIcon className="text-3xl" status="SPINNING" />
-        <div className="mt-4 flex flex-col gap-5 text-center">
-          <p className="animate-pulse">{progressDisplay}</p>
-          <p className="text-alert-warn border-alert-warn rounded border p-2 text-sm">
-            {t(
-              "Please wait while we change your password. This can take some time. Do not close the browser window."
-            )}
-          </p>
+      <ModalDialog title={t("Changing password")} centerTitle className="w-[40rem]">
+        <ProcessAnimation status="processing" className="my-8 h-[14rem]" />
+        <div className="flex flex-col gap-5 ">
+          <p className="my-8 animate-pulse text-center">{progressDisplay}</p>
+          <div className="bg-alert-warn/10 text-alert-warn flex items-center gap-6 rounded p-4 px-6 text-sm">
+            <AlertTriangleIcon className="text-alert-warn shrink-0 text-lg" />
+            <div>
+              {t(
+                "Please wait while we change your password. This can take some time. Do not close the browser window."
+              )}
+            </div>
+          </div>
         </div>
       </ModalDialog>
     </Modal>
