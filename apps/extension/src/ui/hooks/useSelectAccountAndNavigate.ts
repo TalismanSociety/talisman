b011a8449250968710c0c6219b51f111122bc36a
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-10-15 09:48:28 +0900

    fix: create account portfolio flicker

--- apps/extension/src/ui/hooks/useSelectAccountAndNavigate.ts
+++ apps/extension/src/ui/hooks/useSelectAccountAndNavigate.ts
@@ -1,12 +1,16 @@
+import { sleep } from "@talismn/util"
 import { useCallback } from "react"
 import { useNavigate } from "react-router-dom"
 
-// TODO remove
 export const useSelectAccountAndNavigate = (url: string) => {
   const navigate = useNavigate()
 
   const setAddress = useCallback(
-    (address: string) => {
+    async (address: string) => {
+      // this method is used to navigate to the portfolio page with a newly created account
+      // wait for accounts subscription to be updated on frontend before proceeding or it will flicker
+      await sleep(250)
+
       const searchParams = new URLSearchParams({
         account: address,
       })
