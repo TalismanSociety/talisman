f0288890e04b73deb46a7ed339016252902e5cf0
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-10-15 18:18:32 +0900

    fix: setAddress to wait for account to exist

--- apps/extension/src/ui/hooks/useSelectAccountAndNavigate.ts
+++ apps/extension/src/ui/hooks/useSelectAccountAndNavigate.ts
@@ -1,23 +1,36 @@
-import { sleep } from "@talismn/util"
-import { useCallback } from "react"
+import { isAddressEqual } from "@talismn/util"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
+
+import useAccounts from "./useAccounts"
 
 export const useSelectAccountAndNavigate = (url: string) => {
   const navigate = useNavigate()
 
-  const setAddress = useCallback(
-    async (address: string) => {
-      // this method is used to navigate to the portfolio page with a newly created account
-      // wait for accounts subscription to be updated on frontend before proceeding or it will flicker
-      await sleep(250)
+  const [address, setAddress] = useState<string>()
+  const accounts = useAccounts()
 
-      const searchParams = new URLSearchParams({
-        account: address,
-      })
-      navigate(`${url}?${searchParams}`)
-    },
-    [navigate, url]
-  )
+  useEffect(() => {
+    if (!address) return
+
+    const targetUrl = `${url}?${new URLSearchParams({ account: address })}`
+
+    let timeout: ReturnType<typeof setTimeout> | null
+
+    // redirect if account exists
+    if (accounts.some((a) => isAddressEqual(a.address, address))) {
+      navigate(targetUrl)
+    } else {
+      // if account still doesnt exist after 1 second, redirect anyway
+      timeout = setTimeout(() => {
+        navigate(targetUrl)
+      }, 1000)
+    }
+
+    return () => {
+      if (timeout !== null) clearTimeout(timeout)
+    }
+  }, [accounts, address, navigate, url])
 
   return { setAddress }
 }
