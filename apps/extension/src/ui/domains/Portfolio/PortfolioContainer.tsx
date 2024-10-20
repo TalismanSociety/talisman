33fbb4b9f61ecf1f7c09f684f2ce3dfe89dff62d
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-10-20 17:26:07 +0900

    feat: portfolio preload

--- apps/extension/src/ui/domains/Portfolio/PortfolioContainer.tsx
+++ apps/extension/src/ui/domains/Portfolio/PortfolioContainer.tsx
@@ -1,23 +1,28 @@
+import { bind } from "@react-rxjs/core"
 import { FC, ReactNode, useEffect } from "react"
+import { combineLatest } from "rxjs"
 
-import { portfolioSelectedAccounts$, usePortfolio } from "@ui/state"
+import {
+  accounts$,
+  accountsCatalog$,
+  balancesHydrate$,
+  portfolioSelectedAccounts$,
+  remoteConfig$,
+  stakingBannerStore$,
+  usePortfolio,
+} from "@ui/state"
 
 import { usePortfolioNavigation } from "./usePortfolioNavigation"
 
-// const preloadAtom = atom((get) =>
-//   Promise.all([
-//     get(accountsByCategoryAtomFamily("all")),
-//     get(remoteConfigAtom),
-//     get(balancesHydrateAtom),
-//     get(stakingBannerAtom),
-//   ])
-// )
+const [usePreload] = bind(
+  combineLatest([balancesHydrate$, accounts$, accountsCatalog$, remoteConfig$, stakingBannerStore$])
+)
 
 export const PortfolioContainer: FC<{ children: ReactNode; renderWhileLoading?: boolean }> = ({
   children,
   renderWhileLoading, // true in popup, false in dashboard
 }) => {
-  // useAtomValue(preloadAtom) TODO
+  usePreload()
 
   const { selectedAccounts } = usePortfolioNavigation()
   const { isProvisioned } = usePortfolio()
@@ -26,14 +31,9 @@ export const PortfolioContainer: FC<{ children: ReactNode; renderWhileLoading?: 
     portfolioSelectedAccounts$.next(selectedAccounts)
   }, [selectedAccounts])
 
-  // keeps portfolio sync atoms up to date with subscription async atoms
-  // const isProvisioned = usePortfolioProvisioning()
-
   // // on popup home page, portfolio is loading while we display the home page
   // // but on dashboard, don't render until portfolio is provisioned
   if (!renderWhileLoading && !isProvisioned) return null
-
-  // if (!isProvisioned) return null
 
   return <>{children}</>
 }
