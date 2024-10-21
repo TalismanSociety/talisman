18f6f5d7aa077c3fb1711543548f04d45ef87863
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-10-21 18:31:55 +0900

    feat: migration

--- packages/extension-core/src/libs/migrations/index.ts
+++ packages/extension-core/src/libs/migrations/index.ts
@@ -3,7 +3,7 @@ import {
   migrateToNewAccountTypes,
 } from "../../domains/accounts/migrations"
 import { migratePosthogDistinctIdToAnalyticsStore } from "../../domains/analytics/migrations"
-import { cleanBadContacts } from "../../domains/app/migrations"
+import { cleanBadContacts, hideGetStartedIfFunded } from "../../domains/app/migrations"
 import { migrateAssetDiscoveryRollout } from "../../domains/assetDiscovery/migrations"
 import { migrateToNewDefaultEvmNetworks } from "../../domains/ethereum/migrations"
 import { migrateSeedStoreToMultiple } from "../../domains/mnemonics/migrations"
@@ -20,4 +20,10 @@ export const migrations: Migrations = [
   cleanBadContacts,
   migratePolkadotLedgerAccounts,
   migratePosthogDistinctIdToAnalyticsStore,
+  hideGetStartedIfFunded,
 ]
+
+// @dev snippet to use in dev console of background worker to remove a migration:
+// const state = await chrome.storage.local.get("migrations")
+// delete state.migrations["7"] // CHANGE THIS TO YOUR MIGRATION'S INDEX
+// await chrome.storage.local.set(state)
