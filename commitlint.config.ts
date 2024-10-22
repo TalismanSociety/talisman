dc44ac08423a76721882cc29630adea67984abd5
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-10-22 18:33:36 +0900

    chore: bump tooling deps

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,7 @@
-var configConventional = require("@commitlint/config-conventional")
+import configConventional from "@commitlint/config-conventional"
 
 // Add 'wip' to the list of allowed commit types
-var typeEnum = configConventional.rules["type-enum"]
+const typeEnum = configConventional.rules["type-enum"]
 typeEnum[2].push("wip")
 
 module.exports = {
