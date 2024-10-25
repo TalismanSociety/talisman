bf849d92e246c754a9aa95b76fd9786df0907cdd
alecdwm <alec@owls.io>
2024-10-25 06:29:34 +0000

    feat: include sources and .env file in firefox review builds

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ RUN corepack enable
 WORKDIR /talisman
 COPY . ./
 
+RUN pnpm clean
 RUN pnpm install
 ENV USE_ONE_DIST_DIR=true
---
+++ scripts/build-review-firefox.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+rm -rf review
+mkdir review
+docker build . --tag talisman-builder
+docker run --rm --volume "$(pwd)/review":/review talisman-builder bash -c ' \
+    NODE_OPTIONS=--max_old_space_size=8192 pnpm build:extension:prod:firefox && \
+    cp /talisman/apps/extension/dist/*.zip /review/ && \
+    rm -rf /talisman/apps/extension/dist && \
+    find /talisman/ -depth -type d -name node_modules -exec rm -rf {} \; && \
+    cp -r /talisman/ /review/sources \
+'
+cd review
+zip -r extension-and-sources.zip ./*
