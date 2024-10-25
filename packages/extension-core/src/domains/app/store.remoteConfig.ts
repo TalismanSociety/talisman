990328e5b2400220c1f101acf61ce7fcc3515e67
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-10-25 14:58:15 +0900

    chore: cleamup

--- packages/extension-core/src/domains/app/store.remoteConfig.ts
+++ packages/extension-core/src/domains/app/store.remoteConfig.ts
@@ -41,8 +41,6 @@ export class RemoteConfigStore extends StorageProvider<RemoteConfigStoreData> {
             config.coingecko.apiKeyName = process.env.COINGECKO_API_KEY_NAME
           if (process.env.COINGECKO_API_KEY_VALUE)
             config.coingecko.apiKeyValue = process.env.COINGECKO_API_KEY_VALUE
-
-          config.featureFlags.RISK_ANALYSIS = false
         }
 
         // first arg is an empty object so that DEFAULT_REMOTE_CONFIG is not mutated
