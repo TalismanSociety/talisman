13eb1a44faa92e9c613fb2cd87532f75eca4a820
Chid Gilovitz <chid@talisman.xyz>
2024-11-06 14:44:19 +0800

    fix: minor build issues for FF review

--- scripts/build-review-firefox.sh
+++ scripts/build-review-firefox.sh
@@ -6,10 +6,11 @@ mkdir review
 docker build . --tag talisman-builder
 docker run --rm --volume "$(pwd)/review":/review talisman-builder bash -c ' \
     NODE_OPTIONS=--max_old_space_size=8192 pnpm build:extension:prod:firefox && \
-    cp /talisman/apps/extension/dist/*.zip /review/ && \
+    cp /talisman/apps/extension/dist/firefox/*.zip /review/ && \
     rm -rf /talisman/apps/extension/dist && \
     find /talisman/ -depth -type d -name node_modules -exec rm -rf {} \; && \
-    cp -r /talisman/ /review/sources \
+    cp -r /talisman/ /review/sources && \
+    rm /talisman/sources/apps/extension/.env.local
 '
 cd review
 zip -r source.zip ./sources/
