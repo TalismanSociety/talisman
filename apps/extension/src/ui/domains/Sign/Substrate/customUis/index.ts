61abca28a98481704563a299c16fd06cc9d18a0d
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-11-13 16:09:08 +0900

    chore: yeet custom uis

--- apps/extension/src/ui/domains/Sign/Substrate/customUis/index.ts
+++ apps/extension/src/ui/domains/Sign/Substrate/customUis/index.ts
@@ -1,14 +1,15 @@
 import { DecodedCallComponentDefs } from "../types"
-import { CUSTOM_UI_CONVICTION_VOTING } from "./CustomUisConvictionVoting"
-import { CUSTOM_UI_NOMINATION_POOLS } from "./CustomUisNominationPools"
+// import { CUSTOM_UI_CONVICTION_VOTING } from "./CustomUisConvictionVoting"
+// import { CUSTOM_UI_NOMINATION_POOLS } from "./CustomUisNominationPools"
 import { CUSTOM_UI_UTILITY } from "./CustomUisUtility"
-import { CUSTOM_UI_XCM } from "./CustomUisXcm"
-import { CUSTOM_UI_X_TOKENS } from "./CustomUisXTokens"
+
+// import { CUSTOM_UI_XCM } from "./CustomUisXcm"
+// import { CUSTOM_UI_X_TOKENS } from "./CustomUisXTokens"
 
 export const CUSTOM_UI_COMPONENTS: DecodedCallComponentDefs = [
   ...CUSTOM_UI_UTILITY, // batch
-  ...CUSTOM_UI_CONVICTION_VOTING,
-  ...CUSTOM_UI_NOMINATION_POOLS,
-  ...CUSTOM_UI_X_TOKENS,
-  ...CUSTOM_UI_XCM,
+  // ...CUSTOM_UI_CONVICTION_VOTING,
+  // ...CUSTOM_UI_NOMINATION_POOLS,
+  // ...CUSTOM_UI_X_TOKENS,
+  // ...CUSTOM_UI_XCM,
 ]
