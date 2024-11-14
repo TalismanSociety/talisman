e50f8568889068f48b9367fd66c44a3198706eaa
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-11-14 14:53:53 +0900

    feat: yeet mythical bg

--- apps/extension/src/ui/apps/onboard/components/OnboardBackground.tsx
+++ apps/extension/src/ui/apps/onboard/components/OnboardBackground.tsx
@@ -1,28 +1,24 @@
-import { CSSProperties, memo, useMemo } from "react"
-import { MYSTICAL_PHYSICS_V3, MysticalBackground, MysticalPhysicsV3 } from "talisman-ui"
+import { CSSProperties, useMemo } from "react"
 
 import { useOnboard } from "../context"
 
-const BG_CONFIG: MysticalPhysicsV3 = {
-  ...MYSTICAL_PHYSICS_V3,
-  artifacts: 4,
-  radiusMax: 1.4,
-}
-
-// Memoize so animations don't reset on every render
-const Background = memo(() => (
-  <MysticalBackground className="fixed left-0 top-0 z-0 h-lvh w-lvw" config={BG_CONFIG} />
-))
-Background.displayName = "Background"
-
 export const OnboardBackground = () => {
   const { stage } = useOnboard()
-  const style: CSSProperties = useMemo(() => ({ opacity: 1 - stage * 0.2 }), [stage])
+  const style: CSSProperties = useMemo(
+    () => ({
+      // generated using https://colorgradient.dev/gradient-generator/
+      backgroundImage:
+        "radial-gradient(49% 81% at 45% 47%, #fd4848 0%, #073AFF00 100%),radial-gradient(113% 91% at 17% -2%, #d5ff5c 1%, #FF000000 99%),radial-gradient(142% 91% at 83% 7%, #d5ff5c 1%, #FF000000 99%),radial-gradient(142% 91% at 111% 84%, #121212 0%, #121212 100%)",
+      opacity: (1 - stage * 0.2) / 4,
+    }),
+    [stage],
+  )
 
   return (
     // hide on mobile to prevent glitches
-    <div className="hidden transition-opacity duration-[2.5s] ease-in-out sm:block" style={style}>
-      <Background />
-    </div>
+    <div
+      className="fixed left-0 top-0 z-0 h-lvh w-lvw transition-opacity duration-[2.5s] ease-in-out"
+      style={style}
+    ></div>
   )
 }
