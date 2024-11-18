b553fbe916f9366ef528abebef0c1355961007dc
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-11-18 10:44:31 +0900

    fix: linear easing

--- apps/extension/src/ui/apps/onboard/components/OnboardBackground.tsx
+++ apps/extension/src/ui/apps/onboard/components/OnboardBackground.tsx
@@ -28,7 +28,7 @@ export const OnboardBackground = () => {
 
   return (
     <div
-      className="fixed left-0 top-0 z-0 h-lvh w-lvw transition-opacity duration-[2s] ease-in"
+      className="linear fixed left-0 top-0 z-0 h-lvh w-lvw transition-opacity duration-[2s]"
       style={style}
     ></div>
   )
