052b24f9633bfdaf72e3dd33673c57a6e646e9eb
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-11-21 14:50:39 +0900

    fix: centered title in modal dialog

--- packages/talisman-ui/src/components/ModalDialog.tsx
+++ packages/talisman-ui/src/components/ModalDialog.tsx
@@ -31,6 +31,12 @@ export const ModalDialog: FC<ModalDialogProps> = ({
       tabIndex={-1} // reset to prevent tab key from giving focus to elements below the modal
     >
       <header className="flex w-full items-center justify-between gap-8 overflow-hidden p-10">
+        {onClose && (
+          // placeholder to keep the title centered
+          <IconButton className="invisible">
+            <XIcon />
+          </IconButton>
+        )}
         <h1
           className={classNames(
             "flex-grow overflow-hidden text-ellipsis whitespace-nowrap text-base font-bold",
