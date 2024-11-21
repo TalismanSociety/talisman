42e3fb884087b940854635186b054c42c67e2eef
Will Urban <will.urban.dev@gmail.com>
2024-11-21 14:09:39 +0700

    refactor: broke down StakingUnbondingPeriod into multiple child components

--- apps/extension/src/ui/domains/Staking/shared/StakingUnbondingPeriod.tsx
+++ apps/extension/src/ui/domains/Staking/shared/StakingUnbondingPeriod.tsx
@@ -1,36 +1,16 @@
-import { formatDistance } from "date-fns"
 import { ChainId } from "extension-core"
-import { FC, useMemo } from "react"
-import { useTranslation } from "react-i18next"
+import { FC } from "react"
 
-import { useStakingBondingDuration } from "../hooks/nomPools/useStakingBondingDuration"
+import { BittensorUnbondingPeriod } from "../Bittensor/BittensorUnbondingPeriod"
+import { NomPoolUnbondingPeriod } from "../NominationPools/NomPoolUnbondingPeriod"
 
 export const StakingUnbondingPeriod: FC<{ chainId: ChainId | null | undefined }> = ({
   chainId,
 }) => {
-  const { t } = useTranslation()
-
-  let data,
-    isLoading = false,
-    duration = t("N/A")
-
-  const hookMap = {
-    nominationPool: useStakingBondingDuration,
-  }
   switch (chainId) {
     case "bittensor":
-      duration = t("None")
-      break
+      return <BittensorUnbondingPeriod />
     default:
-      ;({ data, isLoading } = hookMap["nominationPool"](chainId))
-      duration = formatDistance(0, Number(data?.toString()) || 0)
-      break
+      return <NomPoolUnbondingPeriod chainId={chainId} />
   }
-
-  const display = useMemo(() => (duration ? duration : t("N/A")), [duration, t])
-
-  if (isLoading)
-    return <div className="text-grey-700 bg-grey-700 rounded-xs animate-pulse">28 Days</div>
-
-  return <>{display}</>
 }
