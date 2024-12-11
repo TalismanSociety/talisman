0b4c113082780d3c2a2327a2e6ab4f72da7dcfa4
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-12-11 20:12:41 +0900

    chore: yeet

--- apps/extension/src/ui/domains/Staking/hooks/bittensor/useGetBittensorStakeHotkeys.ts
+++ apps/extension/src/ui/domains/Staking/hooks/bittensor/useGetBittensorStakeHotkeys.ts
@@ -1,4 +1,4 @@
-import { useQueries, useQuery } from "@tanstack/react-query"
+import { useQuery } from "@tanstack/react-query"
 import { ChainId } from "extension-core"
 
 import { useScaleApi } from "@ui/hooks/sapi/useScaleApi"
@@ -8,8 +8,6 @@ type GetBittensorStakeHotkeys = {
   address: string | null | undefined
   totalStaked?: number
 }
-
-type GetBittensorStakesHotkeys = Omit<GetBittensorStakeHotkeys, "address"> & { addresses: string[] }
 
 export const useGetBittensorStakeHotkeys = ({
   chainId,
@@ -24,28 +22,5 @@ export const useGetBittensorStakeHotkeys = ({
     queryFn: async () =>
       await sapi?.getStorage<string[]>("SubtensorModule", "StakingHotkeys", [address]),
     enabled: !!sapi && !!address && chainId === "bittensor",
-  })
-}
-
-export const useGetBittensorStakesHotkeys = ({
-  chainId,
-  addresses,
-  totalStaked,
-}: GetBittensorStakesHotkeys) => {
-  const { data: sapi } = useScaleApi(chainId)
-  return useQueries({
-    queries: addresses.map((address) => ({
-      queryKey: ["getBittensorStakeHotkeys", sapi?.id, address, totalStaked],
-      queryFn: () => sapi?.getStorage<string[]>("SubtensorModule", "StakingHotkeys", [address]),
-      enabled: !!sapi && !!address && chainId === "bittensor",
-    })),
-    combine: (results) => {
-      return {
-        data: results.map((result) => result.data),
-        isPending: results.some((result) => result.isPending),
-        isLoading: results.some((result) => result.isLoading),
-        error: results.find((result) => result.isError),
-      }
-    },
   })
 }
