3341c346bdb40bdd90264eb64be0161001e72fba
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-12-16 16:50:56 +0900

    chore: delete unused file

--- packages/extension-core/src/domains/tokenRates/migrations/index.ts
+++
@@ -1,7 +0,0 @@
-import { db } from "../../../db"
-import { Migration, MigrationFunction } from "../../../libs/migrations/types"
-
-export const migrateTokenRates: Migration = {
-  forward: new MigrationFunction(() => db.tokenRates.clear()),
-  // no way back
-}
