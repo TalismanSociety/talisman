5f99f7cb0abfbaa2f93270f373905c5b35fd2ca4
Will Urban <will.urban.dev@gmail.com>
2025-01-06 12:16:02 +0700

    chore: hide navigation chevron on expanded view

--- apps/extension/src/ui/domains/Ramp/RampOptionSwitchHeader.tsx
+++ apps/extension/src/ui/domains/Ramp/RampOptionSwitchHeader.tsx
@@ -1,9 +1,11 @@
 import { ChevronLeftIcon } from "@talismn/icons"
+import { classNames } from "@talismn/util"
 import { useTranslation } from "react-i18next"
 import { useNavigate } from "react-router-dom"
 import { IconButton } from "talisman-ui"
 
 import { OptionSwitch } from "@talisman/components/OptionSwitch"
+import { IS_POPUP } from "@ui/util/constants"
 
 type RampOptionSwitchHeaderProps = {
   setSelectedFormType: React.Dispatch<React.SetStateAction<"buy" | "sell">>
@@ -14,10 +16,12 @@ export const RampOptionSwitchHeader = ({ setSelectedFormType }: RampOptionSwitch
   return (
     <div className="flex w-full flex-col py-4">
       <div className="flex items-center justify-between pb-6">
-        <div className="flex items-center gap-2">
-          <IconButton onClick={() => navigate("/portfolio")}>
-            <ChevronLeftIcon />
-          </IconButton>
+        <div className={"flex items-center gap-2"}>
+          <div className={classNames("flex items-center", !IS_POPUP && "hidden")}>
+            <IconButton onClick={() => navigate("/portfolio")}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
           <div>
             <div className="font-bold capitalize text-white">{t("Buy/Sell")}</div>
           </div>
