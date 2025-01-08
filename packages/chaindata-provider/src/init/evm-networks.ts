21c960161b73206077814b2ec5d0047190143fc6
Chid Gilovitz <chid@talisman.xyz>
2025-01-08 14:50:05 +0800

    chore: update version v2.3.2 and init data

--- packages/chaindata-provider/src/init/evm-networks.ts
+++ packages/chaindata-provider/src/init/evm-networks.ts
@@ -2,7 +2,7 @@ export const evmNetworks = [
   {
     id: "1",
     isTestnet: false,
-    sortIndex: 333,
+    sortIndex: 336,
     name: "Ethereum Mainnet",
     themeColor: "#62688f",
     logo: "https://raw.githubusercontent.com/TalismanSociety/chaindata/main/assets/chains/1.svg",
