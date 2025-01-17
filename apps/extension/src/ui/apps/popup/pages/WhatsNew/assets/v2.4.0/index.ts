e18471dbfaf3cb3cf142840f8e709e926a5346d8
Chid Gilovitz <chid@talisman.xyz>
2025-01-17 14:14:42 +0800

    chore: add whats new v2.4.0

---
+++ apps/extension/src/ui/apps/popup/pages/WhatsNew/assets/v2.4.0/index.ts
@@ -0,0 +1,11 @@
+import { WhatsNewVersionData } from "../types"
+import content from "./content.md"
+
+const content240: WhatsNewVersionData = {
+  "2.4.0": {
+    content,
+    date: "January 2025",
+  },
+}
+
+export default content240
