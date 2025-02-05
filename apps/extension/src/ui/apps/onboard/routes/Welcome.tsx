7ad37657fa1c3509ba0e9343ce6664bb7b7a240e
Chid Gilovitz <chid@talisman.xyz>
2025-02-05 20:32:55 +0800

    chore: copy updates

--- apps/extension/src/ui/apps/onboard/routes/Welcome.tsx
+++ apps/extension/src/ui/apps/onboard/routes/Welcome.tsx
@@ -136,7 +136,7 @@ export const WelcomePage = () => {
           </div>
           <div className="welcome-subtitle text-[2rem] lg:text-[2.8rem]">
             {t(
-              "Talisman supports Ethereum and Polkadot networks, including chains like Base, Bittensor, and Sonic",
+              "Talisman supports all Ethereum and Polkadot networks, including chains like Base, Bittensor, and Sonic",
             )}
           </div>
           <div className="justify flex justify-between gap-2 py-10 align-middle">
