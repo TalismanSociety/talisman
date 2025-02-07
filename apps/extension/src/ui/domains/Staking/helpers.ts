13995aed0f0d1ba71345fbbb311df77e11332fda
alecdwm <2741569+alecdwm@users.noreply.github.com>
2025-02-07 05:09:07 +0000

    feat: add remark on nompool join/bond_extra txs

--- apps/extension/src/ui/domains/Staking/helpers.ts
+++ apps/extension/src/ui/domains/Staking/helpers.ts
@@ -110,43 +110,26 @@ export const getNomPoolStakingPayload = async (
   isBondExtra: boolean,
   withSetClaimPermission: boolean,
 ) => {
-  if (withSetClaimPermission)
-    return sapi.getExtrinsicPayload(
-      "Utility",
-      "batch_all",
-      {
-        calls: [
-          isBondExtra
-            ? sapi.getDecodedCall("NominationPools", "bond_extra", {
-                extra: Enum("FreeBalance", amount),
-              })
-            : sapi.getDecodedCall("NominationPools", "join", { amount, pool_id: poolId }),
-          sapi.getDecodedCall("NominationPools", "set_claim_permission", {
-            permission: Enum("PermissionlessCompound"),
-          }),
-        ],
-      },
-      { address },
-    )
+  const call_joinPoolOrBondExtra = isBondExtra
+    ? sapi.getDecodedCall("NominationPools", "bond_extra", { extra: Enum("FreeBalance", amount) })
+    : sapi.getDecodedCall("NominationPools", "join", { amount, pool_id: poolId })
 
-  return isBondExtra
-    ? sapi.getExtrinsicPayload(
-        "NominationPools",
-        "bond_extra",
-        {
-          extra: Enum("FreeBalance", amount),
-        },
-        { address },
-      )
-    : sapi.getExtrinsicPayload(
-        "NominationPools",
-        "join",
-        {
-          amount,
-          pool_id: poolId,
-        },
-        { address },
-      )
+  const call_setClaimPermission = withSetClaimPermission
+    ? sapi.getDecodedCall("NominationPools", "set_claim_permission", {
+        permission: Enum("PermissionlessCompound"),
+      })
+    : undefined
+
+  const call_remark = sapi.getDecodedCall("System", "remark_with_event", {
+    remark: Binary.fromText("talisman"),
+  })
+
+  return sapi.getExtrinsicPayload(
+    "Utility",
+    "batch_all",
+    { calls: [call_joinPoolOrBondExtra, call_setClaimPermission, call_remark].filter(Boolean) },
+    { address },
+  )
 }
 export const cleanupNomPoolName = (name: string | null | undefined) =>
   name
