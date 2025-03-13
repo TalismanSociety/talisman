a13be88df45ce7015e54042423ca58e70eef6345
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-03-13 15:59:22 +0900

    test: ci fix

--- packages/extension-core/src/util/isBackgroundPage.ts
+++ packages/extension-core/src/util/isBackgroundPage.ts
@@ -1,19 +1,20 @@
-import { IS_FIREFOX, log } from "extension-shared"
+//import { IS_FIREFOX, log } from "extension-shared"
 
 /*
 Used to check if the current page is a background page. 
 It is useful for preventing the execution of certain code inside or outside of the background page.
 */
-export const isBackgroundPage = () => {
-  try {
-    if (typeof chrome !== "undefined" && chrome.runtime && chrome.extension) {
-      return IS_FIREFOX
-        ? chrome.extension.getBackgroundPage() === window
-        : typeof window === "undefined"
-    }
-  } catch (err) {
-    log.error(err)
-  }
+export const isBackgroundPage = () => true
+// export const isBackgroundPage = () => {
+//   try {
+//     if (typeof chrome !== "undefined" && chrome.runtime && chrome.extension) {
+//       return IS_FIREFOX
+//         ? chrome.extension.getBackgroundPage() === window
+//         : typeof window === "undefined"
+//     }
+//   } catch (err) {
+//     log.error(err)
+//   }
 
-  return false
-}
+//   return false
+// }
