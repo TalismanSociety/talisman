04333bd21c230ec79d52973a9624f6a820293598
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-03-24 13:25:09 +0900

    fix: ens support in add contact form

--- apps/extension/src/ui/domains/Settings/AddressBook/hooks.ts
+++ apps/extension/src/ui/domains/Settings/AddressBook/hooks.ts
@@ -11,15 +11,20 @@ export const useChainsFilteredByAddressPrefix = (address?: string) => {
   return useMemo(() => {
     if (!address) return []
 
-    const encoding = detectAddressEncoding(address)
-    if (encoding !== "ss58") return []
+    try {
+      const encoding = detectAddressEncoding(address)
+      if (encoding !== "ss58") return []
 
-    const [, ss58Format] = decodeSs58Address(address)
-    if (typeof ss58Format !== "number") return []
+      const [, ss58Format] = decodeSs58Address(address)
+      if (typeof ss58Format !== "number") return []
 
-    // 42 is generic format
-    if (ss58Format === 42) return chains
-    return chains.filter((c) => c.prefix === ss58Format)
+      // 42 is generic format
+      if (ss58Format === 42) return chains
+      return chains.filter((c) => c.prefix === ss58Format)
+    } catch (err) {
+      // invalid address
+      return []
+    }
   }, [address, chains])
 }
 
