300149bc0a9067c57a83402645074114656d896f
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-03-24 09:37:27 +0900

    fix: comment

--- packages/crypto/src/address/encoding/base58.ts
+++ packages/crypto/src/address/encoding/base58.ts
@@ -3,11 +3,11 @@ import { base58 } from "@scure/base"
 export const encodeAddressBase58 = (publicKey: Uint8Array): string => {
   return base58.encode(publicKey)
 }
-
+/** Detect if address is base58 encoded (NOTE: also returns true for ss58 addresses) */
 export function isBase58Address(address: string): boolean {
   try {
     base58.decode(address)
-    return true // note that it will return true for a ss58 address too
+    return true
   } catch (error) {
     return false
   }
