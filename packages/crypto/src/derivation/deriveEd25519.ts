7f4ef18d4a80b1d561770151bda7058c2ca14ddf
alecdwm <2741569+alecdwm@users.noreply.github.com>
2025-04-09 06:23:52 +0000

    fix: support for ed25519 accounts imported via polkadot-js

--- packages/crypto/src/derivation/deriveEd25519.ts
+++ packages/crypto/src/derivation/deriveEd25519.ts
@@ -18,5 +18,18 @@ export const deriveEd25519 = (seed: Uint8Array, derivationPath: string): Keypair
 }
 
 export const getPublicKeyEd25519 = (secretKey: Uint8Array) => {
+  // When importing ed25519 polkadot-js accounts via json, which we do inside of `packages/extension-core/src/domains/keyring/getSecretKeyFromPjsJson.ts`,
+  // the secretKey we produce is 64 bytes in length.
+  //
+  // When using the ed25519 curve to derive a publicKey for this 64 bytes privateKey, we should only take the first 32 bytes:
+  // - https://github.com/paulmillr/noble-curves/issues/53#issuecomment-1577362759
+  // - https://github.com/paulmillr/noble-curves/discussions/33#discussioncomment-5685971
+  // - https://github.com/paulmillr/noble-curves/pull/54
+  // - https://github.com/paulmillr/noble-curves/issues/88
+  //
+  // When you compare the ed25519 publicKey of a given account produced by this function to the publicKey produced by
+  // polkadot-js, you will find that they are the same as eachother.
+  if (secretKey.length === 64) return ed25519.getPublicKey(secretKey.slice(0, 32))
+
   return ed25519.getPublicKey(secretKey)
 }
