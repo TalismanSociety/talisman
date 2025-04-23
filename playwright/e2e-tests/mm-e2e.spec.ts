fd1bca62edf760d6ff67abb45c963f40ac42fd7f
prury <prury@tlsmn.z>
2025-04-23 11:16:34 +0200

    test: improvements

--- playwright/e2e-tests/mm-e2e.spec.ts
+++ playwright/e2e-tests/mm-e2e.spec.ts
@@ -6,15 +6,15 @@ test("MM e2e", async ({ addAccount, context }) => {
   const ethAccount = await addAccount({ type: "ethereum", name: ETH_ACC_NAME })
   await expect(ethAccount.getByTestId("sidebar-account-list")).toContainText(ETH_ACC_NAME)
   await ethAccount.goto("https://metamask.github.io/test-dapp/")
-  //close unwanted tabs
+  //wait for wallet popup to appear
   const [popup] = await Promise.all([
     context.waitForEvent("page"),
-    await ethAccount.locator("#connectButton").click(),
+    ethAccount.locator("#connectButton").click(),
   ])
   //interacts with wallet popup
   await popup.waitForTimeout(5000)
   await popup.bringToFront()
-  await popup.setViewportSize({ width: 1200, height: 800 })
+  await popup.setViewportSize({ width: 1280, height: 720 })
   await popup.getByRole("button", { name: ETH_ACC_NAME }).click()
   await popup.getByTestId("connection-connect-button").click()
   await expect(ethAccount.locator("#accounts")).toContainText(ETH_ACC_ADDRESS)
