c395dd6eb700a33b88b54794d094792caffe3c19
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-04-30 16:45:01 +0900

    feat: redirect url for coinbase

--- apps/extension/src/ui/domains/Ramps/coinbase/helpers.ts
+++ apps/extension/src/ui/domains/Ramps/coinbase/helpers.ts
@@ -21,6 +21,7 @@ export const getCoinbaseBuyUrl = async (
     fiatCurrency: currencyCode,
     quoteId,
     destinationWallets: JSON.stringify([{ address, blockchains: [network] }]),
+    redirect: chrome.runtime.getURL("dashboard.html"),
   })
 
   return urlJoin(RAMPS_COINBASE_PAY_URL, `/buy/select-asset?${query}`)
@@ -45,6 +46,7 @@ export const getCoinbaseSellUrl = async (
     presetCryptoAmount: amountIn,
     fiatCurrency: currencyCode,
     quoteId,
+    redirect: chrome.runtime.getURL("dashboard.html"),
   })
 
   return urlJoin(RAMPS_COINBASE_PAY_URL, `/v3/sell/input?${query}`)
