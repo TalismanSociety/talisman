98ea4be93da7768a1366deaac6d416b916007e35
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-05-17 21:21:56 +0900

    fix: consistency between useAccountsForSite and getPublicAccounts

--- apps/extension/src/ui/hooks/useAccountsForSite.ts
+++ apps/extension/src/ui/hooks/useAccountsForSite.ts
@@ -1,15 +1,24 @@
 import { AuthorizedSite } from "extension-core"
 import { isTalismanUrl } from "extension-shared"
+import { useMemo } from "react"
 
 import { useAccounts, useSettingValue } from "@ui/state"
 
 export const useAccountsForSite = (site: AuthorizedSite | string | null) => {
   const url = typeof site === "string" ? site : site?.url
-  const requestUrlIsTalisman = isTalismanUrl(url)
-  const isDevMode = useSettingValue("developerMode")
-  const allOrOwnedAccounts = useAccounts(requestUrlIsTalisman || isDevMode ? "all" : "owned")
-  const signetAccounts = useAccounts("signet")
+  const isTalismanApp = isTalismanUrl(url)
+  const developerMode = useSettingValue("developerMode")
 
-  // returns all if url is talisman, else return owned + signet accounts
-  return requestUrlIsTalisman ? allOrOwnedAccounts : [...allOrOwnedAccounts, ...signetAccounts]
+  const allAccounts = useAccounts("all")
+
+  return useMemo(
+    () =>
+      allAccounts.filter((a) => {
+        // same logic as in getPublicAccounts
+        if (developerMode) return true
+        if (isTalismanApp) return a.type !== "contact"
+        return !["watch-only", "contact"].includes(a.type)
+      }),
+    [allAccounts, developerMode, isTalismanApp],
+  )
 }
