e68de39471f94588b0f07f0185e79982c0112eef
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-05-19 16:21:01 +0900

    chore: cleanup

--- packages/extension-core/src/domains/nfts/service.ts
+++ packages/extension-core/src/domains/nfts/service.ts
@@ -1,24 +1,17 @@
 import { isAccountEthereum, isAccountNotContact } from "@talismn/keyring"
-import { getQuery$, QueryResult, sleep } from "@talismn/util"
-import { DEBUG } from "extension-shared"
+import { getQuery$, QueryResult } from "@talismn/util"
 import { isEqual } from "lodash"
 import { combineLatest, distinctUntilChanged, map, Observable, switchMap } from "rxjs"
 
 import { activeEvmNetworksObservable } from "../balances/pool"
 import { keyringStore } from "../keyring/store"
 import { fetchNfts } from "./fetchNfts"
-import { fetchRefreshNftMetadata } from "./fetchRefreshNftMetadata"
 import { nftsStore$ } from "./store"
 import { FetchNftsResponse, NftData } from "./types"
 
-const TEN_SECONDS = 60 * 1000
 const ONE_MINUTE = 60 * 1000
-const ONE_HOUR = 60 * ONE_MINUTE
 
-// const UPDATE_CHECK_INTERVAL = TEN_SECONDS
-const DEFAULT_UPDATE_INTERVAL = DEBUG ? TEN_SECONDS : ONE_HOUR
-
-const UPDATE_INTERVAL = DEFAULT_UPDATE_INTERVAL // can be changed based on context
+const UPDATE_INTERVAL = ONE_MINUTE // leverage cache on endpoint
 
 export const nfts$ = new Observable<NftData>((subscriber) => {
   const addresses$ = keyringStore.accounts$.pipe(
@@ -104,19 +97,16 @@ export const setFavoriteNft = (id: string, isFavorite: boolean) => {
   })
 }
 
-// TODO doesnt work yet
 export const refreshNftMetadata = async (id: string) => {
   const nft = nftsStore$.value.nfts.find((n) => n.id === id)
   if (!nft) return
+  throw new Error("Not implemented yet")
 
-  const { networkId, contract, tokenId } = nft
-
-  await fetchRefreshNftMetadata(networkId, contract, tokenId)
-
-  // force an update after 15 seconds, might be lucky !
-  await sleep(15_000)
-  // updateData()
-
+  // const { networkId, contract, tokenId } = nft
+  // await fetchRefreshNftMetadata(networkId, contract, tokenId)
+  // // force an update after 15 seconds, might be lucky !
+  // await sleep(15_000)
+  // // updateData()
   // we don't know when the refresh will be done, lower the update interval to 10 minute for this session
   // UPDATE_INTERVAL = 60 * 1000
 }
