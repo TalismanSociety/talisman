bf8707c00b5e3393b508aff5b38b4521dfff1a77
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-05-21 16:06:21 +0900

    fix: snooze backup banner duration to 3 days

--- packages/extension-core/src/domains/app/store.app.ts
+++ packages/extension-core/src/domains/app/store.app.ts
@@ -41,14 +41,13 @@ export type AppStoreData = {
   // dismissed banners
   hideUnifiedAddressBanner?: boolean
   hideAutonomysQuestBanner?: boolean
-  hideBackupReminderBanner?: boolean
 
   // represents a migration that is currently running
   currentMigration?: CurrentMigration
 }
 
 const ANALYTICS_VERSION = "1.5.0"
-const BACKUP_WARNING_SNOOZE = 60 * 60 * 24 * 3 * 10000 // 3 days
+const BACKUP_WARNING_SNOOZE = 60 * 60 * 24 * 3 * 1000 // 3 days
 
 export const DEFAULT_APP_STATE: AppStoreData = {
   onboarded: UNKNOWN,
