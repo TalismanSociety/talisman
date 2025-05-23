9008a28909bf7b69d93867b0ee3077a96cdd12a6
Kheops <26880866+0xKheops@users.noreply.github.com>
2025-05-23 21:11:46 +0900

    Update BraveWarningCard.tsx
    Co-authored-by: Ramo <36627808+prury@users.noreply.github.com>

--- apps/extension/src/ui/domains/Settings/BraveWarning/BraveWarningCard.tsx
+++ apps/extension/src/ui/domains/Settings/BraveWarning/BraveWarningCard.tsx
@@ -23,7 +23,7 @@ export const BraveWarningCard = ({ className, onLearnMoreClick }: Props) => {
       }
       description={
         <span className="text-body-secondary text-sm">
-          {t("By default, Brave prevents Talisman from loading all balances.")}
+          {t("By default, Brave prevents Talisman from loading all your balances.")}
         </span>
       }
       cta={
