7e45adfdbc0d20ff1114613cbd3f03c86c68c73e
Kheops <26880866+0xKheops@users.noreply.github.com>
2025-05-23 14:58:57 +0900

    Update apps/extension/src/ui/apps/popup/components/banners/BackupReminderBanner.tsx
    Co-authored-by: Ramo <36627808+prury@users.noreply.github.com>

--- apps/extension/src/ui/apps/popup/components/banners/BackupReminderBanner.tsx
+++ apps/extension/src/ui/apps/popup/components/banners/BackupReminderBanner.tsx
@@ -44,7 +44,7 @@ export const BackupReminderBanner = () => {
                 "You have funds! Talisman is a non custodial wallet so only you have access to your keys, make sure you have backed them up or you may lose access to your funds.",
               )
             : t(
-                "Your recovery phrases control your accounts. Talisman is a non custodial wallet so only you have access to your keys, make sure you have backed them up.",
+                "Your recovery phrases control your accounts. Talisman is a non-custodial wallet, so only you have access to your keys. Make sure you’ve backed them up.",
               )}
         </p>
         <div className="mt-5 grid grid-cols-2 gap-4">
