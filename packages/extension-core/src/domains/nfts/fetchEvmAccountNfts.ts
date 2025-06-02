c83b7c689beb31937864916e20e57fb1fc2e3500
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-06-02 18:21:58 +0900

    feat: connect to ada

--- packages/extension-core/src/domains/nfts/fetchEvmAccountNfts.ts
+++ packages/extension-core/src/domains/nfts/fetchEvmAccountNfts.ts
@@ -1,7 +1,9 @@
+import { ASSET_DISCOVERY_API_URL } from "extension-shared"
+
 import { AccountNfts } from "./types"
 
 export const fetchEvmAccountNfts = async (address: string) => {
-  const req = await fetch(`http://localhost:8787/nfts/${address}`)
+  const req = await fetch(`${ASSET_DISCOVERY_API_URL}/nfts/${address}`)
 
   if (!req.ok) throw new Error("Failed to fetch nfts")
 
--- packages/extension-core/src/domains/nfts/fetchEvmNftRefresh.ts
+++ packages/extension-core/src/domains/nfts/fetchEvmNftRefresh.ts
@@ -1,5 +1,7 @@
+import { ASSET_DISCOVERY_API_URL } from "extension-shared"
+
 export const fetchEvmNftRefresh = async (id: string) => {
-  const req = await fetch(`http://localhost:8787/nfts/refresh`, {
+  const req = await fetch(`${ASSET_DISCOVERY_API_URL}/nfts/refresh`, {
     method: "POST",
     body: JSON.stringify({ id }),
   })
