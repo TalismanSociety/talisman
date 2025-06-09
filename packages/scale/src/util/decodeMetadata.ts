7c7c1041690e0ee2d03cf2b1eddc6d0c901f15e5
alecdwm <2741569+alecdwm@users.noreply.github.com>
2025-06-09 05:15:30 +0000

    fix: v16 decodeMetadata

--- packages/scale/src/util/decodeMetadata.ts
+++ packages/scale/src/util/decodeMetadata.ts
@@ -1,4 +1,4 @@
-import { V14, V15 } from "@polkadot-api/substrate-bindings"
+import { V14, V15, V16 } from "@polkadot-api/substrate-bindings"
 
 import { metadata as scaleMetadata } from "../papito"
 import { getMetadataVersion } from "./getMetadataVersion"
@@ -6,14 +6,18 @@ import { getMetadataVersion } from "./getMetadataVersion"
 export const decodeMetadata = (
   metadataRpc: string,
 ): { metadataVersion: number } & (
+  | { metadata: V16; tag: "v16" }
   | { metadata: V15; tag: "v15" }
   | { metadata: V14; tag: "v14" }
   | { metadata?: undefined; tag?: undefined }
 ) => {
   const metadataVersion = getMetadataVersion(metadataRpc)
-  if (metadataVersion !== 15 && metadataVersion !== 14) return { metadataVersion }
+  if (metadataVersion !== 16 && metadataVersion !== 15 && metadataVersion !== 14)
+    return { metadataVersion }
 
   const decoded = scaleMetadata.dec(metadataRpc)
+  if (decoded.metadata.tag === "v16")
+    return { metadataVersion, metadata: decoded.metadata.value, tag: decoded.metadata.tag }
   if (decoded.metadata.tag === "v15")
     return { metadataVersion, metadata: decoded.metadata.value, tag: decoded.metadata.tag }
   if (decoded.metadata.tag === "v14")
