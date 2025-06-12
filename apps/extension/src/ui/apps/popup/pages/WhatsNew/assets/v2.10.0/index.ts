65c304bfb72c77719575969baeb43eddd1efdbe6
Alec WM <2741569+alecdwm@users.noreply.github.com>
2025-06-12 21:18:42 +1000

    chore: added v2.11.0 whats new (#2042)

--- apps/extension/src/ui/apps/popup/pages/WhatsNew/assets/v2.10.0/index.ts
+++ apps/extension/src/ui/apps/popup/pages/WhatsNew/assets/v2.10.0/index.ts
@@ -1,11 +1,11 @@
 import { WhatsNewVersionData } from "../types"
 import content from "./content.md"
 
-const content210: WhatsNewVersionData = {
+const content_v2_10_0: WhatsNewVersionData = {
   "2.10.0": {
     content,
     date: "May 2025",
   },
 }
 
-export default content210
+export default content_v2_10_0
