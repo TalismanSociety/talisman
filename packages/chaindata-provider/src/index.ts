02e6a12a8cf45eb4f36a2a396334dffe01b33b23
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-06-15 18:22:50 +0900

    fix: build

--- packages/chaindata-provider/src/index.ts
+++ packages/chaindata-provider/src/index.ts
@@ -1,9 +1,9 @@
-export * from "./constants"
 export * from "./chaindata"
+export * from "./constants"
+
 export * from "./init"
 export * from "./net"
 export * from "./types" // TODO remove this one someday
 export * from "./util"
 
 export * from "./ChaindataProvider"
-export * from "./chaindata"
--- packages/chaindata-provider/src/types/Network/index.ts
+++
@@ -1,4 +0,0 @@
-// export * from "./NetworkBase"
-// export * from "./DotNetwork"
-// export * from "./EthNetwork"
-// export * from "./Network"
