199280ad92c3ec84c1e828a118f8bb975ce7af68
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-06-27 21:08:04 +0900

    fix: sign extension list in send funds

--- packages/extension-core/src/util/getTypeRegistry.ts
+++ packages/extension-core/src/util/getTypeRegistry.ts
@@ -68,13 +68,14 @@ export const getTypeRegistry = async (
       registry.setMetadata(metadata)
     }
 
-    registry.setSignedExtensions(signedExtensions, {
-      ...metadataDef.userExtensions,
-      ...chain?.signedExtensions,
-    })
+    if (signedExtensions || metadataDef.userExtensions || chain?.signedExtensions)
+      registry.setSignedExtensions(signedExtensions, {
+        ...metadataDef.userExtensions,
+        ...chain?.signedExtensions,
+      })
 
     if (!metadataDef.metadataRpc && metadataDef.types) registry.register(metadataDef.types)
-  } else {
+  } else if (signedExtensions || chain?.signedExtensions) {
     registry.setSignedExtensions(signedExtensions, chain?.signedExtensions)
   }
 
