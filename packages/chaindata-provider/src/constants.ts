35aac2fc7cb646f9455ebd96ed24859260c16155
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-07-04 02:02:20 +0900

    fix: target main

--- packages/chaindata-provider/src/constants.ts
+++ packages/chaindata-provider/src/constants.ts
@@ -1,5 +1,5 @@
 /** @dev temporarily change branch here when testing changes in chaindata */
-const CHAINDATA_BRANCH = "fix/minimetadata-versions"
+const CHAINDATA_BRANCH = "main"
 
 // pub subfolder needs to be incremented each time the schema of chaindata changes, or when the content of the minimetadata changes
 const CHAINDATA_PUB_FOLDER = "pub/v4"
