7445101f89351b31f46097622407e8fe4258a39f
prury <prury@tlsmn.z>
2025-07-08 09:40:25 +0200

    chore: config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig, devices } from "@playwright/test"
+import dotenv from "dotenv"
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({ path: "apps/extension/.env" })
 
 /**
  * See https://playwright.dev/docs/test-configuration.
