f5802150c4fd8ebce1a5dd1511dddf7badab4e87
prury <prury@tlsmn.z>
2025-07-08 16:37:42 +0200

    test: avail

--- playwright/e2e-tests/transfers.ts
+++ playwright/e2e-tests/transfers.ts
@@ -15,4 +15,12 @@ export const testAssets = [
     sendTo: "0x899F2F48D76771b1876165E5B4df1ad13021eb6d",
     amount: "0.001",
   },
+  {
+    assetName: "AVAIL",
+    chain: "Avail Turing Testnet",
+    needsEnabling: true,
+    chainType: "polkadot",
+    sendTo: "5DHyKJK9qY5nosXAvyGiSRTdnC76Z45RcuAMq5223T1Vrtbc",
+    amount: "0.001",
+  },
 ]
