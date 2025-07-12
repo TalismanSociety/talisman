532bafe6119be1e3836ca6cf72d6fd1769368786
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-07-12 19:10:20 +0900

    fix: tao assets details page

--- apps/extension/src/ui/domains/Portfolio/AssetDetails/PopupTokenBalances/BittensorTokenBalances.tsx
+++ apps/extension/src/ui/domains/Portfolio/AssetDetails/PopupTokenBalances/BittensorTokenBalances.tsx
@@ -1,12 +1,15 @@
 import { Balances } from "@talismn/balances"
 import { TokenId } from "@talismn/chaindata-provider"
 
+import { CHAIN_INFO } from "@ui/domains/Staking/Bittensor/utils/constants"
 import { useCombinedSubnetData } from "@ui/domains/Staking/hooks/bittensor/dTao/useCombinedSubnetData"
 import { useSortedGroupedStakes } from "@ui/domains/Staking/hooks/bittensor/dTao/useSortedGroupedStakes"
 import { useTokenRates } from "@ui/state"
 
 import { useTokenBalances } from "../useTokenBalances"
 import { BittensorTokenBalanceList } from "./BittensorTokenBalanceList"
+import { TokenBalancesDetailRow } from "./TokenBalancesDetailRow"
+import { TokenBalancesList } from "./TokenBalancesList"
 
 type TokenBalancesParams = {
   tokenId: TokenId
@@ -25,6 +28,34 @@ export const BittensorTokenBalances = ({ balances, tokenId }: TokenBalancesParam
 
   return sortedGroupedStakes.map(([key, groupedStakesByNetuid]) => {
     if (!groupedStakesByNetuid) return null
+
+    if (key === CHAIN_INFO) {
+      const { network, token, status } = tokenBalances
+      if (!network || !token) return null
+      return (
+        <TokenBalancesList
+          key={key}
+          chainOrNetworkId={network.id}
+          chainOrNetworkName={network.name}
+          networkType={tokenBalances.networkType}
+          detailRowsLength={tokenBalances.detailRows.length}
+          tokenId={tokenId}
+          balances={balances}
+        >
+          {groupedStakesByNetuid.map((row, i) => (
+            <TokenBalancesDetailRow
+              key={i}
+              tokenId={tokenId}
+              status={status}
+              row={row}
+              symbol={token.symbol}
+              tokenDecimals={token.decimals}
+              isLastRow={groupedStakesByNetuid.length === i + 1}
+            />
+          ))}
+        </TokenBalancesList>
+      )
+    }
 
     return (
       <BittensorTokenBalanceList
