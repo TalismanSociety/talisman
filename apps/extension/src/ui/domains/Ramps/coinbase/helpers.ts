3786be2d72e25458a394b6e3f68306d276907089
alecdwm <2741569+alecdwm@users.noreply.github.com>
2025-07-16 05:43:41 +0000

    chore: use assetId in offramp

--- apps/extension/src/ui/domains/Ramps/coinbase/helpers.ts
+++ apps/extension/src/ui/domains/Ramps/coinbase/helpers.ts
@@ -1,26 +1,28 @@
 import { RAMPS_COINBASE_API_BASE_PATH, RAMPS_COINBASE_PAY_URL } from "extension-shared"
 import urlJoin from "url-join"
 
-import { remoteConfig$ } from "@ui/state"
-
 export const getCoinbaseBuyUrl = async (
   currencyCode: string,
   amountIn: string,
-  assetId: string,
+  _assetId: string,
   assetSymbol: string,
   network: string,
   quoteId: string,
   address: string,
 ) => {
-  const remoteConfig = await remoteConfig$.getValue()
-  const sessionToken = await getCoinbaseSessionToken(assetId, assetSymbol, network, address)
+  // NOTE: Ideally we would use assetId instead of assetSymbol,
+  // but when we use the generated sessionToken it seems to raise an "<asset-id> is not available" error on the Coinbase buy widget.
+  const sessionToken = await getCoinbaseSessionToken(/* assetId */ assetSymbol, network, address)
 
   // docs: https://docs.cdp.coinbase.com/onramp/docs/api-oneclickbuy
   const query = new URLSearchParams({
-    appId: remoteConfig.ramps.coinbaseProjectId,
-    destinationWallets: JSON.stringify([{ address, blockchains: [network] }]),
+    // appId: remoteConfig.ramps.coinbaseProjectId, // deprecated, replaced by sessionToken
+    // addresses: JSON.stringify({ [address]: [network] }), // deprecated, replaced by sessionToken
+    // assets: JSON.stringify([/* assetId */ assetSymbol]), // deprecated, replaced by sessionToken
     sessionToken,
-    defaultAsset: assetId,
+    defaultAsset: /* assetId */ assetSymbol,
+    defaultNetwork: network,
+    defaultExperience: "buy",
     partnerUserId: "talisman",
     presetFiatAmount: amountIn,
     fiatCurrency: currencyCode,
@@ -35,20 +37,21 @@ export const getCoinbaseSellUrl = async (
   currencyCode: string,
   amountIn: string,
   assetId: string,
-  assetSymbol: string,
+  _assetSymbol: string,
   network: string,
   quoteId: string,
   address: string,
 ) => {
-  const remoteConfig = await remoteConfig$.getValue()
-  const sessionToken = await getCoinbaseSessionToken(assetId, assetSymbol, network, address)
+  const sessionToken = await getCoinbaseSessionToken(assetId, network, address)
 
   // docs: https://docs.cdp.coinbase.com/onramp/docs/api-offramp-initializing
   const query = new URLSearchParams({
-    appId: remoteConfig.ramps.coinbaseProjectId,
-    addresses: JSON.stringify({ [address]: [network] }),
+    // appId: remoteConfig.ramps.coinbaseProjectId, // deprecated, replaced by sessionToken
+    // addresses: JSON.stringify({ [address]: [network] }), // deprecated, replaced by sessionToken
+    // assets: JSON.stringify([assetId]), // deprecated, replaced by sessionToken
     sessionToken,
-    assets: JSON.stringify([assetId]),
+    defaultAsset: assetId,
+    defaultNetwork: network,
     partnerUserId: "talisman",
     presetCryptoAmount: amountIn,
     fiatCurrency: currencyCode,
@@ -59,20 +62,13 @@ export const getCoinbaseSellUrl = async (
   return urlJoin(RAMPS_COINBASE_PAY_URL, `/v3/sell/input?${query}`)
 }
 
-const getCoinbaseSessionToken = async (
-  _assetId: string,
-  assetSymbol: string,
-  network: string,
-  address: string,
-) => {
+const getCoinbaseSessionToken = async (assetId: string, network: string, address: string) => {
   const url = urlJoin(RAMPS_COINBASE_API_BASE_PATH, "/token")
   const method = "POST"
   const headers = { "Content-Type": "application/json" }
   const body = JSON.stringify({
     addresses: [{ address, blockchains: [network] }],
-    // NOTE: Ideally we would use assetId instead of assetSymbol,
-    // but when we use the generated sessionToken it seems to raise an "asset <asset-id> not found" error on the Coinbase API.
-    assets: [assetSymbol],
+    assets: [assetId],
   })
 
   try {
