568a469ca0f9d79e62e1bd1617a5a1226a12b143
alecdwm <2741569+alecdwm@users.noreply.github.com>
2025-07-23 23:54:47 +0000

    fix: return Network/Token types from chaindata hooks

--- packages/balances-react/src/hooks/useChaindata.ts
+++ packages/balances-react/src/hooks/useChaindata.ts
@@ -1,4 +1,11 @@
-import { NetworkId, NetworkList, TokenId, TokenList } from "@talismn/chaindata-provider"
+import {
+  Network,
+  NetworkId,
+  NetworkList,
+  Token,
+  TokenId,
+  TokenList,
+} from "@talismn/chaindata-provider"
 import { useAtomValue } from "jotai"
 import { keyBy } from "lodash-es"
 import { useMemo } from "react"
@@ -9,22 +16,22 @@ import { chaindataProviderAtom } from "../atoms/chaindataProvider"
 export const useChaindataProvider = () => useAtomValue(chaindataProviderAtom)
 export const useChaindata = () => useAtomValue(chaindataAtom)
 
-export const useNetworks = () => useChaindata().networks
+export const useNetworks = (): Network[] => useChaindata().networks
 export const useNetworksById = (): NetworkList => {
   const { networks } = useChaindata()
   return useMemo(() => keyBy(networks, (n) => n.id), [networks])
 }
-export const useNetwork = (networkId?: NetworkId) => {
+export const useNetwork = (networkId?: NetworkId): Network | null => {
   const networksById = useNetworksById()
   return networksById[networkId ?? ""] ?? null
 }
 
-export const useTokens = () => useChaindata().tokens
+export const useTokens = (): Token[] => useChaindata().tokens
 export const useTokensById = (): TokenList => {
   const { tokens } = useChaindata()
   return useMemo(() => keyBy(tokens, (t) => t.id), [tokens])
 }
-export const useToken = (tokenId?: TokenId) => {
+export const useToken = (tokenId?: TokenId): Token | null => {
   const tokensById = useTokensById()
   return tokensById[tokenId ?? ""] ?? null
 }
