a0209b32ec0650eaff0c67588dc0ff70d5e3f0f5
alecdwm <2741569+alecdwm@users.noreply.github.com>
2025-07-24 00:39:38 +0000

    fix: consistency between chaindata atoms and hooks

--- packages/balances-react/src/atoms/chaindata.ts
+++ packages/balances-react/src/atoms/chaindata.ts
@@ -1,3 +1,4 @@
+import { DotNetwork, Network, NetworkList, Token, TokenList } from "@talismn/chaindata-provider"
 import { firstThenDebounce } from "@talismn/util"
 import { atom } from "jotai"
 import { atomWithObservable } from "jotai/utils"
@@ -24,6 +25,10 @@ const filteredChaindataAtom = atom(async (get) => {
     (n) => (enabledNetworkIds?.includes(n.id) || n.isDefault) && (enableTestnets || !n.isTestnet),
   )
   const networksById = keyBy(networks, (n) => n.id)
+  const dotNetworksByGenesisHash = keyBy(
+    networks.filter((n) => n.platform === "polkadot"),
+    (n) => n.genesisHash,
+  )
 
   const tokens = chaindata.tokens.filter(
     (token) =>
@@ -31,10 +36,22 @@ const filteredChaindataAtom = atom(async (get) => {
   )
   const tokensById = keyBy(tokens, (t) => t.id)
 
-  return { networks, networksById, tokens, tokensById }
+  return { networks, networksById, dotNetworksByGenesisHash, tokens, tokensById }
 })
 
-export const networksAtom = atom(async (get) => (await get(filteredChaindataAtom)).networks)
-export const networksByIdAtom = atom(async (get) => (await get(filteredChaindataAtom)).networksById)
-export const tokensAtom = atom(async (get) => (await get(filteredChaindataAtom)).tokens)
-export const tokensByIdAtom = atom(async (get) => (await get(filteredChaindataAtom)).tokensById)
+export const networksAtom = atom<Promise<Network[]>>(
+  async (get) => (await get(filteredChaindataAtom)).networks,
+)
+export const networksByIdAtom = atom<Promise<NetworkList>>(
+  async (get) => (await get(filteredChaindataAtom)).networksById,
+)
+export const dotNetworksByGenesisHashAtom = atom<Promise<Record<string, DotNetwork>>>(
+  async (get) => (await get(filteredChaindataAtom)).dotNetworksByGenesisHash,
+)
+
+export const tokensAtom = atom<Promise<Token[]>>(
+  async (get) => (await get(filteredChaindataAtom)).tokens,
+)
+export const tokensByIdAtom = atom<Promise<TokenList>>(
+  async (get) => (await get(filteredChaindataAtom)).tokensById,
+)
--- packages/balances-react/src/hooks/useChaindata.ts
+++ packages/balances-react/src/hooks/useChaindata.ts
@@ -1,37 +1,25 @@
-import {
-  Network,
-  NetworkId,
-  NetworkList,
-  Token,
-  TokenId,
-  TokenList,
-} from "@talismn/chaindata-provider"
+import { NetworkId, TokenId } from "@talismn/chaindata-provider"
 import { useAtomValue } from "jotai"
-import { keyBy } from "lodash-es"
-import { useMemo } from "react"
 
-import { chaindataAtom } from "../atoms/chaindata"
+import {
+  chaindataAtom,
+  dotNetworksByGenesisHashAtom,
+  networksAtom,
+  networksByIdAtom,
+  tokensAtom,
+  tokensByIdAtom,
+} from "../atoms/chaindata"
 import { chaindataProviderAtom } from "../atoms/chaindataProvider"
 
 export const useChaindataProvider = () => useAtomValue(chaindataProviderAtom)
 export const useChaindata = () => useAtomValue(chaindataAtom)
 
-export const useNetworks = (): Network[] => useChaindata().networks
-export const useNetworksById = (): NetworkList => {
-  const { networks } = useChaindata()
-  return useMemo(() => keyBy(networks, (n) => n.id), [networks])
-}
-export const useNetwork = (networkId?: NetworkId): Network | null => {
-  const networksById = useNetworksById()
-  return networksById[networkId ?? ""] ?? null
-}
+export const useNetworks = () => useAtomValue(networksAtom)
+export const useNetworksById = () => useAtomValue(networksByIdAtom)
+export const useDotNetworksByGenesisHash = () => useAtomValue(dotNetworksByGenesisHashAtom)
+export const useNetwork = (networkId?: NetworkId) =>
+  useAtomValue(networksByIdAtom)[networkId ?? ""] ?? null
 
-export const useTokens = (): Token[] => useChaindata().tokens
-export const useTokensById = (): TokenList => {
-  const { tokens } = useChaindata()
-  return useMemo(() => keyBy(tokens, (t) => t.id), [tokens])
-}
-export const useToken = (tokenId?: TokenId): Token | null => {
-  const tokensById = useTokensById()
-  return tokensById[tokenId ?? ""] ?? null
-}
+export const useTokens = () => useAtomValue(tokensAtom)
+export const useTokensById = () => useAtomValue(tokensByIdAtom)
+export const useToken = (tokenId?: TokenId) => useAtomValue(tokensByIdAtom)[tokenId ?? ""] ?? null
