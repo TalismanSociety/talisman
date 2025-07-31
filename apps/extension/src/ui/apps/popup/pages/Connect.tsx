46eaa80992111ae9cb64e5a3193cd816b984609e
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-07-31 18:19:13 +0900

    fix: provider type

--- apps/extension/src/ui/apps/popup/pages/Connect.tsx
+++ apps/extension/src/ui/apps/popup/pages/Connect.tsx
@@ -192,7 +192,7 @@ export const ConnectPolkadot: ConnectComponent = ({
         </ConnectAccountsContainer>
         {!accounts.length && (
           <NoAccountWarning
-            type={"polkadot"}
+            type={"solana"}
             onIgnoreClick={onNoAccountClose(false)}
             onAddAccountClick={onNoAccountClose(true)}
           />
