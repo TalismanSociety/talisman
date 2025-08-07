b8c31743073f39aab21dfeabc3b30e929a5aa6b5
Will Urban <will.urban.dev@gmail.com>
2025-08-07 14:12:45 +0700

    Revert "Chore/remove managed delegation (#2129)" (#2131)
    This reverts commit e54c60bbf9b1aaa345b339044f69cf451d2c837e.

--- apps/extension/src/ui/domains/Portfolio/AssetDetails/DashboardTokenBalances/AssetState.tsx
+++ apps/extension/src/ui/domains/Portfolio/AssetDetails/DashboardTokenBalances/AssetState.tsx
@@ -1,5 +1,4 @@
 import { Address } from "extension-core"
-import { Tooltip, TooltipContent, TooltipTrigger } from "talisman-ui"
 
 import { PortfolioAccount } from "../PortfolioAccount"
 
@@ -26,12 +25,7 @@ export const AssetState = ({
       <div className="flex w-full items-baseline gap-4 overflow-hidden">
         <div className="shrink-0 whitespace-nowrap font-bold capitalize text-white">{title}</div>
         {/* show description next to title when address is set */}
-        {description && address && (
-          <Tooltip>
-            <TooltipTrigger className="max-w-full truncate text-sm">{description}</TooltipTrigger>
-            <TooltipContent>{description}</TooltipContent>
-          </Tooltip>
-        )}
+        {description && address && <div className="grow truncate text-sm">{description}</div>}
         {!description && address && isLoading && (
           <div className="bg-grey-800 rounded-xs h-[1.4rem] w-60 animate-pulse" />
         )}
@@ -46,10 +40,7 @@ export const AssetState = ({
         <div className="bg-grey-800 rounded-xs h-[1.6rem] w-60 animate-pulse" />
       )}
       {description && !address && (
-        <Tooltip>
-          <TooltipTrigger className="max-w-full truncate text-sm">{description}</TooltipTrigger>
-          <TooltipContent>{description}</TooltipContent>
-        </Tooltip>
+        <div className="flex-shrink-0 truncate text-sm">{description}</div>
       )}
     </div>
   )
