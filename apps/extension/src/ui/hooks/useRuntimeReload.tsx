6af4c19fb7d29e0cef21e23bb4df622052dc8c1e
alecdwm <2741569+alecdwm@users.noreply.github.com>
2025-08-12 01:32:03 +0000

    fix: docs

--- apps/extension/src/ui/hooks/useRuntimeReload.tsx
+++ apps/extension/src/ui/hooks/useRuntimeReload.tsx
@@ -18,7 +18,7 @@ export const useRuntimeReload = (analyticsPage: AnalyticsPage) => {
       assetDiscoveryStore.reset(),
       connectionMetaDb.delete(),
       talismanDb.metadata.clear(),
-      talismanDb.blobs.bulkDelete(["nfts", "balances"]), // atm it contains 3 entries: nfts, balances (+minimetadatas) and chaindata
+      talismanDb.blobs.bulkDelete(["nfts", "balances"]), // atm it contains 4 entries: nfts, balances (+minimetadatas), chaindata and tokenRates
     ])
 
     chrome.runtime.reload()
