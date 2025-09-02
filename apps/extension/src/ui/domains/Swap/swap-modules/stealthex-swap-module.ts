17ea5234c408ad087ba15461d380ce4639aec404
Alec WM <2741569+alecdwm@users.noreply.github.com>
2025-09-02 17:28:36 +1000

    fix: crash on tx history due to swaps status (#2154)
    * fix: crash on tx history due to swaps status
    
    * fix: race condition

--- apps/extension/src/ui/domains/Swap/swap-modules/stealthex-swap-module.ts
+++ apps/extension/src/ui/domains/Swap/swap-modules/stealthex-swap-module.ts
@@ -10,10 +10,20 @@ import { encodeAnyAddress, isAddressEqual, isEthereumAddress } from "@talismn/cr
 import { ScaleApi } from "@talismn/sapi"
 import BigNumber from "bignumber.js"
 import { UNKNOWN_TOKEN_URL } from "extension-shared"
-import { atom, ExtractAtomValue, Getter } from "jotai"
-import { withAtomEffect } from "jotai-effect"
+import { atom, ExtractAtomValue } from "jotai"
 import { atomWithObservable, loadable } from "jotai/utils"
 import createClient from "openapi-fetch"
+import {
+  catchError,
+  defer,
+  interval,
+  Observable,
+  of,
+  retry,
+  startWith,
+  switchMap,
+  takeWhile,
+} from "rxjs"
 import { encodeFunctionData, erc20Abi, publicActions, TransactionRequest } from "viem"
 import {
   arbitrum,
@@ -861,37 +871,35 @@ export const stealthexSwapModule: SwapModule = {
   decentralisationScore: DECENTRALISATION_SCORE,
 }
 
-export const retryStatus = async (
-  get: Getter,
-  id: string,
-  attempts = 0,
-): Promise<{ status: StealthexExchange["status"] }> => {
-  try {
-    const exchange = await stealthexSdk.getExchange(id)
+export const swapStatus$ = (id: string): Observable<StealthexExchange["status"] | undefined> =>
+  retryStatus$(id).pipe(
+    switchMap((status) => {
+      if (status === undefined) return of(undefined)
 
-    if (!["finished", "failed"].includes(exchange.status)) {
-      get(refreshSwapStatusAtom)
-    }
+      const shouldRefresh = (status: StealthexExchange["status"] | undefined) =>
+        !(status && ["finished", "failed", "expired", "refunded"].includes(status))
 
-    return { status: exchange.status }
-  } catch (cause) {
-    const error = cause as Error & { response?: { status?: number } }
-    // because we're using a broker, the tx isnt always available immediately in their api service
-    // so we wait a bit and retry
-    if (error.name === "AxiosError" && error?.response?.status === 404 && attempts < 10) {
-      await new Promise((resolve) => setTimeout(resolve, 5000))
-      return retryStatus(get, id, attempts + 1)
-    }
-    throw cause
-  }
-}
-
-const _refreshSwapStatusAtom = atom(0)
-const refreshSwapStatusAtom = withAtomEffect(_refreshSwapStatusAtom, (_get, set) => {
-  const intervalId = setInterval(
-    // increment _refreshSwapStatus by 1 every 20s
-    () => set(_refreshSwapStatusAtom, (i) => (i + 1) % Number.MAX_SAFE_INTEGER),
-    20_000,
+      // refresh every 20s if status isn't final
+      if (shouldRefresh(status)) {
+        return interval(20_000).pipe(
+          startWith(-1),
+          switchMap((i) => (i === -1 ? of(status) : retryStatus$(id))),
+          takeWhile((status) => shouldRefresh(status), true),
+        )
+      }
+      return of(status)
+    }),
   )
-  return () => clearInterval(intervalId)
-})
+
+const retryStatus$ = (id: string): Observable<StealthexExchange["status"] | undefined> =>
+  defer(() => stealthexSdk.getExchange(id).then((exchange) => exchange.status)).pipe(
+    // retry up to 10 times, wait 5s between each retry
+    retry({ count: 10, delay: 5_000 }),
+
+    // log when all retries failed, and return undefined
+    catchError((error) => {
+      // eslint-disable-next-line no-console
+      console.error(`Failed to fetch exchange status for '${id}'`, error)
+      return of(undefined)
+    }),
+  )
