5fd22f350e13b59fabd28b208ad8a3925e31b2cc
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-10-03 13:20:05 +0900

    fix: padding

--- apps/extension/src/ui/domains/Portfolio/GetStarted/GetStarted.tsx
+++ apps/extension/src/ui/domains/Portfolio/GetStarted/GetStarted.tsx
@@ -263,8 +263,8 @@ const GetStartedActionButton: FC<{
   <button
     type="button"
     className={classNames(
-      "border-disabled bg-grey-800 border-grey-700/40 hover:bg-grey-750 leading-paragraph enabled:focus-visible:bg-grey-750 @2xl:text-md rounded-sm border px-8 py-4 text-left text-base",
-      "flex w-full items-center gap-8",
+      "border-disabled bg-grey-800 border-grey-700/40 hover:bg-grey-750 leading-paragraph enabled:focus-visible:bg-grey-750 @2xl:text-md @2xl:px-8 rounded-sm border px-6 py-4 text-left text-base",
+      "flex w-full items-center gap-8 overflow-hidden",
       className,
     )}
     onClick={onClick}
