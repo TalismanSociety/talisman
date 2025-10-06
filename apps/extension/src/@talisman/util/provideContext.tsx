06bace9177462d99ff15d659ea4f9d121d5dc09c
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-10-06 14:40:25 +0900

    fix: prevent fetching context outside of a provider

--- apps/extension/src/@talisman/util/provideContext.tsx
+++ apps/extension/src/@talisman/util/provideContext.tsx
@@ -1,5 +1,7 @@
 import { createContext, FC, ReactNode, useContext } from "react"
 
+const UNSET_CONTEXT = {}
+
 // This utility generates a context provider from a react hook passed as argument
 // Returns an array containing the provider and the consumer hook
 export const provideContext = <P, T>(useProviderContext: (props: P) => T) => {
@@ -8,7 +10,7 @@ export const provideContext = <P, T>(useProviderContext: (props: P) => T) => {
   type ProviderProps = P & { children?: ReactNode }
   type ProviderType = FC<ProviderProps>
 
-  const Context = createContext({} as ContextType)
+  const Context = createContext(UNSET_CONTEXT as ContextType)
 
   const Provider: ProviderType = ({ children, ...props }: ProviderProps) => {
     const ctx = useProviderContext(props as P)
@@ -16,7 +18,14 @@ export const provideContext = <P, T>(useProviderContext: (props: P) => T) => {
     return <Context.Provider value={ctx}>{children}</Context.Provider>
   }
 
-  const useProvidedContext = () => useContext(Context)
+  const useProvidedContext = () => {
+    const ctx = useContext(Context)
+
+    // if default value is found, the hook is used outside of a provider
+    if (ctx === UNSET_CONTEXT) throw new Error("useProvidedContext must be used within a Provider")
+
+    return ctx
+  }
 
   return [Provider, useProvidedContext] as [ProviderType, () => ContextType]
 }
