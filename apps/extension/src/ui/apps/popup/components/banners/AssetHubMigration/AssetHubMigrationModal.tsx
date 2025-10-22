5e9ba5d5937e031c31c28a00a96dbe6dc4c1046b
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-10-22 17:06:47 +0900

    feat: modal content

--- apps/extension/src/ui/apps/popup/components/banners/AssetHubMigration/AssetHubMigrationModal.tsx
+++ apps/extension/src/ui/apps/popup/components/banners/AssetHubMigration/AssetHubMigrationModal.tsx
@@ -1,7 +1,10 @@
-import { ClockIcon, CoinsHandIcon, TalismanHandIcon, ZapFastIcon } from "@talismn/icons"
-import { FC, ReactNode, SVGProps } from "react"
-import { useTranslation } from "react-i18next"
+import { ExternalLinkIcon } from "@talismn/icons"
+import { format } from "date-fns"
+import { FC } from "react"
+import { Trans, useTranslation } from "react-i18next"
 import { Button, Modal, ModalDialog } from "talisman-ui"
+
+import { useDateFnsLocale } from "@ui/hooks/useDateFnsLocale"
 
 import { ReactComponent as Background } from "./modal-bg.svg"
 
@@ -23,96 +26,100 @@ export const AssetHubMigrationModal: FC<{ isOpen: boolean; onClose: () => void }
 
 const ModalContent: FC<{ onClose: () => void }> = ({ onClose }) => {
   const { t } = useTranslation()
+  const locale = useDateFnsLocale()
+
   return (
     <ModalDialog
-      onClose={close}
-      //   title={"SEEK Benefits"}
-      className="[&>header>h1]:text-md relative size-full rounded-none border-none bg-gradient-to-b from-[#E6007A] to-transparent to-40%"
+      title={t("Asset Hub Migration")}
+      onClose={onClose}
+      className="[&>header>h1]:text-md bg-black-tertiary relative size-full rounded-none border-none bg-gradient-to-b from-[#E6007A]/50 to-transparent to-40%"
     >
-      <Background className="absolute right-0 top-0 z-0 h-[20.7rem] w-[17rem]" />
-      <div className="flex size-full flex-col">
-        <div className="grow">
-          <div className="mt-7 flex h-60 flex-col justify-center gap-4">
-            <p className="text-[2.1rem]">{t("Talisman SEEK is live")}</p>
-            <p className="text-body-secondary max-w-[25rem] text-sm">
-              {t("Become part of the Seeker community")}{" "}
-              {/* <button
-                  type="button"
-                  className="text-primary-500 inline-flex items-center gap-1 text-xs"
-                  onClick={handleClickLearnMore}
-                >
-                  <span>{t("Learn more")}</span>
-                  <ArrowRightIcon />
-                </button> */}
-            </p>
-            {/* <UserSeekBalance /> */}
-          </div>
-          <div className="bg-grey-800 mt-8 flex h-[4.6rem] items-center justify-between rounded-t-sm px-8 text-base">
-            <div className="flex grow items-center justify-start gap-3 overflow-hidden">
-              <div className="truncate">{t("Earn SEEK rewards")}</div>
-              {/* <div className="bg-grey-600 size-2 shrink-0 rounded-full"></div>
-                <div className="text-primary shrink-0 whitespace-nowrap">15% APY</div> */}
-            </div>
-            {/* <button
-                type="button"
-                className="bg-primary/10 text-primary flex h-16 shrink-0 items-center gap-2 rounded-full px-6 pl-4"
-                onClick={handleClickStake}
-              >
-                <ZapIcon className="shrink-0 text-sm" />
-                <div className="text-xs">{t("Stake")}</div>
-              </button> */}
-          </div>
-          <div className="bg-grey-850 border-t-none border-grey-800 flex flex-col gap-16 rounded-b-sm border p-10">
-            <ListItem
-              color="#D5FF5C"
-              backgroundColor="rgba(213, 255, 92, 0.12)"
-              icon={ZapFastIcon}
-              title={t("hi")}
-              description={t("Maximize your staking power with SEEK.")}
+      <Background className="absolute -top-20 right-0 z-0 h-[20.7rem] w-[17rem]" />
+      <div className="flex size-full flex-col gap-8">
+        <div className="grow overflow-auto pt-32">
+          <p className="text-body text-base font-bold">
+            <Trans
+              t={t}
+              components={{ Highlight: <span className="text-[#E6007A]"></span> }}
+              defaults="On <Highlight>{{date}}</Highlight> DOT balances, staking and governance are moving from the Relay Chain to Asset Hub"
+              values={{
+                date: format(new Date("2025-11-04"), "MMMM do y", { locale }),
+              }}
             />
-            <ListItem
-              color="rgba(253, 143, 255, 1)"
-              backgroundColor="rgba(255, 92, 225, 0.12)"
-              icon={CoinsHandIcon}
-              title={t("Earn fee discounts on Bittensor")}
-              description={t("Stake SEEK and trade with lower fees.")}
-            />
-            <ListItem
-              color="rgba(186, 143, 255, 1)"
-              backgroundColor="rgba(121, 112, 255, 0.19)"
-              icon={ClockIcon}
-              title={t("More benefits coming soon")}
-              description={t("Unlock future perks by staking SEEK.")}
-            />
-          </div>
+          </p>
+          <div className="text-body-secondary mt-16">{t("Why is this great?")}</div>
+          <ul className="text-body-secondary mt-4 list-inside list-disc space-y-2 text-sm">
+            <li>
+              <Trans
+                t={t}
+                components={{
+                  Highlight: <span className="text-body"></span>,
+                }}
+                defaults="<Highlight>Lower existential deposit:</Highlight> reduced from 1 DOT to 0.01 DOT."
+              ></Trans>
+            </li>
+            <li>
+              <Trans
+                t={t}
+                components={{
+                  Highlight: <span className="text-body"></span>,
+                }}
+                defaults="Significantly <Highlight>reduced transaction fees</Highlight> and deposits."
+              ></Trans>
+            </li>
+            <li>
+              <Trans
+                t={t}
+                components={{
+                  Highlight: <span className="text-body"></span>,
+                }}
+                defaults="<Highlight>Expanded asset support:</Highlight> stablecoins (USDT, USDC) and other ecosystem tokens."
+              ></Trans>
+            </li>
+
+            <li>
+              <Trans
+                t={t}
+                components={{
+                  Highlight: <span className="text-body"></span>,
+                }}
+                defaults="<Highlight>Fee flexibility:</Highlight> ability to pay transaction fees in any supported asset."
+              ></Trans>
+            </li>
+            <li>
+              <Trans
+                t={t}
+                components={{
+                  Highlight: <span className="text-body"></span>,
+                }}
+                defaults="<Highlight>Improved user experience:</Highlight> faster and more efficient transactions."
+              ></Trans>
+            </li>
+          </ul>
+          <p className="text-body-secondary mt-8 text-sm">
+            {t(
+              "No action is required: all balances will be transfered automatically as part of the migration.",
+            )}
+          </p>
         </div>
-        <Button iconLeft={TalismanHandIcon} onClick={onClose}>
-          {t("Close")}
-        </Button>
+        <div className="grid grid-cols-2 gap-8">
+          <Button
+            icon={ExternalLinkIcon}
+            className="shrink-0 px-0"
+            onClick={() => {
+              window.open(
+                "https://support.polkadot.network/support/solutions/articles/65000190561",
+                "_blank",
+              )
+            }}
+          >
+            {t("Read More")}
+          </Button>
+          <Button primary className="shrink-0" onClick={onClose}>
+            {t("Close")}
+          </Button>
+        </div>
       </div>
     </ModalDialog>
-  )
-}
-
-const ListItem: FC<{
-  icon: FC<SVGProps<SVGSVGElement>>
-  color: string
-  backgroundColor: string
-  title: ReactNode
-  description: ReactNode
-}> = ({ icon: Icon, color, backgroundColor, title, description }) => {
-  return (
-    <div className="flex h-20 w-full items-center gap-8">
-      <div
-        className="flex size-20 shrink-0 items-center justify-center rounded-full"
-        style={{ backgroundColor }}
-      >
-        <Icon className="size-10" style={{ color }} />
-      </div>
-      <div className="flex grow flex-col gap-2">
-        <div className="text-body text-xs">{title}</div>
-        <div className="text-body-secondary text-xs">{description}</div>
-      </div>
-    </div>
   )
 }
