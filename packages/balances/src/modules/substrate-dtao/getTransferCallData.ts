9821cdd623439f10fa8937501a206ba48b890744
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-10-23 10:29:16 +0900

    feat: transfers

--- packages/balances/src/modules/substrate-dtao/getTransferCallData.ts
+++ packages/balances/src/modules/substrate-dtao/getTransferCallData.ts
@@ -1,118 +1,40 @@
+import { mergeUint8 } from "@polkadot-api/utils"
+import { isTokenOfType, parseSubDTaoTokenId } from "@talismn/chaindata-provider"
+import { parseMetadataRpc } from "@talismn/scale"
+import { Binary } from "polkadot-api"
+
 import { IBalanceModule } from "../../types/IBalanceModule"
 import { MODULE_TYPE } from "./config"
 
-export const getTransferCallData: IBalanceModule<typeof MODULE_TYPE>["getTransferCallData"] = () =>
-  // {
-  // from,
-  // to,
-  // value,
-  // token,
+export const getTransferCallData: IBalanceModule<typeof MODULE_TYPE>["getTransferCallData"] = ({
+  from,
+  to,
+  value,
+  token,
   // type,
-  // metadataRpc,
-  // },
-  {
-    throw new Error("Not implemented")
-    // if (!isTokenOfType(token, MODULE_TYPE))
-    //   throw new Error(`Token type ${token.type} is not ${MODULE_TYPE}.`)
+  metadataRpc,
+}) => {
+  if (!isTokenOfType(token, MODULE_TYPE))
+    throw new Error(`Token type ${token.type} is not ${MODULE_TYPE}.`)
 
-    // const { builder } = parseMetadataRpc(metadataRpc)
-    // const method = getTransferMethod(type)
-    // const { codec, location } = builder.buildCall("Assets", method)
-    // const args = getEncodedArgs(method, token.assetId, to, value, codec)
-    // const callData = Binary.fromBytes(mergeUint8([new Uint8Array(location), args]))
+  const { subnetId, hotkey } = parseSubDTaoTokenId(token.id)
+  if (!hotkey) throw new Error(`Token ${token.id} does not have a hotkey specified.`)
 
-    // return {
-    //   address: from,
-    //   method: callData.asHex() as `0x${string}`,
-    // }
+  const { builder } = parseMetadataRpc(metadataRpc)
+  const { codec, location } = builder.buildCall("SubtensorModule", "transfer_stake")
+
+  const args = codec.enc({
+    destination_coldkey: to,
+    destination_netuid: subnetId,
+    alpha_amount: value,
+    hotkey,
+    origin_netuid: subnetId,
+  })
+
+  const callData = Binary.fromBytes(mergeUint8([new Uint8Array(location), args]))
+
+  return {
+    address: from,
+    method: callData.asHex() as `0x${string}`,
   }
-
-// const getTransferMethod = (type: BalanceTransferType) => {
-//   // TODO handle the case where transfer_all does not exist
-//   switch (type) {
-//     case "keep-alive":
-//       return "transfer_keep_alive"
-//     case "all":
-//       return "transfer_all"
-//     case "allow-death":
-//       return "transfer"
-//   }
-// }
-
-// const getEncodedArgs = (
-//   method: ReturnType<typeof getTransferMethod>,
-//   assetId: string,
-//   to: string,
-//   value: string,
-//   argsCodec: Codec<unknown>,
-// ): Uint8Array => {
-//   try {
-//     switch (method) {
-//       case "transfer_keep_alive":
-//       case "transfer":
-//         return getTransferEncodedArgs(assetId, to, value, argsCodec)
-//       case "transfer_all":
-//         return getTransferAllEncodedArgs(assetId, to, argsCodec)
-//     }
-//   } catch {
-//     throw new Error(`Failed to encode arguments for method ${method}: ${assetId}, ${to}, ${value}`)
-//   }
-// }
-
-// const getEncodedValue = (codec: Codec<unknown>, possibleValue: Array<() => unknown>) => {
-//   for (const getArgs of possibleValue) {
-//     try {
-//       return codec.enc(getArgs())
-//     } catch (error) {
-//       // wrong inputs, ignore and try the next one
-//     }
-//   }
-
-//   throw new Error("Failed to encode")
-// }
-
-// // same inputs for both KeepAlive and allowDeath
-// const getTransferEncodedArgs = (
-//   assetId: string,
-//   to: string,
-//   value: string,
-//   codec: Codec<unknown>,
-// ) => {
-//   return getEncodedValue(codec, [
-//     () => ({
-//       id: Number(assetId), // for most networks
-//       target: Enum("Id", to),
-//       amount: BigInt(value),
-//     }),
-//     () => ({
-//       id: BigInt(assetId), // for Astar
-//       target: Enum("Id", to),
-//       amount: BigInt(value),
-//     }),
-//     () => ({
-//       id: BigInt(assetId), // for Neuroweb
-//       target: to,
-//       amount: BigInt(value),
-//     }),
-//   ])
-// }
-
-// const getTransferAllEncodedArgs = (assetId: string, to: string, codec: Codec<unknown>) => {
-//   return getEncodedValue(codec, [
-//     () => ({
-//       id: Number(assetId), // for most networks
-//       dest: Enum("Id", to),
-//       keep_alive: false,
-//     }),
-//     () => ({
-//       id: BigInt(assetId), // for Astar
-//       dest: Enum("Id", to),
-//       keep_alive: false,
-//     }),
-//     () => ({
-//       id: BigInt(assetId), // for Neuroweb (although transfer_all does not exist)
-//       dest: to,
-//       keep_alive: false,
-//     }),
-//   ])
-// }
+}
