2cd4fe80e8c9480915ca42f872dae67c2ac57a92
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-10-24 16:27:10 +0900

    fix: dtao root symbol

--- packages/balances/src/modules/substrate-dtao/fetchTokens.ts
+++ packages/balances/src/modules/substrate-dtao/fetchTokens.ts
@@ -14,6 +14,12 @@ import { DEFAULT_DTAO_LOGO, MODULE_TYPE, PLATFORM, TokenConfig } from "./config"
 type GetDynamicInfosResult =
   (typeof bittensor)["descriptors"]["apis"]["SubnetInfoRuntimeApi"]["get_all_dynamic_info"][1]
 
+// hardcoded because we dont have access to native tokens from the balance module
+const NATIVE_TOKEN_SYMBOLS: Record<string, string> = {
+  "bittensor": "TAO",
+  "bittensor-testnet": "testTAO",
+}
+
 export const fetchTokens: IBalanceModule<typeof MODULE_TYPE, TokenConfig>["fetchTokens"] = async ({
   networkId,
   connector,
@@ -34,11 +40,16 @@ export const fetchTokens: IBalanceModule<typeof MODULE_TYPE, TokenConfig>["fetch
   return dynamicInfos
     .filter(isNotNil)
     .map((info): SubDTaoToken => {
-      const symbol = new TextDecoder().decode(Uint8Array.from(info.token_symbol))
+      let symbol = new TextDecoder().decode(Uint8Array.from(info.token_symbol))
       const subnetName =
         info.subnet_identity?.subnet_name?.asText() ??
         (info.netuid === 0 ? "Root" : `Subnet ${info.netuid}`)
       const name = `${info.netuid} | ${subnetName} ${symbol}`
+
+      // for root we want same symbol as native so they can be grouped together in portfolio
+      if (info.netuid === 0 && NATIVE_TOKEN_SYMBOLS[networkId])
+        symbol = NATIVE_TOKEN_SYMBOLS[networkId]
+
       return {
         id: subDTaoTokenId(networkId, info.netuid),
         type: MODULE_TYPE,
