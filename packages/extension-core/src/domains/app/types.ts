2d1b6f5fedfee1bd91710225809dcd82dac18545
Kheops <26880866+0xKheops@users.noreply.github.com>
2025-10-24 09:59:34 +0900

    chore: remove auto dapp scan (#2220)

--- packages/extension-core/src/domains/app/types.ts
+++ packages/extension-core/src/domains/app/types.ts
@@ -89,7 +89,6 @@ export type FeatureFlags = Partial<{
   SEEK_BENEFITS: boolean
   SEEK_TAO_DISCOUNT: boolean
   SEEK_PRESALE: boolean
-  BLOCKAID_DAPP_SCAN: boolean
 }>
 export type FeatureFlag = keyof FeatureFlags
 
