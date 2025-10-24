a79f56277c657fa22f549e6520715606ce623ba7
Alec WM <2741569+alecdwm@users.noreply.github.com>
2025-10-24 16:22:15 +1000

    chore: run ff build script without sudo when BUILD_NOSUDO=true (#2221)

--- scripts/build-review-firefox.sh
+++ scripts/build-review-firefox.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+if [ -n "${BUILD_NOSUDO:-}" ]; then
+    DOCKER_CMD="docker"
+else
+    DOCKER_CMD="sudo docker"
+fi
+
 rm -rf review
 mkdir review
-sudo docker build . --tag talisman-builder
-sudo docker run --rm --volume "$(pwd)/review":/review talisman-builder bash -c ' \
+$DOCKER_CMD build . --tag talisman-builder
+$DOCKER_CMD run --rm --volume "$(pwd)/review":/review talisman-builder bash -c ' \
     NODE_OPTIONS=--max_old_space_size=8192 USE_ONE_DIST_DIR=true pnpm build:extension:prod:firefox && \
     cp /talisman/apps/extension/dist/*.zip /review/ && \
     rm -rf /talisman/apps/extension/dist && \
