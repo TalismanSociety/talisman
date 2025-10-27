3824b411c20e90dcb4add462170650fee0999d4f
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-10-27 13:39:54 +0900

    refactor: simplify unbond button

--- apps/extension/src/ui/domains/Portfolio/AssetDetails/DashboardTokenBalances/TokenBalancesDetailRow.tsx
+++ apps/extension/src/ui/domains/Portfolio/AssetDetails/DashboardTokenBalances/TokenBalancesDetailRow.tsx
@@ -17,7 +17,6 @@ export const TokenBalancesDetailRow = ({
   symbol,
   tokenId,
   tokenDecimals,
-  netuid,
 }: {
   row: BalanceDetailRow
   isLastRow?: boolean
@@ -25,7 +24,6 @@ export const TokenBalancesDetailRow = ({
   symbol: string
   tokenId?: TokenId // unsafe, there could be multiple aggregated here
   tokenDecimals: number
-  netuid?: number
 }) => {
   const tokenBalance = useMemo(() => {
     const alphaBalanceInTao = new BigNumber(
@@ -65,7 +63,7 @@ export const TokenBalancesDetailRow = ({
       </div>
       {!!row.locked && row.meta && tokenId && (
         <LockedExtra
-          netuid={netuid}
+          //  netuid={netuid}
           tokenId={tokenId}
           address={row.address}
           isLoading={status.status === "fetching"}
