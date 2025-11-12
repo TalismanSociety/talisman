fa6fd66464fcf720b59c942b5f199c5431bea710
alecdwm <2741569+alecdwm@users.noreply.github.com>
2025-11-12 20:24:11 +0000

    chore: disable more balance modules

--- packages/balances/src/modules/index.ts
+++ packages/balances/src/modules/index.ts
@@ -1,29 +1,20 @@
-import { EvmErc20BalanceModule } from "./evm-erc20"
 import { EvmNativeBalanceModule } from "./evm-native"
-import { EvmUniswapV2BalanceModule } from "./evm-uniswapv2"
 import { SolNativeBalanceModule } from "./sol-native"
-import { SolSplBalanceModule } from "./sol-spl"
-import { SubAssetsBalanceModule } from "./substrate-assets"
-import { SubDTaoBalanceModule } from "./substrate-dtao"
-import { SubForeignAssetsBalanceModule } from "./substrate-foreignassets"
-import { SubHydrationBalanceModule } from "./substrate-hydration"
 import { SubNativeBalanceModule } from "./substrate-native"
-// import { SubPsp22BalanceModule } from "./substrate-psp22"
-import { SubTokensBalanceModule } from "./substrate-tokens"
 
 export const BALANCE_MODULES = [
   SubNativeBalanceModule,
-  SubAssetsBalanceModule,
-  SubDTaoBalanceModule,
-  SubHydrationBalanceModule,
-  SubForeignAssetsBalanceModule,
+  // SubAssetsBalanceModule,
+  // SubDTaoBalanceModule,
+  // SubHydrationBalanceModule,
+  // SubForeignAssetsBalanceModule,
   // SubPsp22BalanceModule,
-  SubTokensBalanceModule,
-  EvmErc20BalanceModule,
-  EvmUniswapV2BalanceModule,
+  // SubTokensBalanceModule,
+  // EvmErc20BalanceModule,
+  // EvmUniswapV2BalanceModule,
   EvmNativeBalanceModule,
   SolNativeBalanceModule,
-  SolSplBalanceModule,
+  // SolSplBalanceModule,
 ]
 
 export type AnyBalanceModule = (typeof BALANCE_MODULES)[number] // TODO yeet ? should use IBalance
