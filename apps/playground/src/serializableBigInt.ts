842c74620518049ecc665e24e9955a0aec324255
Kheops <26880866+0xKheops@users.noreply.github.com>
2025-11-18 16:47:37 +0900

    🔥🔥🔥 BURN PLAYGROUND 🔥🔥🔥 (#2254)
    * chore: yeet playground folder
    
    * chore: remove unused dependencies

--- apps/playground/src/serializableBigInt.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-redeclare, @typescript-eslint/no-unused-vars
-interface BigInt {
-  /** Convert to BigInt to string form in JSON.stringify */
-  toJSON: () => string
-}
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-;(BigInt.prototype as any).toJSON = function () {
-  return this.toString()
-}
