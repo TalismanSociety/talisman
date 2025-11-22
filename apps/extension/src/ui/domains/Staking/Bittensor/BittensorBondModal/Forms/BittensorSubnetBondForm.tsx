b6d7ade7e776438ad2f610cf6869218c4714df27
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-11-22 21:22:55 +0900

    fix: subnet button flickering

--- apps/extension/src/ui/domains/Staking/Bittensor/BittensorBondModal/Forms/BittensorSubnetBondForm.tsx
+++ apps/extension/src/ui/domains/Staking/Bittensor/BittensorBondModal/Forms/BittensorSubnetBondForm.tsx
@@ -1,7 +1,5 @@
 import { useTranslation } from "react-i18next"
 
-import { useCombinedSubnetData } from "@ui/domains/Staking/hooks/bittensor/dTao/useCombinedSubnetData"
-
 import { useBittensorBondWizard } from "../../hooks/useBittensorBondWizard"
 import { BittensorBondFormBase } from "../BittensorBondFormBase"
 import { BittensorSelectButton } from "../BittensorSelectButton"
@@ -9,20 +7,17 @@ import { BittensorSelectButton } from "../BittensorSelectButton"
 export const BittensorSubnetBondForm = () => {
   const { t } = useTranslation()
 
-  const { netuid } = useBittensorBondWizard()
-  const { isLoading, subnetData } = useCombinedSubnetData()
-
-  const { subnet_name, symbol } = subnetData?.[netuid || 0] ?? {}
-
-  const selectedSubnetLabel = `SN${netuid} ${subnet_name} ${symbol}`
-  const label = netuid ? selectedSubnetLabel : "Subnet"
+  const { dtaoToken } = useBittensorBondWizard()
 
   const SubnetStakeDetails = () => {
     return (
       <div className="flex items-center justify-between gap-8">
         <div className="whitespace-nowrap">{t("Select Subnet")}</div>
         <div className="text-body truncate">
-          <BittensorSelectButton label={label} isLoading={isLoading} nextStep="select-subnet" />
+          <BittensorSelectButton
+            label={dtaoToken?.subnetName ?? t("Subnet")}
+            nextStep="select-subnet"
+          />
         </div>
       </div>
     )
