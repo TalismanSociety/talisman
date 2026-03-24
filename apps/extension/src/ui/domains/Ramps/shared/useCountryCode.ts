6bc0e7b468b3bc01e8d650d72092adefc01943f0
0xKheops <26880866+0xKheops@users.noreply.github.com>
2026-03-24 21:12:19 +0900

    feat: gandalf for coinbase api

--- apps/extension/src/ui/domains/Ramps/shared/useCountryCode.ts
+++ apps/extension/src/ui/domains/Ramps/shared/useCountryCode.ts
@@ -1,12 +1,13 @@
 import { RAMPS_COINBASE_API_BASE_PATH } from "@common/constants"
 import { useQuery } from "@tanstack/react-query"
+import { gandalfFetch } from "@ui/util/gandalfFetch"
 import urlJoin from "url-join"
 
 export const useCountryCode = () => {
   return useQuery({
     queryKey: ["useCountryCode"],
     queryFn: async (): Promise<{ countryCode: string; regionCode: string | null }> => {
-      const res = await fetch(urlJoin(RAMPS_COINBASE_API_BASE_PATH, "/country"))
+      const res = await gandalfFetch(urlJoin(RAMPS_COINBASE_API_BASE_PATH, "/country"))
       if (!res.ok) throw new Error("Failed to fetch country code")
       return await res.json()
     },
