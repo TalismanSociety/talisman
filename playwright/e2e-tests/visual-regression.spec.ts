7bc45b43d2cc4a1411419d5d3b4e13ae819fe28b
prury <prury@tlsmn.z>
2026-04-29 15:49:47 +0200

    ci: 30 percent diff on visual regression

--- playwright/e2e-tests/visual-regression.spec.ts
+++ playwright/e2e-tests/visual-regression.spec.ts
@@ -1,51 +1,43 @@
 import { expect, test } from "./fixtures"
 
+const maxDiffPixelRatio = 0.3
+
 test("Visual Regression Test", async ({ onboardedPage, extensionId }) => {
   await onboardedPage.getByTestId("onboarding-toast-animated").click()
   await onboardedPage.emulateMedia({ reducedMotion: "reduce" })
   // Portfolio
-  await expect(onboardedPage).toHaveScreenshot("portfolio.png", {
-    maxDiffPixelRatio: 0.01,
-  })
+  await expect(onboardedPage).toHaveScreenshot("portfolio.png", { maxDiffPixelRatio })
   // Accounts
   await onboardedPage.goto(`chrome-extension://${extensionId}/dashboard.html#/accounts/add/`)
-  await expect(onboardedPage).toHaveScreenshot("accounts-add.png", {
-    maxDiffPixelRatio: 0.01,
-  })
+  await expect(onboardedPage).toHaveScreenshot("accounts-add.png", { maxDiffPixelRatio })
   // Accounts - ledger
   await onboardedPage.goto(`chrome-extension://${extensionId}/dashboard.html#/accounts/add/ledger`)
   await onboardedPage.getByRole("button", { name: "Substrate" }).click()
   await onboardedPage.getByRole("button", { name: "Select a Network" }).click()
   await onboardedPage.getByRole("option", { name: "Polkadot", exact: true }).click()
   await onboardedPage.keyboard.press("End")
-  await expect.soft(onboardedPage).toHaveScreenshot("accounts-ledger.png", {
-    maxDiffPixelRatio: 0.01,
-  })
+  await expect.soft(onboardedPage).toHaveScreenshot("accounts-ledger.png", { maxDiffPixelRatio })
   // Earn
   await onboardedPage.goto(`chrome-extension://${extensionId}/dashboard.html#/earn/positions`)
-  await expect.soft(onboardedPage).toHaveScreenshot("earn.png", {
-    maxDiffPixelRatio: 0.01,
-  })
+  await expect.soft(onboardedPage).toHaveScreenshot("earn.png", { maxDiffPixelRatio })
   // Activity
   await onboardedPage.goto(`chrome-extension://${extensionId}/dashboard.html#/tx-history`)
-  await expect.soft(onboardedPage).toHaveScreenshot("activity.png", {
-    maxDiffPixelRatio: 0.01,
-  })
+  await expect.soft(onboardedPage).toHaveScreenshot("activity.png", { maxDiffPixelRatio })
   // Settings
   await onboardedPage.goto(`chrome-extension://${extensionId}/dashboard.html#/settings`)
-  await expect.soft(onboardedPage).toHaveScreenshot("settings.png", { maxDiffPixelRatio: 0.01 })
+  await expect.soft(onboardedPage).toHaveScreenshot("settings.png", { maxDiffPixelRatio })
   // Settings - Security & Privacy
   await onboardedPage.goto(
     `chrome-extension://${extensionId}/dashboard.html#/settings/security-privacy-settings`
   )
   await expect.soft(onboardedPage).toHaveScreenshot("settings-security-privacy.png", {
-    maxDiffPixelRatio: 0.01,
+    maxDiffPixelRatio,
   })
   // Settings - Networks & Tokens
   await onboardedPage.goto(
     `chrome-extension://${extensionId}/dashboard.html#/settings/networks-tokens`
   )
   await expect.soft(onboardedPage).toHaveScreenshot("settings-network-tokens.png", {
-    maxDiffPixelRatio: 0.01,
+    maxDiffPixelRatio,
   })
 })
