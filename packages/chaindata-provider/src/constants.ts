2f74269a2574db8f3b4723b38bc832f614c30d73
0xKheops <26880866+0xKheops@users.noreply.github.com>
2026-05-07 16:58:33 +0900

    fix: test to use the correct pub folder

--- packages/chaindata-provider/src/constants.ts
+++ packages/chaindata-provider/src/constants.ts
@@ -2,7 +2,7 @@
 const CHAINDATA_BRANCH = "chaindata-v10" // TODO revert to main before merging
 
 // pub subfolder needs to be incremented each time the schema of chaindata changes, or when the content of the minimetadata changes
-const CHAINDATA_PUB_FOLDER = "pub/v10"
+export const CHAINDATA_PUB_FOLDER = "pub/v10"
 
 export const MINIMETADATA_VERSION = CHAINDATA_PUB_FOLDER.split("/").pop()!
 if (!MINIMETADATA_VERSION) {
