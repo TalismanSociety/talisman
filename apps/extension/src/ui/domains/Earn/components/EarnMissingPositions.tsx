4ea287396589260a0cd1bfff6dfc492f225ba0ea
0xKheops <26880866+0xKheops@users.noreply.github.com>
2026-06-15 10:42:57 +0900

    fix: underlined staking link

--- apps/extension/src/ui/domains/Earn/components/EarnMissingPositions.tsx
+++ apps/extension/src/ui/domains/Earn/components/EarnMissingPositions.tsx
@@ -1,22 +1,19 @@
 import { TALISMAN_WEB_APP_URL } from "@common/constants"
 import { InfoIcon } from "@talismn/icons"
 import { Tooltip, TooltipContent, TooltipTrigger } from "@ui/components/Tooltip"
+import { cn } from "@ui/util/cn"
 import type { FC } from "react"
-import { useTranslation } from "react-i18next"
+import { Trans, useTranslation } from "react-i18next"
 
 export const EarnMissingPositions: FC<{ className?: string }> = ({ className }) => {
   const { t } = useTranslation()
 
   return (
-    <div className={className}>
-      <span className="mr-2 text-body-inactive">{t("Missing Substrate staking positions?")}</span>
-      <button
-        type="button"
-        className="inline-block text-left text-body-inactive hover:text-body-secondary"
-        onClick={() => window.open(TALISMAN_WEB_APP_URL, "_blank", "noopener,noreferrer")}
-      >
-        {t("Browse our legacy staking portal")}
-      </button>
+    <div className={cn("text-body-inactive", className)}>
+      <Trans t={t}>
+        Missing Substrate staking positions? Browse our <PortalLink />
+      </Trans>
+
       <Tooltip>
         <TooltipTrigger>
           <InfoIcon className="ml-2 inline-block text-body-inactive" />
@@ -26,5 +23,19 @@ export const EarnMissingPositions: FC<{ className?: string }> = ({ className }) 
         </TooltipContent>
       </Tooltip>
     </div>
+  )
+}
+
+const PortalLink = () => {
+  const { t } = useTranslation()
+
+  return (
+    <button
+      type="button"
+      className="inline-block text-left text-body-inactive underline hover:text-body-secondary"
+      onClick={() => window.open(TALISMAN_WEB_APP_URL, "_blank", "noopener,noreferrer")}
+    >
+      {t("legacy staking portal")}
+    </button>
   )
 }
