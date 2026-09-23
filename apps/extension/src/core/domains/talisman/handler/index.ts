cdcf2ca0938b20423c44542d6984618ab9ba642e
0xKheops <26880866+0xKheops@users.noreply.github.com>
2026-09-23 19:47:07 +0900

    🔥 remove talismanSub custom chaindata subscriptions

--- apps/extension/src/core/domains/talisman/handler/index.ts
+++ apps/extension/src/core/domains/talisman/handler/index.ts
@@ -6,9 +6,6 @@ import type { MessageTypes, RequestTypes, ResponseType } from "../../../types"
 import type { Port } from "../../../types/base"
 import TalismanRpcHandler from "./rpc"
 
-/**
- * Disabled all these messages for now by throwing an error, verified it doesn't break portal
- */
 export default class TalismanHandler extends TabsHandler {
   readonly #subHandlers: readonly TabsHandler[]
 
@@ -31,55 +28,6 @@ export default class TalismanHandler extends TabsHandler {
     if (!isTalismanUrl(url)) throw new Error(`Origin not allowed for message type ${type}`)
 
     switch (type) {
-      case "pub(talisman.customSubstrateChains.subscribe)": {
-        throw new Error("Not implemented")
-        // return genericSubscription(
-        //   id,
-        //   port,
-        //   chaindataProvider
-        //     .getNetworks$("polkadot")
-        //     .pipe(map((networks) => networks.filter(isNetworkCustom)))
-        // )
-      }
-
-      case "pub(talisman.customSubstrateChains.unsubscribe)": {
-        throw new Error("Not implemented")
-        // const subId = request as RequestTypes["pub(talisman.customSubstrateChains.unsubscribe)"]
-        // return unsubscribe(subId)
-      }
-
-      case "pub(talisman.customEvmNetworks.subscribe)": {
-        throw new Error("Not implemented")
-        // return genericSubscription(
-        //   id,
-        //   port,
-        //   chaindataProvider
-        //     .getNetworks$("ethereum")
-        //     .pipe(map((networks) => networks.filter(isNetworkCustom)))
-        // )
-      }
-
-      case "pub(talisman.customEvmNetworks.unsubscribe)": {
-        throw new Error("Not implemented")
-        // const subId = request as RequestTypes["pub(talisman.customEvmNetworks.unsubscribe)"]
-        // return unsubscribe(subId)
-      }
-
-      case "pub(talisman.customTokens.subscribe)": {
-        throw new Error("Not implemented")
-        // return genericSubscription(
-        //   id,
-        //   port,
-        //   chaindataProvider.tokens$.pipe(map((tokens) => tokens.filter(isTokenCustom)))
-        // )
-      }
-
-      case "pub(talisman.customTokens.unsubscribe)": {
-        throw new Error("Not implemented")
-        // const subId = request as RequestTypes["pub(talisman.customTokens.unsubscribe)"]
-        // return unsubscribe(subId)
-      }
-
       case "pub(talisman.extension.openPortfolio)": {
         await windowManager.openDashboard({ route: "/portfolio" })
         return true
