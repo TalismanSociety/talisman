35dec883b2e87e8fc852b2d9ad61565e9ce66e4f
0xKheops <26880866+0xKheops@users.noreply.github.com>
2026-09-26 14:22:05 +0900

    🚚 rename ui state addressBook to contacts and networks to networkDisplay

--- apps/extension/src/ui/domains/Networks/NetworkName.tsx
+++ apps/extension/src/ui/domains/Networks/NetworkName.tsx
@@ -1,5 +1,5 @@
 import type { NetworkId } from "@talismn/chaindata-provider"
-import { useNetworkDisplayName } from "@ui/state/networks"
+import { useNetworkDisplayName } from "@ui/state/networkDisplay"
 import type { FC } from "react"
 
 export const NetworkName: FC<{ networkId: NetworkId | null | undefined; className?: string }> = ({
